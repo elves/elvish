c9d79efdf04d0d1b0fdc915de077c3b3e2c3f9b0
Cheer Xiao <xiaqqaix@gmail.com>
2013-06-26 05:41:30 +0800

    Server: remove unused TubeFd

--- server/tube.c
+++ server/tube.c
@@ -1,12 +1,10 @@
 #include "common.h"
 #include "tube.h"
 
-int TubeFd;
 int FdTubeFd;
 FILE *TubeFile;
 
 void InitTubes(int textTube, int fdTube) {
-    TubeFd = textTube;
     TubeFile = fdopen(textTube, "a+");
     if (!TubeFile) {
         Check_1("fdopen", -1);
