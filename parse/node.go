21837bc0a3f56cbe5a287590abf929dba567c9d5
Cheer Xiao <xiaqqaix@gmail.com>
2013-09-18 19:20:22 +0800

    parse: Remove unneeded NodeType

--- parse/node.go
+++ parse/node.go
@@ -17,7 +17,6 @@ import (
 // The interface contains an unexported method so that only
 // types local to this package can satisfy it.
 type Node interface {
-	Type() NodeType
 	String() string
 	// Copy does a deep copy of the Node and all its components.
 	// To avoid type assertions, some XxxNodes also have specialized
@@ -27,9 +26,6 @@ type Node interface {
 	// Make sure only functions in this package can create Nodes.
 	unexported()
 }
-
-// NodeType identifies the type of a parse tree node.
-type NodeType int
 
 // Pos represents a byte position in the original input text from which
 // this source was parsed.
@@ -44,30 +40,16 @@ func (p Pos) Position() Pos {
 func (Pos) unexported() {
 }
 
-// Type returns itself and provides an easy default implementation
-// for embedding in a Node. Embedded in all non-trivial Nodes.
-func (t NodeType) Type() NodeType {
-	return t
-}
-
-const (
-	NodeString     NodeType = iota // A string literal.
-	NodeCommand                    // A command.
-)
-
 // Nodes.
 
 // ListNode holds a sequence of nodes.
-// Its NodeType is not fixed, i.e. there are different NodeType's implemented
-// in terms of ListNode.
 type ListNode struct {
-	NodeType
 	Pos
 	Nodes []Node
 }
 
-func newList(nodeType NodeType, pos Pos) *ListNode {
-	return &ListNode{NodeType: nodeType, Pos: pos}
+func newList(pos Pos) *ListNode {
+	return &ListNode{Pos: pos}
 }
 
 func (l *ListNode) append(n Node) {
@@ -86,7 +68,7 @@ func (l *ListNode) CopyList() *ListNode {
 	if l == nil {
 		return l
 	}
-	n := newList(l.NodeType, l.Pos)
+	n := newList(l.Pos)
 	for _, elem := range l.Nodes {
 		n.append(elem.Copy())
 	}
@@ -105,19 +87,18 @@ type CommandNode struct {
 }
 
 func newCommand(pos Pos) *CommandNode {
-	return &CommandNode{ListNode: *newList(NodeCommand, pos)}
+	return &CommandNode{ListNode: *newList(pos)}
 }
 
 // StringNode holds a string constant. The value has been "unquoted".
 type StringNode struct {
-	NodeType
 	Pos
 	Quoted string // The original text of the string, with quotes.
 	Text   string // The string, after quote processing.
 }
 
 func newString(pos Pos, orig, text string) *StringNode {
-	return &StringNode{NodeType: NodeString, Pos: pos, Quoted: orig, Text: text}
+	return &StringNode{Pos: pos, Quoted: orig, Text: text}
 }
 
 func (s *StringNode) String() string {
