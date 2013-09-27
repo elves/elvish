516e26177a7b24db74833d23b44929917fdd894c
Cheer Xiao <xiaqqaix@gmail.com>
2013-09-27 21:27:21 +0800

    edit: Format function key runes more compactly

--- edit/key.go
+++ edit/key.go
@@ -39,30 +39,9 @@ func (k Key) String() (s string) {
 }
 
 const (
-	F1 rune = -1-iota
-	F2
-	F3
-	F4
-	F5
-	F6
-	F7
-	F8
-	F9
-	F10
-	F11
-	F12
-
-	Up
-	Down
-	Right
-	Left
-
-	Home
-	Insert
-	Delete
-	End
-	PageUp
-	PageDown
+	F1 rune = -1-iota; F2; F3; F4; F5; F6; F7; F8; F9; F10; F11; F12;
+	Up; Down; Right; Left;
+	Home; Insert; Delete; End; PageUp; PageDown;
 
 	// Some function key names are just aliases for their ASCII representation
 	Tab = '\t'
