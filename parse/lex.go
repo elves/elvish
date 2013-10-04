a24ad635737b9c6f7c1c58f474b3e1688efc043f
Cheer Xiao <xiaqqaix@gmail.com>
2013-10-04 23:59:08 +0800

    parse: Terminate barewords at " ' too

--- parse/lex.go
+++ parse/lex.go
@@ -265,7 +265,7 @@ func lexBare(l *Lexer) stateFn {
 
 func terminatesBare(r rune) bool {
 	switch r {
-	case '\n', '(', ')', Eof:
+	case '\n', '(', ')', '"', '\'', Eof:
 		return true
 	}
 	return isSpace(r)
