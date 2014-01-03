1371ba123e72b581e2c26a3dc693d7674d43d64e
Cheer Xiao <xiaqqaix@gmail.com>
2014-01-03 14:55:07 +0800

    Introduce editorAction so that all keybindings can be implemented as func

--- edit/builtins.go
+++ edit/builtins.go
@@ -10,7 +10,22 @@ import (
 // reside in a dedicated namespace and callable by users, e.g.
 // le/kill-line-forward.
 
-type leBuiltin func (ed *Editor)
+type editorAction int
+
+const (
+	noAction editorAction = iota
+	changeMode
+	changeModeAndReprocess
+	exitReadLine
+)
+
+type leReturn struct {
+	action editorAction
+	newMode bufferMode
+	readLineReturn LineRead
+}
+
+type leBuiltin func (ed *Editor) *leReturn
 
 var leBuiltins = map[string]leBuiltin{
 	"kill-line-b": killLineB,
@@ -25,16 +40,18 @@ var leBuiltins = map[string]leBuiltin{
 	"cycle-cand-f": cycleCandF,
 }
 
-func killLineB(ed *Editor) {
+func killLineB(ed *Editor) *leReturn {
 	ed.line = ed.line[ed.dot:]
 	ed.dot = 0
+	return nil
 }
 
-func killLineF(ed *Editor) {
+func killLineF(ed *Editor) *leReturn {
 	ed.line = ed.line[:ed.dot]
+	return nil
 }
 
-func killRuneB(ed *Editor) {
+func killRuneB(ed *Editor) *leReturn {
 	if ed.dot > 0 {
 		_, w := utf8.DecodeLastRuneInString(ed.line[:ed.dot])
 		ed.line = ed.line[:ed.dot-w] + ed.line[ed.dot:]
@@ -42,23 +59,26 @@ func killRuneB(ed *Editor) {
 	} else {
 		ed.beep()
 	}
+	return nil
 }
 
-func moveDotB(ed *Editor) {
+func moveDotB(ed *Editor) *leReturn {
 	_, w := utf8.DecodeLastRuneInString(ed.line[:ed.dot])
 	ed.dot -= w
+	return nil
 }
 
-func moveDotF(ed *Editor) {
+func moveDotF(ed *Editor) *leReturn {
 	_, w := utf8.DecodeRuneInString(ed.line[ed.dot:])
 	ed.dot += w
+	return nil
 }
 
-func complete(ed *Editor) {
+func complete(ed *Editor) *leReturn {
 	startCompletion(ed)
 	c := ed.completion
 	if c == nil {
-		return
+		return nil
 	}
 	if len(c.candidates) == 0 {
 		ed.pushTip(fmt.Sprintf("No completion for %s", ed.line[c.start:c.end]))
@@ -66,21 +86,26 @@ func complete(ed *Editor) {
 	} else {
 		c.current = 0
 	}
+	return nil
 }
 
-func selectCandB(ed *Editor) {
+func selectCandB(ed *Editor) *leReturn {
 	ed.completion.prev(false)
+	return nil
 }
 
-func selectCandF(ed *Editor) {
+func selectCandF(ed *Editor) *leReturn {
 	ed.completion.next(false)
+	return nil
 }
 
-func cycleCandF(ed *Editor) {
+func cycleCandF(ed *Editor) *leReturn {
 	ed.completion.next(true)
+	return nil
 }
 
-func cancelCompletion(ed *Editor) {
+func cancelCompletion(ed *Editor) *leReturn {
 	ed.completion = nil
 	ed.mode = ModeInsert
+	return nil
 }
