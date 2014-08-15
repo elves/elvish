fab47342cd06c74dd6170fb058ac770b850d163c
Cheer Xiao <xiaqqaix@gmail.com>
2014-08-15 23:18:15 +0200

    Add a trivial list implementation

---
+++ list/list.go
@@ -0,0 +1,26 @@
+package list
+
+// List implements a persistent list. The empty value is a valid empty list.
+type List struct {
+	first interface{}
+	rest  *List
+	count int
+}
+
+var Empty = &List{}
+
+func (l *List) Cons(val interface{}) *List {
+	return &List{val, l, l.count+1}
+}
+
+func (l *List) First() interface{} {
+	return l.first
+}
+
+func (l *List) Rest() *List {
+	return l.rest
+}
+
+func (l *List) Count() int {
+	return l.count
+}
