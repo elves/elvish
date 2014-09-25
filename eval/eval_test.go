91b16c3b542038bf39acdec80e0695cac176dbb4
Cheer Xiao <xiaqqaix@gmail.com>
2014-09-25 23:43:52 +0200

    eval: Also Test for eval errors

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -34,52 +34,55 @@ func stringValues(ss ...string) []Value {
 }
 
 var evalTests = []struct {
-	text   string
-	wanted []Value
+	text      string
+	wanted    []Value
+	wantError bool
 }{
 	// Empty chunk
-	{"", []Value{}},
+	{"", []Value{}, false},
 
 	// Trivial command
-	{"put 233 lorem ipsum", stringValues("233", "lorem", "ipsum")},
+	{"put 233 lorem ipsum", stringValues("233", "lorem", "ipsum"), false},
 
 	// Byte Pipeline
 	{`echo "Albert\nAllan\nAlbraham\nBerlin" | sed s/l/1/g | grep e | feedchan`,
-		stringValues("A1bert", "Ber1in")},
+		stringValues("A1bert", "Ber1in"), false},
 
 	// Arithmetics
 	// TODO test more edge cases
-	{"* 353 661", stringValues("233333")},
-	{"+ 233100 233", stringValues("233333")},
-	{"- 233333 233100", stringValues("233")},
-	{"/ 233333 353", stringValues("661")},
-	{"/ 1 0", stringValues("+Inf")},
+	{"* 353 661", stringValues("233333"), false},
+	{"+ 233100 233", stringValues("233333"), false},
+	{"- 233333 233100", stringValues("233"), false},
+	{"/ 233333 353", stringValues("661"), false},
+	{"/ 1 0", stringValues("+Inf"), false},
 
 	// String literal
-	{"put `such \\\"``literal`", stringValues("such \\\"`literal")},
+	{"put `such \\\"``literal`", stringValues("such \\\"`literal"), false},
 	{`put "much \n\033[31;1m$cool\033[m"`,
-		stringValues("much \n\033[31;1m$cool\033[m")},
+		stringValues("much \n\033[31;1m$cool\033[m"), false},
 
 	// Compounding list primaries
-	{"put {fi elvi}sh{1 2}", stringValues("fish1", "fish2", "elvish1", "elvish2")},
+	{"put {fi elvi}sh{1 2}", stringValues("fish1", "fish2", "elvish1", "elvish2"), false},
 
 	// Table and subscript
 	{"println [a b c &key value] | feedchan",
-		stringValues("[a b c &key value]")},
-	{"put [a b c &key value][2]", stringValues("c")},
-	{"put [a b c &key value][key]", stringValues("value")},
+		stringValues("[a b c &key value]"), false},
+	{"put [a b c &key value][2]", stringValues("c"), false},
+	{"put [a b c &key value][key]", stringValues("value"), false},
 
 	// Variable and compounding
 	{"var $x string `SHELL`\nput `WOW, SUCH `$x`, MUCH COOL`\n",
-		stringValues("WOW, SUCH SHELL, MUCH COOL")},
+		stringValues("WOW, SUCH SHELL, MUCH COOL"), false},
 
 	// var and set
-	{"var [$x $y string] [SUCH VAR]; put $x $y", stringValues("SUCH", "VAR")},
+	{"var [$x $y string] [SUCH VAR]; put $x $y", stringValues("SUCH", "VAR"), false},
 	{"var [$x $y string]; set [$x $y] [SUCH SET]; put $x $y",
-		stringValues("SUCH", "SET")},
+		stringValues("SUCH", "SET"), false},
+	{"var $x", nil, true},
+	{"var [$x string $y]", nil, true},
 
 	// Status capture
-	{"put ?(true|false|false)", stringValues("", "exited 1", "exited 1")},
+	{"put ?(true|false|false)", stringValues("", "exited 1", "exited 1"), false},
 }
 
 func TestEval(t *testing.T) {
@@ -102,12 +105,20 @@ func TestEval(t *testing.T) {
 
 		e := ev.Eval(name, tt.text, n)
 		close(out)
-		if e != nil {
-			t.Errorf("ev.Eval(*, %q, *) => %v, want <nil>", tt.text, e)
-		}
 		<-exhausted
-		if !reflect.DeepEqual(outs, tt.wanted) {
-			t.Errorf("Evalling %q outputs %v, want %v", tt.text, outs, tt.wanted)
+		if tt.wantError {
+			// Test for error, ignore output
+			if e == nil {
+				t.Errorf("ev.Eval(*, %q, *) => <nil>, want non-nil", tt.text)
+			}
+		} else {
+			// Test for output
+			if e != nil {
+				t.Errorf("ev.Eval(*, %q, *) => %v, want <nil>", tt.text, e)
+			}
+			if !reflect.DeepEqual(outs, tt.wanted) {
+				t.Errorf("Evalling %q outputs %v, want %v", tt.text, outs, tt.wanted)
+			}
 		}
 	}
 }
