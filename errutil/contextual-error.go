59d1cf249e4a9a2e39a7ff932a52b1b3ce4bae6d
Cheer Xiao <xiaqqaix@gmail.com>
2015-02-24 21:44:56 +0100

    golint fixes for errutil

--- errutil/contextual-error.go
+++ errutil/contextual-error.go
@@ -1,4 +1,5 @@
-// errutil provides an exception-like mechanism and the ContextualError type.
+// Package errutil provides an exception-like mechanism and the ContextualError
+// type.
 package errutil
 
 import (
@@ -9,6 +10,8 @@ import (
 	"github.com/elves/elvish/util"
 )
 
+// ContextualError is an error associated with a particular point in a source
+// text.
 type ContextualError struct {
 	srcname string
 	title   string
@@ -18,15 +21,21 @@ type ContextualError struct {
 	msg     string
 }
 
+// NewContextualError creates a new ContextualError.
 func NewContextualError(srcname, title, text string, pos int, format string, args ...interface{}) *ContextualError {
 	lineno, colno, line := util.FindContext(text, pos)
 	return &ContextualError{srcname, title, line, lineno, colno, fmt.Sprintf(format, args...)}
 }
 
+// Error implements the error interface. It returns a compact representation of
+// the error.
 func (e *ContextualError) Error() string {
 	return fmt.Sprintf("%s:%d:%d %s:%s", e.srcname, e.lineno, e.colno, e.title, e.msg)
 }
 
+// Pprint pretty-prints a ContextualError with the error position, the affected
+// line an arrow pointing to the point of error and the error message, with
+// colors.
 func (e *ContextualError) Pprint() string {
 	buf := new(bytes.Buffer)
 	// Position info
