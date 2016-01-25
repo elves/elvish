673c2438d2f0132da96966b738d19c4a11e9a76b
Qi Xiao <xiaqqaix@gmail.com>
2016-01-26 00:26:49 +0100

    eval: Factor out DontSearch

--- eval/search.go
+++ eval/search.go
@@ -2,6 +2,7 @@ package eval
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 
@@ -11,13 +12,11 @@ import (
 // Search tries to resolve an external command and return the full (possibly
 // relative) path.
 func (ev *Evaler) Search(exe string) (string, error) {
-	for _, p := range []string{"/", "./", "../"} {
-		if strings.HasPrefix(exe, p) {
-			if IsExecutable(exe) {
-				return exe, nil
-			}
-			return "", fmt.Errorf("external command %s not executable", parse.Quote(exe))
+	if DontSearch(exe) {
+		if IsExecutable(exe) {
+			return exe, nil
 		}
+		return "", fmt.Errorf("external command %s not executable", parse.Quote(exe))
 	}
 	for _, p := range ev.searchPaths {
 		full := p + "/" + exe
@@ -26,6 +25,28 @@ func (ev *Evaler) Search(exe string) (string, error) {
 		}
 	}
 	return "", fmt.Errorf("external command %s not found", parse.Quote(exe))
+}
+
+// AllExecutables writes the names of all executable files in the search path
+// to a channel.
+func (ev *Evaler) AllExecutables(names chan<- string) {
+	for _, dir := range ev.searchPaths {
+		// XXX Ignore error
+		infos, _ := ioutil.ReadDir(dir)
+		for _, info := range infos {
+			if !info.IsDir() && (info.Mode()&0111 != 0) {
+				names <- info.Name()
+			}
+		}
+	}
+}
+
+// DontSearch determines whether the path to an external command should be
+// taken literally and not searched.
+func DontSearch(exe string) bool {
+	return strings.HasPrefix(exe, "/") ||
+		strings.HasPrefix(exe, "./") ||
+		strings.HasPrefix(exe, "../")
 }
 
 // IsExecutable determines whether path refers to an executable file.
