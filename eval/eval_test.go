4c4313eeb09de06fe80f4154d40dd9517ad76d3b
Qi Xiao <xiaqqaix@gmail.com>
2016-01-28 21:30:32 +0100

    eval: test against pure channel pipeline

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -51,7 +51,8 @@ var evalTests = []struct {
 	// Pure byte pipeline
 	{`echo "Albert\nAllan\nAlbraham\nBerlin" | sed s/l/1/g | grep e`,
 		[]Value{}, more{wantBytesOut: []byte("A1bert\nBer1in\n")}},
-	// TODO: Add a useful channel pipeline sample
+	// Pure channel pipeline
+	{`put 233 42 19 | each [x]{+ $x 10}`, strs("243", "52", "29"), nomore},
 	// TODO: Add a useful hybrid pipeline sample
 
 	// Builtin functions
