31fb99daa76689e2a9ffa5df6fe260d13b5fd1b6
Qi Xiao <xiaqqaix@gmail.com>
2016-02-17 01:41:00 +0100

    Test against util.RootNames.

---
+++ util/rootnames_test.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"os/exec"
+	"reflect"
+	"sort"
+	"strings"
+	"testing"
+)
+
+func TestRootNames(t *testing.T) {
+	// NOTE: will fail if there are newlines in /*.
+	want, err := exec.Command("ls", "/").Output()
+	mustOK(err)
+	wantNames := strings.Split(strings.Trim(string(want), "\n"), "\n")
+	for i := range wantNames {
+		wantNames[i] = "/" + wantNames[i]
+	}
+
+	names := RootNames()
+
+	sort.Strings(wantNames)
+	sort.Strings(names)
+
+	if !reflect.DeepEqual(names, wantNames) {
+		t.Errorf("RootNames() -> %s, want %s", names, wantNames)
+	}
+}
