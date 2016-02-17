7cb9a9496ee4ba9c6629c35de0b6853b57840e21
Qi Xiao <xiaqqaix@gmail.com>
2016-02-17 01:27:51 +0100

    util: Test Getwd thoroughly.

---
+++ util/getwd_test.go
@@ -0,0 +1,47 @@
+package util
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestGetwd(t *testing.T) {
+	tmpdir, error := ioutil.TempDir("", "elvishtest.")
+	if error != nil {
+		t.Errorf("Got error when creating temp dir: %v", error)
+	} else {
+		os.Chdir(tmpdir)
+		// On some systems /tmp is a symlink.
+		tmpdir, error = filepath.EvalSymlinks(tmpdir)
+		if gotwd := Getwd(); gotwd != tmpdir || error != nil {
+			t.Errorf("Getwd() -> %v, want %v", gotwd, tmpdir)
+		}
+	}
+
+	// Override $HOME to trick GetHome.
+	os.Setenv("HOME", tmpdir)
+
+	if gotwd := Getwd(); gotwd != "~" {
+		t.Errorf("Getwd() -> %v, want ~", gotwd)
+	}
+
+	mustOK(os.Mkdir("a", 0700))
+	mustOK(os.Chdir("a"))
+	if gotwd := Getwd(); gotwd != "~/a" {
+		t.Errorf("Getwd() -> %v, want ~/a", gotwd)
+	}
+
+	mustOK(os.Remove(tmpdir + "/a"))
+	mustOK(os.Remove(tmpdir))
+	if gotwd := Getwd(); gotwd != "?" {
+		t.Errorf("Getwd() -> %v, want ?", gotwd)
+	}
+}
+
+func mustOK(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
