1f1e739ef21f8c63ebb997f935b3a4e34dbf2da1
Qi Xiao <xiaqqaix@gmail.com>
2016-02-21 16:20:01 +0100

    sys: Fix error reporting of Tcsetgrp.

--- sys/tc.go
+++ sys/tc.go
@@ -2,18 +2,23 @@ package sys
 
 /*
 #include <unistd.h>
+#include <errno.h>
 
 int f(int fd, pid_t pid) {
 	return tcsetpgrp(fd, pid);
+}
+
+int e() {
+	return errno;
 }
 */
 import "C"
 import "syscall"
 
 func Tcsetpgrp(fd int, pid int) error {
-	i := syscall.Errno(C.f(C.int(fd), C.pid_t(pid)))
+	i := C.f(C.int(fd), C.pid_t(pid))
 	if i != 0 {
-		return syscall.Errno(i)
+		return syscall.Errno(C.e())
 	}
 	return nil
 }
