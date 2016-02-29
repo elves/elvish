8819c9ebd45a51f0a707fc13cdd039e4c12c5ff9
Qi Xiao <xiaqqaix@gmail.com>
2016-02-29 19:49:21 +0100

    Fix test.

--- store/dir_test.go
+++ store/dir_test.go
@@ -7,12 +7,14 @@ import (
 
 var (
 	dirsToAdd  = []string{"/usr", "/usr/bin", "/usr"}
-	wantedDirs = []Dir{Dir{"/usr", 20}, Dir{"/usr/bin", 10}}
+	wantedDirs = []Dir{
+		Dir{"/usr", scoreIncrement*scoreDecay*scoreDecay + scoreIncrement},
+		Dir{"/usr/bin", scoreIncrement * scoreDecay}}
 )
 
 func TestDir(t *testing.T) {
 	for _, path := range dirsToAdd {
-		err := tStore.AddDir(path)
+		err := tStore.AddDir(path, 1)
 		if err != nil {
 			t.Errorf("tStore.AddDir(%q) => %v, want <nil>", path, err)
 		}
