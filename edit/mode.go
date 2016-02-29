1bdb21d865c1879d6bcbb7b0ff87faf25e699141
Qi Xiao <xiaqqaix@gmail.com>
2016-02-29 20:21:00 +0100

    Comments.

--- edit/mode.go
+++ edit/mode.go
@@ -3,6 +3,8 @@ package edit
 // Mode is an editor mode.
 type Mode interface {
 	Mode() ModeType
+	// ModeLine renders a mode line under the given width constraint. It
+	// returns a rendered buffer.
 	ModeLine(width int) *buffer
 }
 
@@ -20,5 +22,7 @@ const (
 
 // Lister is a mode with a listing.
 type Lister interface {
+	// List renders the listing under the given constraint of width and maximum
+	// height. It returns a rendered buffer.
 	List(width, maxHeight int) *buffer
 }
