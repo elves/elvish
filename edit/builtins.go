194b672b9c456aeb67c8e7f04ba0f948dcc22c95
Qi Xiao <xiaqqaix@gmail.com>
2016-03-14 21:34:09 +0100

    Use Tab to insert filename in navigation mode.

--- edit/builtins.go
+++ edit/builtins.go
@@ -143,7 +143,7 @@ var defaultBindings = map[ModeType]map[Key]string{
 		Key{Down, 0}:   "select-nav-down",
 		Key{Left, 0}:   "ascend-nav",
 		Key{Right, 0}:  "descend-nav",
-		Key{Enter, 0}:  "nav-insert-selected",
+		Key{Tab, 0}:    "nav-insert-selected",
 		Key{'H', Ctrl}: "trigger-nav-show-hidden",
 		Key{'[', Ctrl}: "start-insert",
 		Default:        "default-navigation",
