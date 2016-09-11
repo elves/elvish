b31e124a6793fb47fdfc4c8cfc79e7b57e196176
Qi Xiao <xiaqqaix@gmail.com>
2016-09-11 19:17:59 +0200

    Add a forgotten file.

---
+++ eval/editor.go
@@ -0,0 +1,11 @@
+package eval
+
+import "sync"
+
+// Editor is the interface that the line editor has to satisfy. It is needed so
+// that this package does not depend on the edit package.
+type Editor interface {
+	Active() bool
+	ActiveMutex() *sync.Mutex
+	Notify(string)
+}
