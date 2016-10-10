ada895c696487f371944a15cb76b106d297a6b39
Shengjing Zhu <zsj950618@gmail.com>
2016-10-10 22:15:06 +0800

    honor LS_COLORS env
    fix #237

--- edit/ls-colors.go
+++ edit/ls-colors.go
@@ -70,7 +70,17 @@ type lsColor struct {
 
 const defaultLsColorString = `rs=:di=01;34:ln=01;36:mh=:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.axv=01;35:*.anx=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=36:*.au=36:*.flac=36:*.mid=36:*.midi=36:*.mka=36:*.mp3=36:*.mpc=36:*.ogg=36:*.ra=36:*.wav=36:*.axa=36:*.oga=36:*.spx=36:*.xspf=36:`
 
-var defaultLsColor *lsColor
+func getLsColorString() string {
+	lsColorString := os.Getenv("LS_COLORS")
+	if len(lsColorString) == 0 {
+		return defaultLsColorString
+	}
+	return lsColorString
+}
+
+func getLsColor() *lsColor {
+	return parseLsColor(getLsColorString())
+}
 
 // parseLsColor parses a string in the LS_COLORS format into lsColor. Erroneous
 // fields are silently ignored.
@@ -82,6 +92,17 @@ func parseLsColor(s string) *lsColor {
 			continue
 		}
 		key, value := words[0], words[1]
+		filterValues := []string{}
+		for _, splitValue := range strings.Split(value, ";") {
+			if strings.Count(splitValue, "0") == len(splitValue) {
+				continue
+			}
+			filterValues = append(filterValues, splitValue)
+		}
+		if len(filterValues) == 0 {
+			continue
+		}
+		value = strings.Join(filterValues, ";")
 		if strings.HasPrefix(key, "*.") {
 			lc.styleForExt[key[2:]] = value
 		} else {
@@ -187,8 +208,4 @@ func (lc *lsColor) getStyle(fname string) string {
 		}
 	}
 	return lc.styleForFeature[feature]
-}
-
-func init() {
-	defaultLsColor = parseLsColor(defaultLsColorString)
 }
