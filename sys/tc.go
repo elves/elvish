81600693d5ef2dbc439fe2c0f44fa160b935905c
Qi Xiao <xiaqqaix@gmail.com>
2017-01-17 00:42:02 +0100

    Add sys.Tcgetpgrp.

--- sys/tc.go
+++ sys/tc.go
@@ -4,7 +4,11 @@ package sys
 #include <unistd.h>
 #include <errno.h>
 
-int f(int fd, pid_t pid) {
+pid_t get(int fd) {
+	return tcgetpgrp(fd);
+}
+
+int set(int fd, pid_t pid) {
 	return tcsetpgrp(fd, pid);
 }
 
@@ -15,8 +19,16 @@ int e() {
 import "C"
 import "syscall"
 
+func Tcgetpgrp(fd int) (int, error) {
+	i := C.get(C.int(fd))
+	if i == -1 {
+		return -1, syscall.Errno(C.e())
+	}
+	return int(i), nil
+}
+
 func Tcsetpgrp(fd int, pid int) error {
-	i := C.f(C.int(fd), C.pid_t(pid))
+	i := C.set(C.int(fd), C.pid_t(pid))
 	if i != 0 {
 		return syscall.Errno(C.e())
 	}
