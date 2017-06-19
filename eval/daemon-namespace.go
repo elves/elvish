9b822111898839975ca72f5ecf5a6b5c81d75fce
Qi Xiao <xiaqqaix@gmail.com>
2017-06-19 16:15:12 +0200

    Guard against access to $daemon:pid when daemon client is nil.

--- eval/daemon-namespace.go
+++ eval/daemon-namespace.go
@@ -1,15 +1,21 @@
 package eval
 
 import (
+	"errors"
 	"net/rpc"
 	"strconv"
 
 	"github.com/elves/elvish/daemon/api"
 )
 
+var ErrDaemonOffline = errors.New("daemon is offline")
+
 func makeDaemonNamespace(daemon *rpc.Client) Namespace {
 	// Obtain process ID
 	daemonPid := func() Value {
+		if daemon == nil {
+			throw(ErrDaemonOffline)
+		}
 		req := &api.PidRequest{}
 		res := &api.PidResponse{}
 		err := daemon.Call(api.ServiceName+".Pid", req, res)
