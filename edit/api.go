c4c465de4a8b075bf5babd4a99808edd435efc63
xofyarg <xofyarg@users.noreply.github.com>
2017-06-23 10:17:00 +0100

    Expose bindings for loc and bang mode (#365)
    Add keys of those two modes into default readline bindings.

--- edit/api.go
+++ edit/api.go
@@ -60,7 +60,8 @@ func installModules(modules map[string]eval.Namespace, ed *Editor) {
 	// Populate binding tables in the variable $binding.
 	// TODO Make binding specific to the Editor.
 	binding := &eval.Struct{
-		[]string{"insert", "command", "completion", "navigation", "history", "histlist"},
+		[]string{"insert", "command", "completion", "navigation", "history",
+			"histlist", "loc", "bang"},
 		[]eval.Variable{
 			eval.NewRoVariable(BindingTable{keyBindings[modeInsert]}),
 			eval.NewRoVariable(BindingTable{keyBindings[modeCommand]}),
@@ -68,6 +69,8 @@ func installModules(modules map[string]eval.Namespace, ed *Editor) {
 			eval.NewRoVariable(BindingTable{keyBindings[modeNavigation]}),
 			eval.NewRoVariable(BindingTable{keyBindings[modeHistory]}),
 			eval.NewRoVariable(BindingTable{keyBindings[modeHistoryListing]}),
+			eval.NewRoVariable(BindingTable{keyBindings[modeLocation]}),
+			eval.NewRoVariable(BindingTable{keyBindings[modeBang]}),
 		},
 	}
 	ns["binding"] = eval.NewRoVariable(binding)
