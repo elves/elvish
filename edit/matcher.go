1d8772bc34ebb9a9e023cfae96c3720ca71f7bfe
Qi Xiao <xiaqqaix@gmail.com>
2017-07-13 11:38:41 +0100

    edit: Add edit:match-subseq.

--- edit/matcher.go
+++ edit/matcher.go
@@ -5,39 +5,52 @@ import (
 	"strings"
 
 	"github.com/elves/elvish/eval"
+	"github.com/elves/elvish/util"
 )
 
 var (
-	errIncorrectNumOfResults = errors.New("matcher must return a bool for each candidate")
-	errMatcherMustBeFn       = errors.New("matcher must be a function")
+	errIncorrectNumOfResults    = errors.New("matcher must return a bool for each candidate")
+	errMatcherMustBeFn          = errors.New("matcher must be a function")
+	errMatcherInputMustBeString = errors.New("matcher input must be string")
 )
 
-func matchPrefix(ec *eval.EvalCtx,
-	args []eval.Value, opts map[string]eval.Value) {
-
-	var pattern eval.String
-	iterate := eval.ScanArgsAndOptionalIterate(ec, args, &pattern)
-	eval.TakeNoOpt(opts)
-
-	out := ec.OutputChan()
-	iterate(func(cand eval.Value) {
-		candidate := eval.ToString(cand)
-		out <- eval.Bool(strings.HasPrefix(candidate, string(pattern)))
-	})
-}
-
 var (
-	matchPrefixFn = &eval.BuiltinFn{"match-prefix", matchPrefix}
-	matchers      = []*eval.BuiltinFn{matchPrefixFn}
+	matchPrefix = &eval.BuiltinFn{
+		"match-prefix", wrapMatcher(strings.HasPrefix)}
+	matchSubseq = &eval.BuiltinFn{
+		"match-subseq", wrapMatcher(util.HasSubseq)}
+	matchers = []*eval.BuiltinFn{
+		matchPrefix,
+		matchSubseq,
+	}
 
 	_ = registerVariable("-matcher", func() eval.Variable {
 		m := map[eval.Value]eval.Value{
-			eval.String("index"):    matchPrefixFn,
-			eval.String("redirect"): matchPrefixFn,
-			eval.String("argument"): matchPrefixFn,
-			eval.String("variable"): matchPrefixFn,
-			eval.String("command"):  matchPrefixFn,
+			eval.String("index"):    matchPrefix,
+			eval.String("redirect"): matchPrefix,
+			eval.String("argument"): matchPrefix,
+			eval.String("variable"): matchPrefix,
+			eval.String("command"):  matchPrefix,
 		}
 		return eval.NewPtrVariableWithValidator(eval.NewMap(m), eval.ShouldBeMap)
 	})
 )
+
+func wrapMatcher(match func(s, p string) bool) eval.BuiltinFnImpl {
+	return func(ec *eval.EvalCtx,
+		args []eval.Value, opts map[string]eval.Value) {
+
+		var pattern eval.String
+		iterate := eval.ScanArgsAndOptionalIterate(ec, args, &pattern)
+		eval.TakeNoOpt(opts)
+
+		out := ec.OutputChan()
+		iterate(func(v eval.Value) {
+			s, ok := v.(eval.String)
+			if !ok {
+				throw(errMatcherInputMustBeString)
+			}
+			out <- eval.Bool(match(string(s), string(pattern)))
+		})
+	}
+}
