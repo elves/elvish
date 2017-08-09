de1a2b7d9b23282b0ab2a2d5d1c8f6fae9f864e1
Diego Zamboni <diego@zzamboni.org>
2017-08-09 18:59:36 +0200

    Updates and fixes to narrow.elv
    - Updated to account for recent syntax fixes.
    - Added hook capabilities to all three modes, documented in narrow.elv
    - Updated embedded module.
    - Added bind_keys function to set key bindings

--- eval/embedded_modules.go
+++ eval/embedded_modules.go
@@ -1,84 +1,101 @@
 package eval
 
 var embeddedModules = map[string]string{
-	"narrow": `fn location {
-    candidates = [(dir-history | each {
-	score = (splits $0[score] &sep=. | take 1)
-        put [
-            &content=$0[path]
-            &display=$score" "$0[path]
-	]
-    })]
+	"narrow": `before-location = []
+after-location = []
+before-history = []
+after-history = []
+before-lastcmd = []
+after-lastcmd = []
 
-    edit:-narrow-read {
-        put $@candidates
-    } {
-        cd $0[content]
-    } &modeline="Location "
+fn location {
+  for hook $before-location { $hook }
+  candidates = [(dir-history | each [arg]{
+	      score = (splits . $arg[score] | take 1)
+        put [
+          &content=$arg[path]
+          &display=$score" "$arg[path]
+	      ]
+  })]
+
+  edit:-narrow-read {
+    put $@candidates
+  } [arg]{
+    cd $arg[content]
+    for hook $after-location { $hook }
+  } &modeline="[narrow] Location " &ignore-case=$true
 }
 
 fn history {
-    candidates = [(edit:command-history | each {
+  for hook $before-history { $hook }
+  candidates = [(edit:command-history | each [arg]{
         put [
-	    &content=$0[cmd]
-	    &display=$0[id]" "$0[cmd]
+	        &content=$arg[cmd]
+	        &display=$arg[id]" "(replaces "\t" " " (replaces "\n" " " $arg[cmd]))
         ]
-    })]
+  })]
 
-    edit:-narrow-read {
-        put $@candidates
-    } {
-        edit:replace-input $0[content]
-    } &modeline="History " &keep-bottom=$true
+  edit:-narrow-read {
+    put $@candidates
+  } [arg]{
+    edit:replace-input $arg[content]
+    for hook $after-history { $hook }
+  } &modeline="[narrow] History " &keep-bottom=$true &ignore-case=$true
 }
 
 fn lastcmd {
-    last = (edit:command-history -1)
-    cmd = [
-            &content=$last[cmd]
-            &display="-1 "$last[cmd]
-	    &filter-text=""
-        ]
-    index = 0
-    candidates = [$cmd ( edit:wordify $last[cmd] | each {
-	put [
-            &content=$0
-            &display=$index" "$0
-            &filter-text=$index
-	]
-	index = (+ $index 1)
-    })]
-    edit:-narrow-read {
-        put $@candidates
-    } {
-        edit:replace-input $0[content]
-    } &modeline="Lastcmd " &auto-commit=$true &bindings=[&M-1={ edit:narrow:accept-close }]
+  for hook $before-lastcmd { $hook }
+  last = (edit:command-history -1)
+  cmd = [
+    &content=$last[cmd]
+    &display="M-1 "$last[cmd]
+	  &filter-text=""
+  ]
+  index = 0
+  candidates = [$cmd ( edit:wordify $last[cmd] | each [arg]{
+	      put [
+          &content=$arg
+          &display=$index" "$arg
+          &filter-text=$index
+	      ]
+	      index = (+ $index 1)
+  })]
+  edit:-narrow-read {
+    put $@candidates
+  } [arg]{
+    edit:insert-at-dot $arg[content]
+    for hook $after-lastcmd { $hook }
+  } &modeline="[narrow] Lastcmd " &auto-commit=$true &bindings=[&M-1={ edit:narrow:accept-close }] &ignore-case=$true
 }
 
 
-# TODO: separate bindings from functions
-
-fn bind [m k f]{
-    edit:binding[$m][$k] = $f
+fn -bind_i [k f]{
+  edit:insert:binding[$k] = $f
 }
 
-bind insert C-l       narrow:location
-bind insert C-r       narrow:history
-bind insert M-1       narrow:lastcmd
+fn -bind_n [k f]{
+  edit:narrow:binding[$k] = $f
+}
 
-bind narrow Up        $edit:narrow:&up
-bind narrow PageUp    $edit:narrow:&page-up
-bind narrow Down      $edit:narrow:&down
-bind narrow PageDown  $edit:narrow:&page-down
-bind narrow Tab       $edit:narrow:&down-cycle
-bind narrow S-Tab     $edit:narrow:&up-cycle
-bind narrow Backspace $edit:narrow:&backspace
-bind narrow Enter     $edit:narrow:&accept-close
-bind narrow M-Enter   $edit:narrow:&accept
-bind narrow default   $edit:narrow:&default
-bind narrow "C-["     $edit:insert:&start
-bind narrow C-G       $edit:narrow:&toggle-ignore-case
-bind narrow C-D       $edit:narrow:&toggle-ignore-duplication
+fn bind_keys [&location=C-l &history=C-r &lastcmd=M-1]{
+  if (> (count $location) 0) { -bind_i $location narrow:location }
+  if (> (count $history) 0)  { -bind_i $history  narrow:history }
+  if (> (count $lastcmd) 0)  { -bind_i $lastcmd  narrow:lastcmd }
+}
+
+-bind_n Up        $edit:narrow:&up
+-bind_n PageUp    $edit:narrow:&page-up
+-bind_n Down      $edit:narrow:&down
+-bind_n PageDown  $edit:narrow:&page-down
+-bind_n Tab       $edit:narrow:&down-cycle
+-bind_n S-Tab     $edit:narrow:&up-cycle
+-bind_n Backspace $edit:narrow:&backspace
+-bind_n Enter     $edit:narrow:&accept-close
+-bind_n M-Enter   $edit:narrow:&accept
+-bind_n default   $edit:narrow:&default
+-bind_n "C-["     $edit:insert:&start
+-bind_n C-G       $edit:narrow:&toggle-ignore-case
+-bind_n C-D       $edit:narrow:&toggle-ignore-duplication
 `,
 	"readline-binding": `binding = [&]
 
