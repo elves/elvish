3bad68c111172899dda39e70274340f65c893693
Qi Xiao <xiaqqaix@gmail.com>
2017-08-17 17:19:51 +0200

    hashmap: Fix Key implementations.

--- hashmap/key.go
+++ hashmap/key.go
@@ -9,6 +9,20 @@ type Key interface {
 	Equal(other interface{}) bool
 }
 
+// UInt32 is a uint32 that can be used as a Key.
+type UInt32 uint32
+
+var _ Key = UInt32(0)
+
+func (i UInt32) Hash() uint32 {
+	return uint32(i)
+}
+
+func (i UInt32) Equal(other interface{}) bool {
+	j, ok := other.(UInt32)
+	return ok && i == j
+}
+
 // UInt64 is a uint64 that can be used as a Key.
 type UInt64 uint64
 
@@ -16,7 +30,7 @@ var _ Key = UInt64(0)
 
 // Hash returns the hash code of an UInt64.
 func (i UInt64) Hash() uint32 {
-	return uint32(i>>32)*33 + uint32(i & 0xffffffff))
+	return uint32(i>>32)*33 + uint32(i&0xffffffff)
 }
 
 // Equal returns whether a Int64 is equal to another value.
@@ -34,7 +48,7 @@ var _ Key = String("")
 func (s String) Hash() uint32 {
 	h := uint32(5381)
 	for i := 0; i < len(s); i++ {
-		h = ((h << 5) + h) * uint32(s[i])
+		h = ((h << 5) + h) + uint32(s[i])
 	}
 	return h
 }
