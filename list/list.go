6019758bdeebb8bef8fa099bc96eb4f44c8e4579
Qi Xiao <xiaqqaix@gmail.com>
2017-08-28 05:04:36 +0200

    Make list.List an interface.

--- list/list.go
+++ list/list.go
@@ -1,32 +1,75 @@
 // Package list implements persistent list.
 package list
 
-// List implements a persistent list. The empty value is a valid empty list.
-type List struct {
+import "github.com/xiaq/persistent/types"
+
+// List is a persistent list.
+type List interface {
+	types.Equaler
+	// Len returns the number of values in the list.
+	Len() int
+	// Cons returns a new list with an additional value in the front.
+	Cons(interface{}) List
+	// First returns the first value in the list.
+	First() interface{}
+	// Rest returns the list after the first value.
+	Rest() List
+}
+
+// Empty is an empty list.
+var Empty List = &list{}
+
+type list struct {
 	first interface{}
-	rest  *List
+	rest  *list
 	count int
 }
 
-// Empty is an empty List.
-var Empty = &List{}
-
-// Cons returns a new List with an additional value in the front.
-func (l *List) Cons(val interface{}) *List {
-	return &List{val, l, l.count + 1}
+func (l *list) Len() int {
+	return l.count
 }
 
-// First returns the first value in the list.
-func (l *List) First() interface{} {
+func (l *list) Cons(val interface{}) List {
+	return &list{val, l, l.count + 1}
+}
+
+func (l *list) First() interface{} {
 	return l.first
 }
 
-// Rest returns the list after the first value.
-func (l *List) Rest() *List {
+func (l *list) Rest() List {
 	return l.rest
 }
 
-// Count returns the number of values in the list.
-func (l *List) Count() int {
-	return l.count
+func (l *list) Equal(other interface{}) bool {
+	l2, ok := other.(List)
+	if !ok {
+		return false
+	}
+	return Equal(l, l2)
+}
+
+// Equal returns whether two List values are equal to each other. The values are
+// compared using the types.Equaler interface if the value in l1 implements
+// types.Equaler, or with == otherwise.
+func Equal(l1, l2 List) bool {
+	if l1.Len() != l2.Len() {
+		return false
+	}
+	for i := 0; i < l1.Len(); i++ {
+		v1 := l1.First()
+		v2 := l2.First()
+		if v1eq, ok := v1.(types.Equaler); ok {
+			if !v1eq.Equal(v2) {
+				return false
+			}
+		} else {
+			if v1 != v2 {
+				return false
+			}
+		}
+		l1 = l1.Rest()
+		l2 = l2.Rest()
+	}
+	return true
 }
