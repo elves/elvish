00ed6923f7c80857ccd4a21f9044290062a4fc12
Qi Xiao <xiaqqaix@gmail.com>
2017-11-30 17:17:25 +0100

    sys: Fix DumpStack to return the correct part of the buffer.

--- sys/dumpstack.go
+++ sys/dumpstack.go
@@ -2,10 +2,15 @@ package sys
 
 import "runtime"
 
+const dumpStackBufSizeInit = 4096
+
 func DumpStack() string {
-	buf := make([]byte, 1024)
-	for runtime.Stack(buf, true) == cap(buf) {
+	buf := make([]byte, dumpStackBufSizeInit)
+	for {
+		n := runtime.Stack(buf, true)
+		if n < cap(buf) {
+			return string(buf[:n])
+		}
 		buf = make([]byte, cap(buf)*2)
 	}
-	return string(buf)
 }
