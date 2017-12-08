457e2e76263206369c7c60dbcf3461e70b253f92
Qi Xiao <xiaqqaix@gmail.com>
2017-12-08 00:52:21 +0000

    util: Make test pass on Windows.

--- util/fullnames_windows_test.go
+++ util/fullnames_windows_test.go
@@ -18,6 +18,18 @@ func ls(dir string) []string {
 	for i := range names {
 		names[i] = dir + names[i]
 	}
-	sort.Strings(names)
-	return names
+	// Remove filenames that start with ".".
+	// XXX: This behavior only serves to make current behavior of FullNames,
+	// which always treat dotfiles as hidden, legal; the validness of this
+	// behavior is quetionable. However, since FullNames is also depended by the
+	// glob package for testing, changing FullNames requires changing the
+	// behavior of globbing as well.
+	filtered := make([]string, 0, len(names))
+	for _, name := range names {
+		if !strings.HasPrefix(name, dir+".") {
+			filtered = append(filtered, name)
+		}
+	}
+	sort.Strings(filtered)
+	return filtered
 }
