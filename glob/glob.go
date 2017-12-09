49f7f8ecf5a819e6cead802badfff07fe9f15c5e
Qi Xiao <xiaqqaix@gmail.com>
2017-12-09 02:47:07 +0000

    glob: Make code more compact and add comments.

--- glob/glob.go
+++ glob/glob.go
@@ -16,13 +16,16 @@ func Glob(p string, cb func(string) bool) bool {
 func (p Pattern) Glob(cb func(string) bool) bool {
 	segs := p.Segments
 	dir := ""
-	if len(segs) > 0 && IsSlash(segs[0]) {
-		segs = segs[1:]
-		dir = "/"
-	}
 
+	// XXX: This is a hack solely for supporting globs that start with ~ in the
+	// eval package.
 	if p.DirOverride != "" {
 		dir = p.DirOverride
+	}
+
+	if len(segs) > 0 && IsSlash(segs[0]) {
+		segs = segs[1:]
+		dir += "/"
 	}
 
 	return glob(segs, dir, cb)
@@ -32,58 +35,37 @@ func (p Pattern) Glob(cb func(string) bool) bool {
 // calls the callback on all of them. If the callback returns false, globbing is
 // interrupted, and glob returns false. Otherwise it returns true.
 func glob(segs []Segment, dir string, cb func(string) bool) bool {
-	// Consume the non-wildcard prefix. This may seem like an optimization, but
-	// is actually required for "." and ".." to be used as path elements, as
-	// they do not appear in the result of ReadDir.
-	for len(segs) > 0 && IsLiteral(segs[0]) {
-		seg0 := segs[0].(Literal).Data
-		var path string
-		switch dir {
-		case "":
-			path = seg0
-		case "/":
-			path = "/" + seg0
-		default:
-			path = dir + "/" + seg0
-		}
-		if len(segs) == 1 {
-			// A lone literal. Generate it if the named file exists, and return.
-			if _, err := os.Stat(path); err == nil {
-				return cb(path)
-			}
+	// Consume non-wildcard path elements simply by following the path. This may
+	// seem like an optimization, but is actually required for "." and ".." to
+	// be used as path elements, as they do not appear in the result of ReadDir.
+	for len(segs) > 1 && IsLiteral(segs[0]) && IsSlash(segs[1]) {
+		elem := segs[0].(Literal).Data
+		segs = segs[2:]
+		dir += elem + "/"
+		if info, err := os.Stat(dir); err != nil || !info.IsDir() {
 			return true
-		} else if IsSlash(segs[1]) {
-			// A lone literal followed by a slash. Change the directory if it
-			// exists, otherwise return.
-			if info, err := os.Stat(path); err == nil && info.IsDir() {
-				dir = path
-			} else {
-				return true
-			}
-			segs = segs[2:]
-		} else {
-			break
 		}
 	}
 
-	// Empty segment, resulting from a trailing slash. Generate the starting
-	// directory.
 	if len(segs) == 0 {
-		return cb(dir + "/")
+		return cb(dir)
+	} else if len(segs) == 1 && IsLiteral(segs[0]) {
+		path := dir + segs[0].(Literal).Data
+		if _, err := os.Stat(path); err == nil {
+			return cb(path)
+		}
+		return true
 	}
 
-	var prefix string
-	if dir == "" {
-		prefix = ""
-		dir = "."
-	} else if dir == "/" {
-		prefix = "/"
-	} else {
-		// dir never has a trailing slash unless it is /.
-		prefix = dir + "/"
+	infos, err := readDir(dir)
+	if err != nil {
+		// XXX Silently drop the error
+		return true
 	}
 
 	i := -1
+	// nexti moves i to the next index in segs that is either / or ** (in other
+	// words, something that matches /).
 	nexti := func() {
 		for i++; i < len(segs); i++ {
 			if IsSlash(segs[i]) || IsWild1(segs[i], StarStar) {
@@ -93,13 +75,26 @@ func glob(segs []Segment, dir string, cb func(string) bool) bool {
 	}
 	nexti()
 
-	infos, err := ioutil.ReadDir(dir)
-	if err != nil {
-		// XXX Silently drop the error
-		return true
-	}
-
-	// Enumerate the position of the first slash.
+	// Enumerate the position of the first slash. In the presense of multiple
+	// **'s in the pattern, the first slash may be in any of those.
+	//
+	// For instance, in x**y**z, the first slash may be in the first ** or the
+	// second:
+	// 1) If it is in the first, then pattern is equivalent to x*/**y**z. We
+	//    match directories with x* and recurse in each subdirectory with the
+	//    pattern **y**z.
+	// 2) If it is the in the second, we know that since the first ** can no
+	//    longer contain any slashes, we treat it as * (this is done in
+	//    matchElement). The pattern is now equivalent to x*y*/**z. We match
+	//    directories with x*y* and recurse in each subdirectory with the
+	//    pattern **z.
+	//
+	// The rules are:
+	// 1) For each **, we treat it as */** and all previous ones as *. We match
+	//    subdirectories with the part before /, and recurse in subdirectories
+	//    with the pattern after /.
+	// 2) If a literal / is encountered, we return after recursing in the
+	//    subdirectories.
 	for i < len(segs) {
 		slash := IsSlash(segs[i])
 		var first, rest []Segment
@@ -114,7 +109,7 @@ func glob(segs []Segment, dir string, cb func(string) bool) bool {
 		for _, info := range infos {
 			name := info.Name()
 			if matchElement(first, name) && info.IsDir() {
-				if !glob(rest, prefix+name, cb) {
+				if !glob(rest, dir+name+"/", cb) {
 					return false
 				}
 			}
@@ -127,16 +122,26 @@ func glob(segs []Segment, dir string, cb func(string) bool) bool {
 		nexti()
 	}
 
-	// If we reach here, it is possible to have no slashes at all.
+	// If we reach here, it is possible to have no slashes at all. Simply match
+	// the entire pattern with all files.
 	for _, info := range infos {
 		name := info.Name()
 		if matchElement(segs, name) {
-			if !cb(prefix + name) {
+			if !cb(dir + name) {
 				return false
 			}
 		}
 	}
 	return true
+}
+
+// readDir is just like ioutil.ReadDir except that it treats an argument of ""
+// as ".".
+func readDir(dir string) ([]os.FileInfo, error) {
+	if dir == "" {
+		dir = "."
+	}
+	return ioutil.ReadDir(dir)
 }
 
 // matchElement matches a path element against segments, which may not contain
