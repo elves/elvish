9d8933c901915a9a8549a6d852cf8f28c8b026d9
Qi Xiao <xiaqqaix@gmail.com>
2017-12-13 01:08:15 +0000

    sys: WaitForRead is only for Unix.

--- sys/waitforread_unix.go
+++ sys/waitforread_unix.go
@@ -4,7 +4,13 @@ package sys
 
 import "os"
 
-func waitForRead(files ...*os.File) (ready []bool, err error) {
+// WaitForRead blocks until any of the given files is ready to be read. It
+// returns a boolean array indicating which files are ready to be read and
+// possible errors.
+//
+// It is implemented with select(2) on Unix and WaitForMultipleObjects on
+// Windows.
+func WaitForRead(files ...*os.File) (ready []bool, err error) {
 	maxfd := 0
 	fdset := NewFdSet()
 	for _, file := range files {
