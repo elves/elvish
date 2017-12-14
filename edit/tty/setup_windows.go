3ec5654d38a1a72d2caf473e7a0e93e7284036fa
Qi Xiao <xiaqqaix@gmail.com>
2017-12-14 01:34:45 +0000

    edit/tty: Implement Setup for Windows.

--- edit/tty/setup_windows.go
+++ edit/tty/setup_windows.go
@@ -1,7 +1,27 @@
 package tty
 
-import "os"
+import (
+	"os"
+
+	"golang.org/x/sys/windows"
+)
+
+const wantedMode = windows.ENABLE_WINDOW_INPUT |
+	windows.ENABLE_MOUSE_INPUT | windows.ENABLE_PROCESSED_INPUT
 
 func setup(file *os.File) (func() error, error) {
-	panic("not implemented")
+	handle := windows.Handle(file.Fd())
+
+	var oldMode uint32
+	err := windows.GetConsoleMode(handle, &oldMode)
+	if err != nil {
+		return nil, err
+	}
+	err = windows.SetConsoleMode(handle, wantedMode)
+	if err != nil {
+		return nil, err
+	}
+	return func() error {
+		return windows.SetConsoleMode(handle, oldMode)
+	}, nil
 }
