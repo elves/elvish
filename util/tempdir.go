914da1e010e9e6193b8095c30fb0171d52b7d7c2
Qi Xiao <xiaqqaix@gmail.com>
2017-12-17 04:16:12 +0000

    util: Add WithTempDirs; add test against tempdir.go and fix a bug.

--- util/tempdir.go
+++ util/tempdir.go
@@ -1,41 +1,70 @@
 package util
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 )
 
-// WithTempDir creates a new temporary directory and runs a function, passing
-// the path of the temporary directory. After the function returns, it removes
-// the temporary directory. It panics if it cannot make a temporary directory or
-// cd into it.
+// WithTempDirs creates a requested number of temporary directories and runs a
+// function, passing the paths of the temporary directories. After the function
+// returns, it removes the temporary directories. It panics if it cannot make a
+// temporary directory, and prints an error message to stderr if it cannot
+// remove the temporary directories.
 //
 // It is useful in tests.
-func WithTempDir(f func(string)) {
-	tmpdir, err := ioutil.TempDir("", "elvishtest.")
-	if err != nil {
-		panic(err)
+func WithTempDirs(n int, f func([]string)) {
+	tmpdirs := make([]string, n)
+	for i := range tmpdirs {
+		var err error
+		tmpdirs[i], err = ioutil.TempDir("", "elvishtest.")
+		if err != nil {
+			panic(err)
+		}
 	}
-	defer os.RemoveAll(tmpdir)
+	defer func() {
+		for _, tmpdir := range tmpdirs {
+			err := os.RemoveAll(tmpdir)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Warning: failed to remove temp dir", tmpdir)
+			}
+		}
+	}()
 
-	f(tmpdir)
+	f(tmpdirs)
+}
+
+// WithTempDir is like with WithTempDirs, except that it always creates one
+// temporary directory and pass the function a string instead of []string.
+func WithTempDir(f func(string)) {
+	WithTempDirs(1, func(s []string) {
+		f(s[0])
+	})
 }
 
 // InTempDir is like WithTempDir, but also cd into the directory before running
 // the function, and cd backs after running the function if possible.
 //
+// It panics if it could not get the working directory or change directory.
+//
 // It is useful in tests.
 func InTempDir(f func(string)) {
 	WithTempDir(func(tmpdir string) {
-		pwd, err := os.Getwd()
-		if err != nil {
-			defer os.Chdir(pwd)
-		}
-
-		err = os.Chdir(tmpdir)
+		oldpwd, err := os.Getwd()
 		if err != nil {
 			panic(err)
 		}
+
+		mustChdir(tmpdir)
+		defer mustChdir(oldpwd)
+
 		f(tmpdir)
 	})
+}
+
+func mustChdir(dir string) {
+	err := os.Chdir(dir)
+	if err != nil {
+		panic(err)
+	}
 }
