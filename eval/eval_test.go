b60d72934ff8940dbc6bdf22d64ad0b93e899fa6
Qi Xiao <xiaqqaix@gmail.com>
2017-12-20 18:09:46 +0000

    eval: Move test utility into a non-test file.

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -1,15 +1,12 @@
 package eval
 
 import (
-	"errors"
-	"os"
 	"reflect"
 	"strconv"
 	"syscall"
 	"testing"
 
 	"github.com/elves/elvish/daemon/api"
-	"github.com/elves/elvish/parse"
 )
 
 func TestBuiltinPid(t *testing.T) {
@@ -20,52 +17,12 @@ func TestBuiltinPid(t *testing.T) {
 	}
 }
 
-type evalTest struct {
-	text string
-	want
-}
-
-type want struct {
-	out      []Value
-	bytesOut []byte
-	err      error
-}
-
-// A special value for want.err to indicate that any error, as long as not nil,
-// is OK
-var errAny = errors.New("")
-
-var (
-	wantNothing = want{}
-	wantTrue    = want{out: bools(true)}
-	wantFalse   = want{out: bools(false)}
-)
-
-// Shorthands for values in want.out
-
-func strs(ss ...string) []Value {
-	vs := make([]Value, len(ss))
-	for i, s := range ss {
-		vs[i] = String(s)
-	}
-	return vs
-}
-
-func bools(bs ...bool) []Value {
-	vs := make([]Value, len(bs))
-	for i, b := range bs {
-		vs[i] = Bool(b)
-	}
-	return vs
-}
-
 // To be called from init in separate test files.
 func addToEvalTests(tests []evalTest) {
 	evalTests = append(evalTests, tests...)
 }
 
 var evalTests = []evalTest{
-
 	// Pseudo-namespaces local: and up:
 	{"x=lorem; []{local:x=ipsum; put $up:x $local:x}",
 		want{out: strs("lorem", "ipsum")}},
@@ -76,48 +33,13 @@ var evalTests = []evalTest{
 	{"del E:FOO; put $E:FOO", want{out: strs("")}},
 }
 
-func mustParseAndCompile(t *testing.T, ev *Evaler, name, text string) Op {
-	n, err := parse.Parse(name, text)
-	if err != nil {
-		t.Fatalf("Parse(%q) error: %s", text, err)
-	}
-	op, err := ev.Compile(n, name, text)
-	if err != nil {
-		t.Fatalf("Compile(Parse(%q)) error: %s", text, err)
-	}
-	return op
-}
-
 func TestEval(t *testing.T) {
-	for _, tt := range evalTests {
-		// fmt.Printf("eval %q\n", tt.text)
-
-		out, bytesOut, err := evalAndCollect(t, []string{tt.text}, len(tt.want.out))
-
-		first := true
-		errorf := func(format string, args ...interface{}) {
-			if first {
-				first = false
-				t.Errorf("eval(%q) fails:", tt.text)
-			}
-			t.Errorf("  "+format, args...)
-		}
-
-		if !matchOut(tt.want.out, out) {
-			errorf("got out=%v, want %v", out, tt.want.out)
-		}
-		if string(tt.want.bytesOut) != string(bytesOut) {
-			errorf("got bytesOut=%q, want %q", bytesOut, tt.want.bytesOut)
-		}
-		if !matchErr(tt.want.err, err) {
-			errorf("got err=%v, want %v", err, tt.want.err)
-		}
-	}
+	testEval(t, dataDir, evalTests)
 }
 
 func TestMultipleEval(t *testing.T) {
 	texts := []string{"x=hello", "put $x"}
-	outs, _, err := evalAndCollect(t, texts, 1)
+	outs, _, err := evalAndCollect(t, dataDir, texts, 1)
 	wanted := strs("hello")
 	if err != nil {
 		t.Errorf("eval %s => %v, want nil", texts, err)
@@ -125,76 +47,6 @@ func TestMultipleEval(t *testing.T) {
 	if !reflect.DeepEqual(outs, wanted) {
 		t.Errorf("eval %s outputs %v, want %v", texts, outs, wanted)
 	}
-}
-
-func evalAndCollect(t *testing.T, texts []string, chsize int) ([]Value, []byte, error) {
-	name := "<eval test>"
-	ev := NewEvaler(api.NewClient("/invalid"), nil, dataDir, nil)
-
-	// Collect byte output
-	bytesOut := []byte{}
-	pr, pw, _ := os.Pipe()
-	bytesDone := make(chan struct{})
-	go func() {
-		for {
-			var buf [64]byte
-			nr, err := pr.Read(buf[:])
-			bytesOut = append(bytesOut, buf[:nr]...)
-			if err != nil {
-				break
-			}
-		}
-		close(bytesDone)
-	}()
-
-	// Channel output
-	outs := []Value{}
-
-	// Eval error. Only that of the last text is saved.
-	var ex error
-
-	for _, text := range texts {
-		op := mustParseAndCompile(t, ev, name, text)
-
-		outCh := make(chan Value, chsize)
-		outDone := make(chan struct{})
-		go func() {
-			for v := range outCh {
-				outs = append(outs, v)
-			}
-			close(outDone)
-		}()
-
-		ports := []*Port{
-			{File: os.Stdin, Chan: ClosedChan},
-			{File: pw, Chan: outCh},
-			{File: os.Stderr, Chan: BlackholeChan},
-		}
-
-		ex = ev.eval(op, ports, name, text)
-		close(outCh)
-		<-outDone
-	}
-
-	pw.Close()
-	<-bytesDone
-	pr.Close()
-
-	return outs, bytesOut, ex
-}
-
-func matchOut(want, got []Value) bool {
-	if len(got) == 0 && len(want) == 0 {
-		return true
-	}
-	return reflect.DeepEqual(got, want)
-}
-
-func matchErr(want, got error) bool {
-	if got == nil {
-		return want == nil
-	}
-	return want == errAny || reflect.DeepEqual(got.(*Exception).Cause, want)
 }
 
 func BenchmarkOutputCaptureOverhead(b *testing.B) {
---
+++ eval/testutils.go
@@ -0,0 +1,187 @@
+// Common testing utilities. This file does not file a _test.go suffix so that
+// it can be used from other packages that also want to test the modules they
+// implement (e.g. edit: and re:).
+
+package eval
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/elves/elvish/daemon/api"
+	"github.com/elves/elvish/parse"
+)
+
+type evalTest struct {
+	text string
+	want
+}
+
+type want struct {
+	out      []Value
+	bytesOut []byte
+	err      error
+}
+
+// A special value for want.err to indicate that any error, as long as not nil,
+// is OK
+var errAny = errors.New("")
+
+var (
+	wantNothing = want{}
+	wantTrue    = want{out: bools(true)}
+	wantFalse   = want{out: bools(false)}
+)
+
+// Shorthands for values in want.out
+
+func strs(ss ...string) []Value {
+	vs := make([]Value, len(ss))
+	for i, s := range ss {
+		vs[i] = String(s)
+	}
+	return vs
+}
+
+func bools(bs ...bool) []Value {
+	vs := make([]Value, len(bs))
+	for i, b := range bs {
+		vs[i] = Bool(b)
+	}
+	return vs
+}
+
+func testEval(t *testing.T, dataDir string, evalTests []evalTest) {
+	for _, tt := range evalTests {
+		// fmt.Printf("eval %q\n", tt.text)
+
+		out, bytesOut, err := evalAndCollect(t, dataDir, []string{tt.text}, len(tt.want.out))
+
+		first := true
+		errorf := func(format string, args ...interface{}) {
+			if first {
+				first = false
+				t.Errorf("eval(%q) fails:", tt.text)
+			}
+			t.Errorf("  "+format, args...)
+		}
+
+		if !matchOut(tt.want.out, out) {
+			errorf("got out=%v, want %v", out, tt.want.out)
+		}
+		if string(tt.want.bytesOut) != string(bytesOut) {
+			errorf("got bytesOut=%q, want %q", bytesOut, tt.want.bytesOut)
+		}
+		if !matchErr(tt.want.err, err) {
+			errorf("got err=%v, want %v", err, tt.want.err)
+		}
+	}
+}
+
+func evalAndCollect(t *testing.T, dataDir string, texts []string, chsize int) ([]Value, []byte, error) {
+	name := "<eval test>"
+	ev := NewEvaler(api.NewClient("/invalid"), nil, dataDir, nil)
+
+	// Collect byte output
+	bytesOut := []byte{}
+	pr, pw, _ := os.Pipe()
+	bytesDone := make(chan struct{})
+	go func() {
+		for {
+			var buf [64]byte
+			nr, err := pr.Read(buf[:])
+			bytesOut = append(bytesOut, buf[:nr]...)
+			if err != nil {
+				break
+			}
+		}
+		close(bytesDone)
+	}()
+
+	// Channel output
+	outs := []Value{}
+
+	// Eval error. Only that of the last text is saved.
+	var ex error
+
+	for _, text := range texts {
+		op := mustParseAndCompile(t, ev, name, text)
+
+		outCh := make(chan Value, chsize)
+		outDone := make(chan struct{})
+		go func() {
+			for v := range outCh {
+				outs = append(outs, v)
+			}
+			close(outDone)
+		}()
+
+		ports := []*Port{
+			{File: os.Stdin, Chan: ClosedChan},
+			{File: pw, Chan: outCh},
+			{File: os.Stderr, Chan: BlackholeChan},
+		}
+
+		ex = ev.eval(op, ports, name, text)
+		close(outCh)
+		<-outDone
+	}
+
+	pw.Close()
+	<-bytesDone
+	pr.Close()
+
+	return outs, bytesOut, ex
+}
+
+func mustParseAndCompile(t *testing.T, ev *Evaler, name, text string) Op {
+	n, err := parse.Parse(name, text)
+	if err != nil {
+		t.Fatalf("Parse(%q) error: %s", text, err)
+	}
+	op, err := ev.Compile(n, name, text)
+	if err != nil {
+		t.Fatalf("Compile(Parse(%q)) error: %s", text, err)
+	}
+	return op
+}
+
+func matchOut(want, got []Value) bool {
+	if len(got) == 0 && len(want) == 0 {
+		return true
+	}
+	return reflect.DeepEqual(got, want)
+}
+
+func matchErr(want, got error) bool {
+	if got == nil {
+		return want == nil
+	}
+	return want == errAny || reflect.DeepEqual(got.(*Exception).Cause, want)
+}
+
+// compareValues compares two slices, using equals for each element.
+func compareSlice(wantValues, gotValues []interface{}) error {
+	if len(wantValues) != len(gotValues) {
+		return fmt.Errorf("want %d values, got %d",
+			len(wantValues), len(gotValues))
+	}
+	for i, want := range wantValues {
+		if !equals(want, gotValues[i]) {
+			return fmt.Errorf("want [%d] = %s, got %s", i, want, gotValues[i])
+		}
+	}
+	return nil
+}
+
+// equals compares two values. It uses Eq if want is a Value instance, or
+// reflect.DeepEqual otherwise.
+func equals(a, b interface{}) bool {
+	if aValue, ok := a.(Value); ok {
+		return aValue.Equal(b)
+	}
+	return reflect.DeepEqual(a, b)
+}
