5034914a6f67a90f5105c7cf83cc7212d5f5e07e
Qi Xiao <xiaqqaix@gmail.com>
2017-12-28 22:24:45 +0000

    daemon: Add a test.

--- daemon/daemon_test.go
+++ daemon/daemon_test.go
@@ -1,7 +1,37 @@
 package daemon
 
-import "testing"
+import (
+	"testing"
+	"time"
 
-func TestService(t *testing.T) {
-	// TODO(xiaq): Add tests.
+	"github.com/elves/elvish/util"
+)
+
+func TestDaemon(t *testing.T) {
+	util.InTempDir(func(string) {
+		serverDone := make(chan struct{})
+		go func() {
+			Serve("sock", "db")
+			close(serverDone)
+		}()
+
+		client := NewClient("sock")
+		for i := 0; i < 10; i++ {
+			client.ResetConn()
+			_, err := client.Version()
+			if err == nil {
+				break
+			} else if i == 9 {
+				t.Fatal("Failed to connect after 100ms")
+			}
+			time.Sleep(10 * time.Millisecond)
+		}
+		_, err := client.AddCmd("test cmd")
+		if err != nil {
+			t.Errorf("client.AddCmd -> error %v", err)
+		}
+		client.Close()
+		// Wait for server to quit before returning
+		<-serverDone
+	})
 }
