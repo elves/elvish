1edec901b8530ce78fac72b40c4ca9253717e8c0
Qi Xiao <xiaqqaix@gmail.com>
2018-01-02 01:43:31 +0000

    eval: Move Rat to eval/types.

--- eval/types/rat.go
+++ eval/types/rat.go
@@ -1,11 +1,10 @@
-package eval
+package types
 
 import (
 	"errors"
 	"fmt"
 	"math/big"
 
-	"github.com/elves/elvish/eval/types"
 	"github.com/xiaq/persistent/hash"
 )
 
@@ -16,7 +15,7 @@ type Rat struct {
 	b *big.Rat
 }
 
-var _ types.Value = Rat{}
+var _ Value = Rat{}
 
 func (Rat) Kind() string {
 	return "string"
@@ -51,15 +50,15 @@ func (r Rat) String() string {
 
 // ToRat converts a Value to rat. A str can be converted to a rat if it can be
 // parsed. A rat is returned as-is. Other types of values cannot be converted.
-func ToRat(v types.Value) (Rat, error) {
+func ToRat(v Value) (Rat, error) {
 	switch v := v.(type) {
 	case Rat:
 		return v, nil
-	case types.String:
+	case String:
 		r := big.Rat{}
 		_, err := fmt.Sscanln(string(v), &r)
 		if err != nil {
-			return Rat{}, fmt.Errorf("%s cannot be parsed as rat", v.Repr(types.NoPretty))
+			return Rat{}, fmt.Errorf("%s cannot be parsed as rat", v.Repr(NoPretty))
 		}
 		return Rat{&r}, nil
 	default:
