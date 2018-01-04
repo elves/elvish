960b50c34309d14b8b56797e4006608808969c98
Qi Xiao <xiaqqaix@gmail.com>
2018-01-04 22:59:58 +0000

    eval/vartypes: Disallow assigning non-strings to env variables.

--- eval/vartypes/env.go
+++ eval/vartypes/env.go
@@ -1,10 +1,13 @@
 package vartypes
 
 import (
+	"errors"
 	"os"
 
 	"github.com/elves/elvish/eval/types"
 )
+
+var errEnvMustBeString = errors.New("environment variable can only be set string values")
 
 // envVariable represents an environment variable.
 type envVariable struct {
@@ -12,14 +15,18 @@ type envVariable struct {
 }
 
 func (ev envVariable) Set(val types.Value) error {
-	os.Setenv(ev.name, types.ToString(val))
-	return nil
+	if s, ok := val.(types.String); ok {
+		os.Setenv(ev.name, string(s))
+		return nil
+	}
+	return errEnvMustBeString
 }
 
 func (ev envVariable) Get() types.Value {
 	return types.String(os.Getenv(ev.name))
 }
 
+// NewEnv returns an environment variable.
 func NewEnv(name string) Variable {
 	return envVariable{name}
 }
