f7a5f768a2f85099f22aff98ff9f51b085396492
Qi Xiao <xiaqqaix@gmail.com>
2018-01-31 20:29:35 +0100

    names.go -> resolve.go.

--- eval/builtin_fn_cmd.go
+++ eval/builtin_fn_cmd.go
@@ -37,6 +37,24 @@ func resolveFn(ec *Frame, args []interface{}, opts map[string]interface{}) {
 	out <- resolve(cmd, ec)
 }
 
+func resolve(s string, ec *Frame) Callable {
+	// NOTE: This needs to be kept in sync with the resolution algorithm used in
+	// (*compiler).form.
+
+	// Try variable
+	explode, ns, name := ParseVariableRef(s)
+	if !explode {
+		if v := ec.ResolveVar(ns, name+FnSuffix); v != nil {
+			if caller, ok := v.Get().(Callable); ok {
+				return caller
+			}
+		}
+	}
+
+	// External command
+	return ExternalCmd{s}
+}
+
 func external(ec *Frame, args []interface{}, opts map[string]interface{}) {
 	var cmd string
 	ScanArgs(args, &cmd)
