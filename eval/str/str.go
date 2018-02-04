8036a8c96ebed1f4feba38bb2d2f0219960587a0
Qi Xiao <xiaqqaix@gmail.com>
2018-02-03 22:50:55 -0800

    eval/str: Use ReflectBuiltinFn.

--- eval/str/str.go
+++ eval/str/str.go
@@ -3,83 +3,44 @@
 package str
 
 import (
-	"strconv"
 	"strings"
 
 	"github.com/elves/elvish/eval"
-	"github.com/elves/elvish/eval/types"
+	"github.com/elves/elvish/eval/vartypes"
 )
 
 func Ns() eval.Ns {
 	ns := eval.Ns{}
-	eval.AddBuiltinFns(ns, fns...)
+	for name, impl := range fns {
+		ns[name+eval.FnSuffix] = vartypes.NewRo(eval.NewReflectBuiltinFn("str:"+name, impl))
+	}
 	return ns
 }
 
-var fns = []*eval.BuiltinFn{
-	{"compare", wrapSSI(strings.Compare)},
-	{"contains", wrapSSB(strings.Contains)},
-	{"contains-any", wrapSSB(strings.ContainsAny)},
-	{"count", wrapSSI(strings.Count)},
-	{"equal-fold", wrapSSB(strings.EqualFold)},
+var fns = map[string]interface{}{
+	"compare":      strings.Compare,
+	"contains":     strings.Contains,
+	"contains-any": strings.ContainsAny,
+	"count":        strings.Count,
+	"equal-fold":   strings.EqualFold,
 	// TODO: Fields, FieldsFunc
-	{"has-prefix", wrapSSB(strings.HasPrefix)},
-	{"has-suffix", wrapSSB(strings.HasSuffix)},
-	{"index", wrapSSI(strings.Index)},
-	{"index-any", wrapSSI(strings.IndexAny)},
+	"has-prefix": strings.HasPrefix,
+	"has-suffix": strings.HasSuffix,
+	"index":      strings.Index,
+	"index-any":  strings.IndexAny,
 	// TODO: IndexFunc, Join
-	{"last-index", wrapSSI(strings.LastIndex)},
+	"last-index": strings.LastIndex,
 	// TODO: LastIndexFunc, Map, Repeat, Replace, Split, SplitAfter
-	{"title", wrapSS(strings.Title)},
-	{"to-lower", wrapSS(strings.ToLower)},
-	{"to-title", wrapSS(strings.ToTitle)},
-	{"to-upper", wrapSS(strings.ToUpper)},
+	"title":    strings.Title,
+	"to-lower": strings.ToLower,
+	"to-title": strings.ToTitle,
+	"to-upper": strings.ToUpper,
 	// TODO: ToLowerSpecial, ToTitleSpecial, ToUpperSpecial
-	{"trim", wrapSSS(strings.Trim)},
-	{"trim-left", wrapSSS(strings.TrimLeft)},
-	{"trim-right", wrapSSS(strings.TrimRight)},
-	// TODO: Trim{Left,Right}Func
-	{"trim-space", wrapSS(strings.TrimSpace)},
-	{"trim-prefix", wrapSSS(strings.TrimPrefix)},
-	{"trim-suffix", wrapSSS(strings.TrimSuffix)},
-}
-
-func wrapSS(inner func(string) string) eval.BuiltinFnImpl {
-	return func(fm *eval.Frame, args []interface{}, opts map[string]interface{}) {
-		var s string
-		eval.ScanArgs(args, &s)
-		eval.TakeNoOpt(opts)
-		ret := inner(s)
-		fm.OutputChan() <- ret
-	}
-}
-
-func wrapSSS(inner func(a, b string) string) eval.BuiltinFnImpl {
-	return func(fm *eval.Frame, args []interface{}, opts map[string]interface{}) {
-		var a, b string
-		eval.ScanArgs(args, &a, &b)
-		eval.TakeNoOpt(opts)
-		ret := inner(a, b)
-		fm.OutputChan() <- ret
-	}
-}
-
-func wrapSSI(inner func(a, b string) int) eval.BuiltinFnImpl {
-	return func(fm *eval.Frame, args []interface{}, opts map[string]interface{}) {
-		var a, b string
-		eval.ScanArgs(args, &a, &b)
-		eval.TakeNoOpt(opts)
-		ret := inner(a, b)
-		fm.OutputChan() <- strconv.Itoa(ret)
-	}
-}
-
-func wrapSSB(inner func(a, b string) bool) eval.BuiltinFnImpl {
-	return func(fm *eval.Frame, args []interface{}, opts map[string]interface{}) {
-		var a, b string
-		eval.ScanArgs(args, &a, &b)
-		eval.TakeNoOpt(opts)
-		ret := inner(a, b)
-		fm.OutputChan() <- types.Bool(ret)
-	}
+	"trim":       strings.Trim,
+	"trim-left":  strings.TrimLeft,
+	"trim-right": strings.TrimRight,
+	// TODO: TrimLeft,Right}Func
+	"trim-space":  strings.TrimSpace,
+	"trim-prefix": strings.TrimPrefix,
+	"trim-suffix": strings.TrimSuffix,
 }
