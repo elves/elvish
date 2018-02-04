5f1d0aa7a1fbb1a34a2e4c53f7ca3987fd8c2473
Qi Xiao <xiaqqaix@gmail.com>
2018-02-03 18:51:35 -0800

    Fix Windows CI.

--- eval/builtin_fn_fs_test.go
+++ eval/builtin_fn_fs_test.go
@@ -9,7 +9,7 @@ func TestBuiltinFnFS(t *testing.T) {
 	pathSep := string(filepath.Separator)
 	runTests(t, []Test{
 		{`path-base a/b/c.png`, want{out: strs("c.png")}},
-		{`tilde-abbr $E:HOME` + pathSep + `foobar`,
+		{`tilde-abbr $E:HOME'` + pathSep + `'foobar`,
 			want{out: strs("~" + pathSep + "foobar")}},
 
 		{`-is-dir ~/dir`, wantTrue}, // see testmain_test.go for setup
