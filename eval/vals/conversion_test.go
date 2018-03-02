eb5465d263b399135adbda460d1f0376fe0a37a8
Qi Xiao <xiaqqaix@gmail.com>
2018-03-01 19:15:40 -0500

    eval/vals: More tests.

--- eval/vals/conversion_test.go
+++ eval/vals/conversion_test.go
@@ -22,7 +22,9 @@ func scanToGo2(src interface{}, dstInit interface{}) (interface{}, error) {
 
 var scanToGoTests = tt.Table{
 	Args("12", 0).Rets(12),
+	Args("0x12", 0).Rets(0x12),
 	Args("23", 0.0).Rets(23.0),
+	Args("0x23", 0.0).Rets(float64(0x23)),
 	Args("x", ' ').Rets('x'),
 	Args("foo", "").Rets("foo"),
 	Args(someType{"foo"}, someType{}).Rets(someType{"foo"}),
---
+++ eval/vals/equal_test.go
@@ -0,0 +1,33 @@
+package vals
+
+import (
+	"testing"
+
+	"github.com/elves/elvish/tt"
+)
+
+type customEqualer struct{ ret bool }
+
+func (c customEqualer) Equal(interface{}) bool { return c.ret }
+
+type customStruct struct{ a, b string }
+
+var equalTests = tt.Table{
+	Args(customEqualer{true}, 2).Rets(true),
+	Args(customEqualer{false}, 2).Rets(false),
+	Args(true, true).Rets(true),
+	Args(true, false).Rets(false),
+	Args("lorem", "lorem").Rets(true),
+	Args("lorem", "ipsum").Rets(false),
+	Args(MakeList("a", "b"), MakeList("a", "b")).Rets(true),
+	Args(MakeList("a", "b"), MakeList("a")).Rets(false),
+	Args(MakeList("a", "b"), MakeList("a", "c")).Rets(false),
+	Args(MakeMapFromKV("k", "v"), MakeMapFromKV("k", "v")).Rets(true),
+	Args(MakeMapFromKV("k", "v"), MakeMapFromKV("k2", "v")).Rets(false),
+	Args(MakeMapFromKV("k", "v", "k2", "v2"), MakeMapFromKV("k", "v")).Rets(false),
+	Args(&customStruct{"a", "b"}, &customStruct{"a", "b"}).Rets(true),
+}
+
+func TestEqual(t *testing.T) {
+	tt.Test(t, tt.Fn("Equal", Equal), equalTests)
+}
