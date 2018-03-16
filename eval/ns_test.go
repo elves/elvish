2f834da5c0f275607c576841182ed71878a6d971
Qi Xiao <xiaqqaix@gmail.com>
2018-03-16 18:17:04 +0000

    eval: Fix and simplify ns tests.

--- eval/ns_test.go
+++ eval/ns_test.go
@@ -9,5 +9,12 @@ func TestNs(t *testing.T) {
 		That("ns = (ns [&]); eq $ns $ns").Puts(true),
 		That("eq (ns [&]) (ns [&])").Puts(false),
 		That("eq (ns [&]) [&]").Puts(false),
+
+		That(`ns: = (ns [&a=b &x=y]); put $ns:a`).Puts("b"),
+		That(`ns: = (ns [&a=b &x=y]); put $ns:[a]`).Puts("b"),
+		// Test multi-key ns when sorting is possible
+		That(`keys (ns [&a=b])`).Puts("a"),
+		That(`has-key (ns [&a=b &x=y]) a`).Puts(true),
+		That(`has-key (ns [&a=b &x=y]) b`).Puts(false),
 	})
 }
