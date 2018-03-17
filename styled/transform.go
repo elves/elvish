6be5ab2adf9f8efd9d64db18e2663a2067bd1850
Andreas Fehn <andi.fehn@gmail.com>
2018-03-17 15:34:38 +0100

    Add default style transformers to reimplement $edit:styled~

---
+++ styled/transform.go
@@ -0,0 +1,58 @@
+package styled
+
+type segmentTransformer func(Segment) Segment
+
+var SegmentTransformers map[string]segmentTransformer
+
+func init() {
+	myTrue := true
+
+	SegmentTransformers = map[string]segmentTransformer{
+		"bold": func(segment Segment) Segment {
+			segment.bold = &myTrue
+			return segment
+		},
+		"dim": func(segment Segment) Segment {
+			segment.dim = &myTrue
+			return segment
+		},
+		"italic": func(segment Segment) Segment {
+			segment.italic = &myTrue
+			return segment
+		},
+		"underline": func(segment Segment) Segment {
+			segment.underline = &myTrue
+			return segment
+		},
+		"blink": func(segment Segment) Segment {
+			segment.blink = &myTrue
+			return segment
+		},
+		"inverse": func(segment Segment) Segment {
+			var val bool
+			if segment.inverse == nil || !(*segment.inverse) {
+				val = true
+			}
+			segment.inverse = &val
+			return segment
+		},
+	}
+
+	makeFg := func(col Color) segmentTransformer {
+		return func(segment Segment) Segment {
+			segment.Foreground = &col
+			return segment
+		}
+	}
+	makeBg := func(col Color) segmentTransformer {
+		return func(segment Segment) Segment {
+			segment.Background = &col
+			return segment
+		}
+	}
+
+	for c := ColorDefault; c <= ColorWhite; c++ {
+		SegmentTransformers[c.String()] = makeFg(c)
+		SegmentTransformers["bg-"+c.String()] = makeBg(c)
+	}
+}
