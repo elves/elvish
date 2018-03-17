8c3424a8601f51038e64340dd227c84891d7531c
Andreas Fehn <andi.fehn@gmail.com>
2018-03-17 15:31:07 +0100

    Add structs for styled segments and texts

---
+++ styled/segment.go
@@ -0,0 +1,102 @@
+package styled
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/elves/elvish/eval/vals"
+)
+
+// A styled Segment is a string that has some style applied to it.
+type Segment struct {
+	Style
+	Text string
+}
+
+func (Segment) Kind() string { return "styled-segment" }
+
+// Returns the representation of this Segment. The string can be used to construct
+// an identical Segment. Unset or default attributes are skipped. If the Segment
+// represents an unstyled string only this string is returned.
+func (s Segment) Repr(indent int) string {
+	buf := new(bytes.Buffer)
+	add := func(key string, val interface{}) {
+		fmt.Fprintf(buf, "&%s=%s ", key, vals.Repr(val, 0))
+	}
+
+	for k, v := range s.Style.ToMap() {
+		switch v := v.(type) {
+		case Color:
+			// todo: Display ColorDefault?
+			if v != ColorDefault {
+				add(k, v.String())
+			}
+		case bool:
+			if v {
+				add(k, v)
+			}
+		}
+	}
+
+	if buf.Len() == 0 {
+		return s.Text
+	}
+
+	return fmt.Sprintf("(styled-segment %s %s)", s.Text, strings.TrimSpace(buf.String()))
+}
+
+func (s Segment) IterateKeys(fn func(v interface{}) bool) {
+	for k := range s.Style.ToMap() {
+		if !fn(k) {
+			break
+		}
+	}
+}
+
+// Provides access to the attributes of the Segment.
+func (s Segment) Index(k interface{}) (v interface{}, ok bool) {
+	if k == "text" {
+		return s.Text, true
+	} else if k, ok := k.(string); ok {
+		m := s.Style.ToMap()
+		if v, ok := m[k]; ok {
+			if v, ok := v.(Color); ok {
+				return v.String(), true
+			}
+			return v, true
+		}
+	}
+
+	return nil, false
+}
+
+// Implements Segment+string, Segment+Segment and Segment+Text.
+func (s Segment) Concat(v interface{}) (interface{}, error) {
+	switch rhs := v.(type) {
+	case string:
+		return Text{
+			s,
+			Segment{Text: rhs},
+		}, nil
+	case Segment:
+		return Text{s, rhs}, nil
+	case Text:
+		return Text(append([]Segment{s}, rhs...)), nil
+	}
+
+	return nil, vals.ErrConcatNotImplemented
+}
+
+// Implements string+Segment.
+func (s Segment) RConcat(v interface{}) (interface{}, error) {
+	switch lhs := v.(type) {
+	case string:
+		return Text{
+			Segment{Text: lhs},
+			s,
+		}, nil
+	}
+
+	return nil, vals.ErrConcatNotImplemented
+}
---
+++ styled/text.go
@@ -0,0 +1,68 @@
+package styled
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+
+	"github.com/elves/elvish/eval/vals"
+)
+
+// A styled Text contains of a list of styled Segments.
+type Text []Segment
+
+func (t Text) Kind() string { return "styled-text" }
+
+// Returns the representation of the current Text. It is just a wrapper
+// around the containing Segments.
+func (t Text) Repr(indent int) string {
+	buf := new(bytes.Buffer)
+	for _, s := range t {
+		buf.WriteString(s.Repr(indent + 1))
+	}
+	return fmt.Sprintf("(styled %s)", buf.String())
+}
+
+func (t Text) IterateKeys(fn func(interface{}) bool) {
+	for i := 0; i < len(t); i++ {
+		if !fn(strconv.Itoa(i)) {
+			break
+		}
+	}
+}
+
+// Provides access to the underlying Segments.
+func (t Text) Index(k interface{}) (interface{}, error) {
+	index, err := vals.ConvertListIndex(k, len(t))
+	if err != nil {
+		return nil, err
+	} else if index.Slice {
+		return t[index.Lower:index.Upper], nil
+	} else {
+		return t[index.Lower], nil
+	}
+}
+
+// Implements Text+string, Text+Segment and Text+Text.
+func (t Text) Concat(v interface{}) (interface{}, error) {
+	switch rhs := v.(type) {
+	case string:
+		return Text(append(t, Segment{Text: rhs})), nil
+	case Segment:
+		return Text(append(t, rhs)), nil
+	case Text:
+		return Text(append(t, rhs...)), nil
+	}
+
+	return nil, vals.ErrConcatNotImplemented
+}
+
+// Implements string+Text.
+func (t Text) RConcat(v interface{}) (interface{}, error) {
+	switch lhs := v.(type) {
+	case string:
+		return Text(append([]Segment{{Text: lhs}}, t...)), nil
+	}
+
+	return nil, vals.ErrConcatNotImplemented
+}
