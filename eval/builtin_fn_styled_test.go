35ffac4aace43f359be99e83a3e1a28327c50156
Andreas Fehn <andi.fehn@gmail.com>
2018-05-23 21:02:30 +0200

    Rebase for new test framework api and expand test cases

--- eval/builtin_fn_styled_test.go
+++ eval/builtin_fn_styled_test.go
@@ -1,65 +1,82 @@
 package eval
 
 import (
-	"fmt"
 	"testing"
 )
 
-// Taken from the original implementation and expanded with default foreground color.
-var styleTranslationTable = map[string]string{
-	"bold":       "1",
-	"dim":        "2",
-	"italic":     "3",
-	"underlined": "4",
-	"blink":      "5",
-	"inverse":    "7",
-
-	"default":      "39",
-	"black":        "30",
-	"red":          "31",
-	"green":        "32",
-	"yellow":       "33",
-	"blue":         "34",
-	"magenta":      "35",
-	"cyan":         "36",
-	"lightgray":    "37",
-	"gray":         "90",
-	"lightred":     "91",
-	"lightgreen":   "92",
-	"lightyellow":  "93",
-	"lightblue":    "94",
-	"lightmagenta": "95",
-	"lightcyan":    "96",
-	"white":        "97",
-
-	"bg-default":      "49",
-	"bg-black":        "40",
-	"bg-red":          "41",
-	"bg-green":        "42",
-	"bg-yellow":       "43",
-	"bg-blue":         "44",
-	"bg-magenta":      "45",
-	"bg-cyan":         "46",
-	"bg-lightgray":    "47",
-	"bg-gray":         "100",
-	"bg-lightred":     "101",
-	"bg-lightgreen":   "102",
-	"bg-lightyellow":  "103",
-	"bg-lightblue":    "104",
-	"bg-lightmagenta": "105",
-	"bg-lightcyan":    "106",
-	"bg-white":        "107",
+func TestStyledString(t *testing.T) {
+	Test(t,
+		That("print (styled abc bold)").Prints("\033[1mabc\033[m"),
+		That("print (styled abc red cyan)").Prints("\033[36mabc\033[m"),
+		That("print (styled abc bg-green)").Prints("\033[42mabc\033[m"),
+	)
 }
 
-func TestCompatibility(t *testing.T) {
-	const text = "abc"
+func TestStyledSegment(t *testing.T) {
+	Test(t,
+		That("print (styled (styled-segment abc &fg-color=cyan) bold)").Prints("\033[1;36mabc\033[m"),
+		That("print (styled (styled-segment (styled-segment abc &fg-color=magenta) &dim=$true) cyan)").Prints("\033[2;36mabc\033[m"),
+		That("print (styled (styled-segment abc &inverse=$true) inverse)").Prints("abc"),
+	)
+}
 
-	tests := make([]Test, len(styleTranslationTable))
-	i := 0
-	for k, v := range styleTranslationTable {
-		tests[i] = That(fmt.Sprintf("print (styled %s %s)", text, k)).Prints(fmt.Sprintf("\033[%sm%s\033[m", v, text))
-		i++
-	}
+func TestStyledText(t *testing.T) {
+	Test(t,
+		That("print (styled (styled abc red) blue)").Prints("\033[34mabc\033[m"),
+		That("print (styled (styled abc italic) red)").Prints("\033[3;31mabc\033[m"),
+		That("print (styled (styled abc inverse) inverse)").Prints("abc"),
+		That("print (styled (styled abc inverse) inverse inverse)").Prints("\033[7mabc\033[m"),
+	)
+}
 
-	runTests(t, tests)
+func TestStyledConcat(t *testing.T) {
+	Test(t,
+		// string+segment
+		That("print abc(styled-segment abc &fg-color=red)").Prints("abc\033[31mabc\033[m"),
+		// segment+string
+		That("print (styled-segment abc &fg-color=red)abc").Prints("\033[31mabc\033[mabc"),
+		// segment+segment
+		That("print (styled-segment abc &bg-color=red)(styled-segment abc &fg-color=red)").Prints("\033[41mabc\033[m\033[31mabc\033[m"),
+		// segment+text
+		That("print (styled-segment abc &underlined=$true)(styled abc lightcyan)").Prints("\033[4mabc\033[m\033[96mabc\033[m"),
+	)
+
+	Test(t,
+		// string+text
+		That("print abc(styled abc blink)").Prints("abc\033[5mabc\033[m"),
+		// text+string
+		That("print (styled abc blink)abc").Prints("\033[5mabc\033[mabc"),
+		// text+segment
+		That("print (styled abc inverse)(styled-segment abc &bg-color=white)").Prints("\033[7mabc\033[m\033[107mabc\033[m"),
+		// text+text
+		That("print (styled abc bold)(styled abc dim)").Prints("\033[1mabc\033[m\033[2mabc\033[m"),
+	)
+}
+
+func TestFunctionalStyleTransformers(t *testing.T) {
+	// lambda
+	Test(t,
+		That("print (styled abc [s]{ styled-segment $s &bold=$true &italic=$false })").Prints("\033[1mabc\033[m"),
+		That("print (styled abc italic [s]{ styled-segment $s &bold=$true &italic=$false })").Prints("\033[1mabc\033[m"),
+	)
+
+	// fn
+	Test(t,
+		That("fn f [s]{ styled-segment $s &bold=$true &italic=$false }; print (styled abc $f~)").Prints("\033[1mabc\033[m"),
+		That("fn f [s]{ styled-segment $s &bold=$true &italic=$false }; print (styled abc italic $f~)").Prints("\033[1mabc\033[m"),
+	)
+
+	// var
+	Test(t,
+		That("f = [s]{ styled-segment $s &bold=$true &italic=$false }; print (styled abc $f)").Prints("\033[1mabc\033[m"),
+		That("f = [s]{ styled-segment $s &bold=$true &italic=$false }; print (styled abc italic $f)").Prints("\033[1mabc\033[m"),
+	)
+}
+
+func TestStyledIndexing(t *testing.T) {
+	Test(t,
+		That("put (styled-segment abc &italic=$true &fg-color=red)[bold]").Puts(false),
+		That("put (styled-segment abc &italic=$true &fg-color=red)[italic]").Puts(true),
+		That("put (styled-segment abc &italic=$true &fg-color=red)[fg-color]").Puts("red"),
+	)
 }
