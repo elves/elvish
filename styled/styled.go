949b409356e3f9495cb6b8a9eb277ca718d80dbf
Andreas Fehn <andi.fehn@gmail.com>
2018-05-28 17:47:10 +0200

    Use pointers instead of passing setter functions

--- styled/styled.go
+++ styled/styled.go
@@ -23,20 +23,20 @@ func EmptyStyle() Style {
 }
 
 func (s *Style) ImportFromOptions(options map[string]interface{}) error {
-	assignColor := func(key string, assign func(string)) error {
+	assignColor := func(key string, colorField *string) error {
 		if c, ok := options[key]; ok {
 			if c, ok := c.(string); ok && IsValidColorString(c) {
-				assign(c)
+				*colorField = c
 			} else {
 				return fmt.Errorf("value to option '%s' must be a valid color string", key)
 			}
 		}
 		return nil
 	}
-	assignBool := func(key string, assign func(bool)) error {
+	assignBool := func(key string, attrField *bool) error {
 		if b, ok := options[key]; ok {
 			if b, ok := b.(bool); ok {
-				assign(b)
+				*attrField = b
 			} else {
 				return fmt.Errorf("value to option '%s' must be a bool value", key)
 			}
@@ -45,28 +45,28 @@ func (s *Style) ImportFromOptions(options map[string]interface{}) error {
 		return nil
 	}
 
-	if err := assignColor("fg-color", func(c string) { s.Foreground = c }); err != nil {
+	if err := assignColor("fg-color", &s.Foreground); err != nil {
 		return err
 	}
-	if err := assignColor("bg-color", func(c string) { s.Background = c }); err != nil {
+	if err := assignColor("bg-color", &s.Background); err != nil {
 		return err
 	}
-	if err := assignBool("bold", func(b bool) { s.Bold = b }); err != nil {
+	if err := assignBool("bold", &s.Bold); err != nil {
 		return err
 	}
-	if err := assignBool("dim", func(b bool) { s.Dim = b }); err != nil {
+	if err := assignBool("dim", &s.Dim); err != nil {
 		return err
 	}
-	if err := assignBool("italic", func(b bool) { s.Italic = b }); err != nil {
+	if err := assignBool("italic", &s.Italic); err != nil {
 		return err
 	}
-	if err := assignBool("underlined", func(b bool) { s.Underlined = b }); err != nil {
+	if err := assignBool("underlined", &s.Underlined); err != nil {
 		return err
 	}
-	if err := assignBool("blink", func(b bool) { s.Blink = b }); err != nil {
+	if err := assignBool("blink", &s.Blink); err != nil {
 		return err
 	}
-	if err := assignBool("inverse", func(b bool) { s.Inverse = b }); err != nil {
+	if err := assignBool("inverse", &s.Inverse); err != nil {
 		return err
 	}
 
