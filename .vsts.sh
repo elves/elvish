05b49f8076a052401c53861a9f89f6c0d29dc031
Qi Xiao <xiaqqaix@gmail.com>
2018-06-06 23:52:25 +0100

    Attempt to fix .vsts.sh.

--- .vsts.sh
+++ .vsts.sh
@@ -1,10 +1,12 @@
-mkdir ~/bin ~/go
+mkdir ~/bin
 export PATH=$HOME/bin:$PATH
 
 curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
 chmod +x ~/bin/gimme
 eval "$(gimme 1.9)"
 
+mkdir ~/go
 export GOPATH=$HOME/go
-go get github.com/elves/elvish
-go test github.com/elves/elvish/...
+go get -d github.com/elves/elvish
+cd ~/go/github.com/elves/elvish
+make test
