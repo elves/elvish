c660a5aceb857b93a3dc0edbe17147a947c2ad29
Qi Xiao <xiaqqaix@gmail.com>
2018-07-07 22:32:35 +0100

    newedit/core: fakeMode supports defining result for ModeLine and ModeRenderFlag.

--- newedit/core/fake_mode.go
+++ newedit/core/fake_mode.go
@@ -2,17 +2,20 @@ package core
 
 import "github.com/elves/elvish/edit/ui"
 
-// An implementation of Mode. The HandleKey method returns CommitCode after a
-// certain number of key events and keeps the key event history, and the
-// ModeLine methods returns a predefined value.
+// An implementation of Mode. Its HandleKey method returns CommitCode after a
+// certain number of key events and keeps the key event history, and its
+// ModeLine and ModeRenderFlag methods returns predefined values.
 type fakeMode struct {
-	maxKeys int
+	maxKeys        int
+	modeLine       ui.Renderer
+	modeRenderFlag ModeRenderFlag
 
 	keys []ui.Key
 }
 
-func (m *fakeMode) ModeLine() ui.Renderer          { return nil }
-func (m *fakeMode) ModeRenderFlag() ModeRenderFlag { return 0 }
+func (m *fakeMode) ModeLine() ui.Renderer { return m.modeLine }
+
+func (m *fakeMode) ModeRenderFlag() ModeRenderFlag { return m.modeRenderFlag }
 
 func (m *fakeMode) HandleKey(k ui.Key, st *State) HandlerAction {
 	m.keys = append(m.keys, k)
