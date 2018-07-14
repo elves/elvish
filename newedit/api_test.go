bab7f107b795e77ce75dffa9595a353e8213f561
Qi Xiao <xiaqqaix@gmail.com>
2018-07-14 19:30:15 +0100

    newedit: Test against the API.

---
+++ newedit/api_test.go
@@ -0,0 +1,19 @@
+package newedit
+
+import (
+	"os"
+	"testing"
+
+	"github.com/elves/elvish/eval"
+)
+
+func TestNs(t *testing.T) {
+	ev := eval.NewEvaler()
+	ed := NewEditor(os.Stdin, os.Stdout).(*editor)
+	ev.Global.AddNs("edit", ed.Ns())
+
+	ev.EvalSource(eval.NewScriptSource("[t]", "[t]", "edit:max-height = 20"))
+	if ed.core.Config.RenderConfig.MaxHeight != 20 {
+		t.Errorf("Failed to set MaxHeight to 20 via binding")
+	}
+}
