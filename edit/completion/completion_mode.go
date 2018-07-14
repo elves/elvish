43e0761ec7214988fd1d9fba035bd0c834eb4de8
Qi Xiao <xiaqqaix@gmail.com>
2018-07-14 17:35:44 +0100

    edit/completion: prev and next does nothing when there is no candidate.
    This fixes #717.

--- edit/completion/completion_mode.go
+++ edit/completion/completion_mode.go
@@ -181,6 +181,9 @@ func (c *completion) apply(line string, dot int) (string, int) {
 }
 
 func (c *completion) prev(cycle bool) {
+	if c.selected == -1 {
+		return
+	}
 	c.selected--
 	if c.selected == -1 {
 		if cycle {
@@ -192,6 +195,9 @@ func (c *completion) prev(cycle bool) {
 }
 
 func (c *completion) next(cycle bool) {
+	if c.selected == -1 {
+		return
+	}
 	c.selected++
 	if c.selected == len(c.filtered) {
 		if cycle {
