f9ef4e62364ab0d662982012a2c592649cd3124d
Kye W. Shi <kwshi@hmc.edu>
2018-08-07 03:03:29 -0700

    edit/edcore: Implement functions for word-based movement. (#721)
    * edit/edcore: Implement some insert-mode functions for word-based movement.
    
    Implemented `kill-word-right`, `kill-small-word-right`, `move-dot-left-small-word`, and `move-dot-right-small-word`.
    
    * eval/bundled: Create default bindings for `kill-word-left` and `kill-word-right`.
    
    * edit/edcore: Fix a dumb arithmetic error in `kill-word-right`.
    
    * edit/edcore: Purify move- and kill- buffer commands.
    
    * Implement category-based movement, per discussions in #696
    
    * edit/edcore: Design unit tests for moveDot functions.
    
    * edit/edcore: Revise categorizer-based movement for clarity; rewrite some comments.

--- edit/edcore/insert.go
+++ edit/edcore/insert.go
@@ -23,23 +23,26 @@ func init() {
 
 func initCoreFns(ed *editor, ns eval.Ns) {
 	ns.AddBuiltinFns("edit:", map[string]interface{}{
-		"kill-line-left":       ed.killLineLeft,
-		"kill-line-right":      ed.killLineRight,
-		"kill-word-left":       ed.killWordLeft,
-		"kill-small-word-left": ed.killSmallWordLeft,
-		"kill-rune-left":       ed.killRuneLeft,
-		"kill-rune-right":      ed.killRuneRight,
+		"kill-line-left":        ed.applyKill(moveDotSOL),
+		"kill-line-right":       ed.applyKill(moveDotEOL),
+		"kill-word-left":        ed.applyKill(moveDotLeftWord),
+		"kill-word-right":       ed.applyKill(moveDotRightWord),
+		"kill-small-word-left":  ed.applyKill(moveDotLeftSmallWord),
+		"kill-small-word-right": ed.applyKill(moveDotRightSmallWord),
+		"kill-rune-left":        ed.applyKill(moveDotLeft),
+		"kill-rune-right":       ed.applyKill(moveDotRight),
 
-		"move-dot-left":             ed.moveDotLeft,
-		"move-dot-right":            ed.moveDotRight,
-		"move-dot-left-word":        ed.moveDotLeftWord,
-		"move-dot-right-word":       ed.moveDotRightWord,
-		"move-dot-left-small-word":  ed.moveDotLeftSmallWord,
-		"move-dot-right-small-word": ed.moveDotRightSmallWord,
-		"move-dot-sol":              ed.moveDotSOL,
-		"move-dot-eol":              ed.moveDotEOL,
-		"move-dot-up":               ed.moveDotUp,
-		"move-dot-down":             ed.moveDotDown,
+		"move-dot-left":             ed.applyMove(moveDotLeft),
+		"move-dot-right":            ed.applyMove(moveDotRight),
+		"move-dot-left-word":        ed.applyMove(moveDotLeftWord),
+		"move-dot-right-word":       ed.applyMove(moveDotRightWord),
+		"move-dot-left-small-word":  ed.applyMove(moveDotLeftSmallWord),
+		"move-dot-right-small-word": ed.applyMove(moveDotRightSmallWord),
+		"move-dot-sol":              ed.applyMove(moveDotSOL),
+		"move-dot-eol":              ed.applyMove(moveDotEOL),
+
+		"move-dot-up":   ed.moveDotUp,
+		"move-dot-down": ed.moveDotDown,
 
 		"insert-last-word": ed.insertLastWord,
 		"insert-key":       ed.insertKey,
@@ -133,136 +136,164 @@ func (ed *editor) commandStart() {
 	ed.SetMode(ed.command)
 }
 
-func (ed *editor) killLineLeft() {
-	sol := util.FindLastSOL(ed.buffer[:ed.dot])
-	ed.buffer = ed.buffer[:sol] + ed.buffer[ed.dot:]
-	ed.dot = sol
-}
-
-func (ed *editor) killLineRight() {
-	eol := util.FindFirstEOL(ed.buffer[ed.dot:]) + ed.dot
-	ed.buffer = ed.buffer[:ed.dot] + ed.buffer[eol:]
-}
-
-// NOTE(xiaq): A word is a run of non-space runes. When killing a word,
-// trimming spaces are removed as well. Examples:
-// "abc  xyz" -> "abc  ", "abc xyz " -> "abc  ".
-
-func (ed *editor) killWordLeft() {
-	if ed.dot == 0 {
-		return
+func (ed *editor) applyMove(move func(string, int) int) func() {
+	return func() {
+		ed.dot = move(ed.buffer, ed.dot)
 	}
-	space := strings.LastIndexFunc(
-		strings.TrimRightFunc(ed.buffer[:ed.dot], unicode.IsSpace),
-		unicode.IsSpace) + 1
-	ed.buffer = ed.buffer[:space] + ed.buffer[ed.dot:]
-	ed.dot = space
 }
 
-// NOTE(xiaq): A small word is either a run of alphanumeric (Unicode category L
-// or N) runes or a run of non-alphanumeric runes. This is consistent with vi's
-// definition of word, except that "_" is not considered alphanumeric. When
-// killing a small word, trimming spaces are removed as well. Examples:
-// "abc/~" -> "abc", "~/abc" -> "~/", "abc* " -> "abc"
+func (ed *editor) applyKill(move func(string, int) int) func() {
+	return func() {
+		index := move(ed.buffer, ed.dot)
 
-func (ed *editor) killSmallWordLeft() {
-	left := strings.TrimRightFunc(ed.buffer[:ed.dot], unicode.IsSpace)
-	// The case of left == "" is handled as well.
-	r, _ := utf8.DecodeLastRuneInString(left)
-	if isAlnum(r) {
-		left = strings.TrimRightFunc(left, isAlnum)
-	} else {
-		left = strings.TrimRightFunc(
-			left, func(r rune) bool { return !isAlnum(r) })
+		if index < ed.dot { // delete left
+			ed.buffer = ed.buffer[:index] + ed.buffer[ed.dot:]
+			ed.dot = index
+		} else { // delete right
+			ed.buffer = ed.buffer[:ed.dot] + ed.buffer[index:]
+		}
 	}
-	ed.buffer = left + ed.buffer[ed.dot:]
-	ed.dot = len(left)
 }
 
 func isAlnum(r rune) bool {
 	return unicode.IsLetter(r) || unicode.IsNumber(r)
 }
 
-func (ed *editor) killRuneLeft() {
-	if ed.dot > 0 {
-		_, w := utf8.DecodeLastRuneInString(ed.buffer[:ed.dot])
-		ed.buffer = ed.buffer[:ed.dot-w] + ed.buffer[ed.dot:]
-		ed.dot -= w
-	} else {
-		ed.flash()
+func moveDotLeft(buffer string, dot int) int {
+	_, w := utf8.DecodeLastRuneInString(buffer[:dot])
+	return dot - w
+}
+
+func moveDotRight(buffer string, dot int) int {
+	_, w := utf8.DecodeRuneInString(buffer[dot:])
+	return dot + w
+}
+
+// Returns a move- function that moves the dot to the beginning of the
+// last run of characters of the same non-whitespace category to the
+// left of the dot..
+//
+// The categorization of characters is derived from the categorizer
+// function, which returns for each character an integer representing
+// the category of the integer.  The return value `0` of the
+// categorizer function is used as a special category representing
+// non-word, or "always-skip" characters.  All other return values are
+// equivalent, used only to distinguish between different character
+// classes.
+//
+// See `categorizeWord`, `categorizeSmallWord`, and
+// `categorizeAlphanumeric` for examples.
+func moveDotLeftCategoryFunc(categorize func(rune) int, buffer string, dot int) int {
+	// skip trailing whitespaces left of cursor
+	left := strings.TrimRightFunc(buffer[:dot], func(r rune) bool {
+		return categorize(r) == 0
+	})
+
+	// get category of last character
+	r, _ := utf8.DecodeLastRuneInString(left)
+	cat := categorize(r)
+
+	// skip characters of same category
+	left = strings.TrimRightFunc(left, func(r rune) bool {
+		return categorize(r) == cat
+	})
+
+	return len(left)
+}
+
+// Returns a move-function that moves the cursor to the beginning of
+// the next run of characters of the same non-whitespace category to
+// the right of the dot.
+//
+// See `moveDotLeftCategoryFunc`.
+func moveDotRightCategoryFunc(categorize func(rune) int, buffer string, dot int) int {
+
+	// skip leading whitespaces right of dot
+	right := strings.TrimLeftFunc(buffer[dot:], func(r rune) bool {
+		return categorize(r) == 0
+	})
+
+	// check whether any whitespace was skipped; if whitespace was
+	// skipped, then dot is already successfully moved to next
+	// non-whitespace run
+	if dot < len(buffer)-len(right) {
+		return len(buffer) - len(right)
+	}
+
+	// no whitespace was skipped, so we still have to skip to the next
+	// non-whitespace run
+
+	// get category of first non-whitespace character
+	r, _ := utf8.DecodeRuneInString(right)
+	cat := categorize(r)
+
+	// skip characters of same category
+	right = strings.TrimLeftFunc(right, func(r rune) bool {
+		return categorize(r) == cat
+	})
+
+	// skip remaining whitespace
+	right = strings.TrimLeftFunc(right, func(r rune) bool {
+		return categorize(r) == 0
+	})
+
+	return len(buffer) - len(right)
+}
+
+func categorizeWord(r rune) int {
+	switch {
+	case unicode.IsSpace(r):
+		return 0
+	default:
+		return 1
 	}
 }
 
-func (ed *editor) killRuneRight() {
-	if ed.dot < len(ed.buffer) {
-		_, w := utf8.DecodeRuneInString(ed.buffer[ed.dot:])
-		ed.buffer = ed.buffer[:ed.dot] + ed.buffer[ed.dot+w:]
-	} else {
-		ed.flash()
+func categorizeSmallWord(r rune) int {
+	switch {
+	case unicode.IsSpace(r):
+		return 0
+	case isAlnum(r):
+		return 1
+	default:
+		return 2
 	}
 }
 
-func (ed *editor) moveDotLeft() {
-	_, w := utf8.DecodeLastRuneInString(ed.buffer[:ed.dot])
-	ed.dot -= w
-}
-
-func (ed *editor) moveDotRight() {
-	_, w := utf8.DecodeRuneInString(ed.buffer[ed.dot:])
-	ed.dot += w
-}
-
-func (ed *editor) moveDotLeftSepFunc(fn func(rune) bool) {
-	if ed.dot == 0 {
-		return
+func categorizeAlphanumeric(r rune) int {
+	switch {
+	case isAlnum(r):
+		return 1
+	default:
+		return 0
 	}
-	space := strings.LastIndexFunc(
-		strings.TrimRightFunc(ed.buffer[:ed.dot], fn),
-		fn) + 1
-	ed.dot = space
 }
 
-func (ed *editor) moveDotRightSepFunc(fn func(rune) bool) {
-	// Move to first matching rune
-	p := strings.IndexFunc(ed.buffer[ed.dot:], fn)
-	if p == -1 {
-		ed.dot = len(ed.buffer)
-		return
-	}
-	ed.dot += p
-	// Move to first non-matching rune
-	p = strings.IndexFunc(ed.buffer[ed.dot:], func(r rune) bool { return !fn(r) })
-	if p == -1 {
-		ed.dot = len(ed.buffer)
-		return
-	}
-	ed.dot += p
+func moveDotLeftWord(buffer string, dot int) int {
+	return moveDotLeftCategoryFunc(categorizeWord, buffer, dot)
+}
+func moveDotRightWord(buffer string, dot int) int {
+	return moveDotRightCategoryFunc(categorizeWord, buffer, dot)
+}
+func moveDotLeftSmallWord(buffer string, dot int) int {
+	return moveDotLeftCategoryFunc(categorizeSmallWord, buffer, dot)
+}
+func moveDotRightSmallWord(buffer string, dot int) int {
+	return moveDotRightCategoryFunc(categorizeSmallWord, buffer, dot)
+}
+func moveDotLeftAlphanumeric(buffer string, dot int) int {
+	return moveDotLeftCategoryFunc(categorizeAlphanumeric, buffer, dot)
+}
+func moveDotRightAlphanumeric(buffer string, dot int) int {
+	return moveDotRightCategoryFunc(categorizeAlphanumeric, buffer, dot)
 }
 
-func (ed *editor) moveDotLeftWord() {
-	ed.moveDotLeftSepFunc(unicode.IsSpace)
+func moveDotSOL(buffer string, dot int) int {
+	return util.FindLastSOL(buffer[:dot])
 }
 
-func (ed *editor) moveDotRightWord() {
-	ed.moveDotRightSepFunc(unicode.IsSpace)
-}
-
-func (ed *editor) moveDotLeftSmallWord() {
-	ed.moveDotLeftSepFunc(func(r rune) bool { return !isAlnum(r) })
-}
-
-func (ed *editor) moveDotRightSmallWord() {
-	ed.moveDotRightSepFunc(func(r rune) bool { return !isAlnum(r) })
-}
-
-func (ed *editor) moveDotSOL() {
-	sol := util.FindLastSOL(ed.buffer[:ed.dot])
-	ed.dot = sol
-}
-
-func (ed *editor) moveDotEOL() {
-	eol := util.FindFirstEOL(ed.buffer[ed.dot:]) + ed.dot
-	ed.dot = eol
+func moveDotEOL(buffer string, dot int) int {
+	return util.FindFirstEOL(buffer[dot:]) + dot
 }
 
 func (ed *editor) moveDotUp() {
---
+++ edit/edcore/insert_test.go
@@ -0,0 +1,167 @@
+package edcore
+
+import (
+	"testing"
+
+	"github.com/elves/elvish/tt"
+)
+
+// The string below is carefully chosen to test all word, small-word,
+// and alphanumeric move/kill functions, because it contains features
+// to set the different movement behaviors apart.  For convenience,
+// the string is annotated with carets (^) to indicate the first
+// indices of runs of word/small-word/alphanumeric characters (and
+// hence return values of corresponding move functions), and dots (.)
+// to indicate trailing characters in a run of non-skip characters of
+// the same category.  Indices are also annotated for convenience.
+//
+//   cd ~/downloads; rm -rf 2018aug07-pics/*;
+//   ^. ^........... ^. ^.. ^................  (word)
+//   ^. ^.^........^ ^. ^^. ^........^^...^..  (small-word)
+//   ^.   ^........  ^.  ^. ^........ ^...     (alphanumeric)
+//   01234567890123456789012345678901234567890
+//   0         1         2         3         4
+//
+//   word boundaries:         0 3      16 19    23
+//   small-word boundaries:   0 3 5 14 16 19 20 23 32 33 37
+//   alphanumeric boundaries: 0   5    16    20 23    33
+//
+var moveTestBuffer = "cd ~/downloads; rm -rf 2018aug07-pics/*;"
+
+var (
+	moveDotLeftTests = tt.Table{
+		tt.Args("foo", 0).Rets(0),
+		tt.Args("bar", 3).Rets(2),
+		tt.Args("精灵", 0).Rets(0),
+		tt.Args("精灵", 3).Rets(0),
+		tt.Args("精灵", 6).Rets(3),
+	}
+	moveDotRightTests = tt.Table{
+		tt.Args("foo", 0).Rets(1),
+		tt.Args("bar", 3).Rets(3),
+		tt.Args("精灵", 0).Rets(3),
+		tt.Args("精灵", 3).Rets(6),
+		tt.Args("精灵", 6).Rets(6),
+	}
+	// word boundaries: 0 3 16 19 23
+	moveDotLeftWordTests = tt.Table{
+		tt.Args(moveTestBuffer, 0).Rets(0),
+		tt.Args(moveTestBuffer, 1).Rets(0),
+		tt.Args(moveTestBuffer, 2).Rets(0),
+		tt.Args(moveTestBuffer, 3).Rets(0),
+		tt.Args(moveTestBuffer, 4).Rets(3),
+		tt.Args(moveTestBuffer, 16).Rets(3),
+		tt.Args(moveTestBuffer, 19).Rets(16),
+		tt.Args(moveTestBuffer, 23).Rets(19),
+		tt.Args(moveTestBuffer, 40).Rets(23),
+	}
+	moveDotRightWordTests = tt.Table{
+		tt.Args(moveTestBuffer, 0).Rets(3),
+		tt.Args(moveTestBuffer, 1).Rets(3),
+		tt.Args(moveTestBuffer, 2).Rets(3),
+		tt.Args(moveTestBuffer, 3).Rets(16),
+		tt.Args(moveTestBuffer, 16).Rets(19),
+		tt.Args(moveTestBuffer, 19).Rets(23),
+		tt.Args(moveTestBuffer, 23).Rets(40),
+	}
+
+	// small-word boundaries: 0 3 5 14 16 19 20 23 32 33 37
+	moveDotLeftSmallWordTests = tt.Table{
+		tt.Args(moveTestBuffer, 0).Rets(0),
+		tt.Args(moveTestBuffer, 1).Rets(0),
+		tt.Args(moveTestBuffer, 2).Rets(0),
+		tt.Args(moveTestBuffer, 3).Rets(0),
+		tt.Args(moveTestBuffer, 4).Rets(3),
+		tt.Args(moveTestBuffer, 5).Rets(3),
+		tt.Args(moveTestBuffer, 14).Rets(5),
+		tt.Args(moveTestBuffer, 16).Rets(14),
+		tt.Args(moveTestBuffer, 19).Rets(16),
+		tt.Args(moveTestBuffer, 20).Rets(19),
+		tt.Args(moveTestBuffer, 23).Rets(20),
+		tt.Args(moveTestBuffer, 32).Rets(23),
+		tt.Args(moveTestBuffer, 33).Rets(32),
+		tt.Args(moveTestBuffer, 37).Rets(33),
+		tt.Args(moveTestBuffer, 40).Rets(37),
+	}
+	moveDotRightSmallWordTests = tt.Table{
+		tt.Args(moveTestBuffer, 0).Rets(3),
+		tt.Args(moveTestBuffer, 1).Rets(3),
+		tt.Args(moveTestBuffer, 2).Rets(3),
+		tt.Args(moveTestBuffer, 3).Rets(5),
+		tt.Args(moveTestBuffer, 5).Rets(14),
+		tt.Args(moveTestBuffer, 14).Rets(16),
+		tt.Args(moveTestBuffer, 16).Rets(19),
+		tt.Args(moveTestBuffer, 19).Rets(20),
+		tt.Args(moveTestBuffer, 20).Rets(23),
+		tt.Args(moveTestBuffer, 23).Rets(32),
+		tt.Args(moveTestBuffer, 32).Rets(33),
+		tt.Args(moveTestBuffer, 33).Rets(37),
+		tt.Args(moveTestBuffer, 37).Rets(40),
+	}
+	
+	// alphanumeric boundaries: 0 5 16 20 23 33
+	moveDotLeftAlphanumericTests = tt.Table{
+		tt.Args(moveTestBuffer, 0).Rets(0),
+		tt.Args(moveTestBuffer, 1).Rets(0),
+		tt.Args(moveTestBuffer, 2).Rets(0),
+		tt.Args(moveTestBuffer, 3).Rets(0),
+		tt.Args(moveTestBuffer, 4).Rets(0),
+		tt.Args(moveTestBuffer, 5).Rets(0),
+		tt.Args(moveTestBuffer, 6).Rets(5),
+		tt.Args(moveTestBuffer, 16).Rets(5),
+		tt.Args(moveTestBuffer, 20).Rets(16),
+		tt.Args(moveTestBuffer, 23).Rets(20),
+		tt.Args(moveTestBuffer, 33).Rets(23),
+		tt.Args(moveTestBuffer, 40).Rets(33),
+	}
+	moveDotRightAlphanumericTests = tt.Table{
+		tt.Args(moveTestBuffer, 0).Rets(5),
+		tt.Args(moveTestBuffer, 1).Rets(5),
+		tt.Args(moveTestBuffer, 2).Rets(5),
+		tt.Args(moveTestBuffer, 3).Rets(5),
+		tt.Args(moveTestBuffer, 4).Rets(5),
+		tt.Args(moveTestBuffer, 5).Rets(16),
+		tt.Args(moveTestBuffer, 16).Rets(20),
+		tt.Args(moveTestBuffer, 20).Rets(23),
+		tt.Args(moveTestBuffer, 23).Rets(33),
+		tt.Args(moveTestBuffer, 33).Rets(40),
+	}
+)
+
+func TestMoveDotRune(t *testing.T) {
+	tt.Test(t, tt.Fn("moveDotLeft", moveDotLeft), moveDotLeftTests)
+	tt.Test(t, tt.Fn("moveDotRight", moveDotRight), moveDotRightTests)
+}
+
+func TestMoveDotWord(t *testing.T) {
+	tt.Test(t,
+		tt.Fn("moveDotLeftWord", moveDotLeftWord),
+		moveDotLeftWordTests,
+	)
+	tt.Test(t,
+		tt.Fn("moveDotRightWord", moveDotRightWord),
+		moveDotRightWordTests,
+	)
+}
+
+func TestMoveDotSmallWord(t *testing.T) {
+	tt.Test(t,
+		tt.Fn("moveDotLeftSmallWord", moveDotLeftSmallWord),
+		moveDotLeftSmallWordTests,
+	)
+	tt.Test(t,
+		tt.Fn("moveDotRightSmallWord", moveDotRightSmallWord),
+		moveDotRightSmallWordTests,
+	)
+}
+
+func TestMoveDotAlphanumeric(t *testing.T) {
+	tt.Test(t,
+		tt.Fn("moveDotLeftAlphanumeric", moveDotLeftAlphanumeric),
+		moveDotLeftAlphanumericTests,
+	)
+	tt.Test(t,
+		tt.Fn("moveDotRightAlphanumeric", moveDotRightAlphanumeric),
+		moveDotRightAlphanumericTests,
+	)
+}
