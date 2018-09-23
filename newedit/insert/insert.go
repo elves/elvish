bfb4922b16eea0bd94abf17617adb11b52c781d3
Qi Xiao <xiaqqaix@gmail.com>
2018-09-23 13:57:14 +0100

    newedit/insert: New package implementing insert mode.

---
+++ newedit/insert/insert.go
@@ -0,0 +1,166 @@
+// Package insert implements the insert mode for Elvish's editor.
+package insert
+
+import (
+	"strings"
+	"sync"
+
+	"github.com/elves/elvish/edit/tty"
+	"github.com/elves/elvish/edit/ui"
+	"github.com/elves/elvish/newedit/types"
+	"github.com/elves/elvish/newedit/utils"
+	"github.com/elves/elvish/parse"
+)
+
+// Mode represents the insert mode.
+type Mode struct {
+	// Function to handle keys.
+	KeyHandler func(ui.Key) types.HandlerAction
+	// Function that feeds all abbreviation pairs to the callback.
+	AbbrIterate func(func(abbr, full string))
+	// Configuration that can be modified concurrently.
+	Config Config
+
+	// Internal states.
+	inserts string
+	paste   pasteStatus
+	pastes  []rune
+}
+
+type pasteStatus int
+
+const (
+	noPaste pasteStatus = iota
+	quotePaste
+	literalPaste
+)
+
+type Config struct {
+	Raw   RawConfig
+	Mutex sync.RWMutex
+}
+
+func (c *Config) QuotePaste() bool {
+	c.Mutex.RLock()
+	defer c.Mutex.RUnlock()
+	return c.Raw.QuotePaste
+}
+
+type RawConfig struct {
+	// Whether to quote the bracketed-pasted text.
+	QuotePaste bool
+}
+
+var (
+	quotePasteModeLine   = ui.NewModeLineRenderer(" INSERT (pasting, quoted) ", "")
+	literalPasteModeLine = ui.NewModeLineRenderer(" INSERT (pasting, literal) ", "")
+)
+
+func (m *Mode) ModeLine() ui.Renderer {
+	switch {
+	case m.paste == quotePaste:
+		return quotePasteModeLine
+	case m.paste == literalPaste:
+		return literalPasteModeLine
+	default:
+		return nil
+	}
+}
+
+func (m *Mode) ModeRenderFlag() types.ModeRenderFlag {
+	return 0
+}
+
+func (m *Mode) HandleEvent(e tty.Event, st *types.State) types.HandlerAction {
+	switch e := e.(type) {
+	case tty.PasteSetting:
+		if e {
+			m.handlePasteStart()
+		} else {
+			m.handlePasteEnd(st)
+		}
+	case tty.KeyEvent:
+		k := ui.Key(e)
+		if m.paste != noPaste {
+			m.handleKeyInPaste(k)
+			return types.NoAction
+		}
+		return m.handleKey(k, st)
+	}
+	return types.NoAction
+}
+
+func (m *Mode) handlePasteStart() {
+	m.inserts = ""
+	if m.Config.QuotePaste() {
+		m.paste = quotePaste
+	} else {
+		m.paste = literalPaste
+	}
+}
+
+func (m *Mode) handlePasteEnd(st *types.State) {
+	text := string(m.pastes)
+	if m.paste == quotePaste {
+		text = parse.Quote(text)
+	}
+	insert(st, text)
+	m.pastes = nil
+	m.paste = noPaste
+}
+
+func (m *Mode) handleKeyInPaste(k ui.Key) {
+	if k.Mod != 0 || k.Rune < 0 {
+		// TODO: Notify user of the error, instead of silently dropping
+		// function keys.
+	} else {
+		m.pastes = append(m.pastes, k.Rune)
+	}
+}
+
+func (m *Mode) handleKey(k ui.Key, st *types.State) types.HandlerAction {
+	var action types.HandlerAction
+	if m.KeyHandler != nil {
+		action = m.KeyHandler(k)
+	} else {
+		action = utils.BasicHandler(tty.KeyEvent(k), st)
+	}
+	if k.Mod != 0 || k.Rune < 0 {
+		m.inserts = ""
+		return action
+	}
+	// Expand abbreviations.
+	//
+	// NOTE: If the user binds a non-function key to do something other than
+	// inserting the key itself, the behavior can be confusing.
+	m.inserts += string(k.Rune)
+	var abbr, full string
+	if m.AbbrIterate != nil {
+		m.AbbrIterate(func(a, f string) {
+			if strings.HasSuffix(m.inserts, a) && len(a) > len(abbr) {
+				abbr, full = a, f
+			}
+		})
+	}
+	if len(abbr) > 0 {
+		st.Mutex.Lock()
+		defer st.Mutex.Unlock()
+		raw := &st.Raw
+		// Make sure that abbr is to the left of the dot.
+		if strings.HasSuffix(raw.Code[:raw.Dot], abbr) {
+			// Replace abbr with full
+			beforeAbbr := raw.Code[:raw.Dot-len(abbr)]
+			raw.Code = beforeAbbr + full + raw.Code[raw.Dot:]
+			raw.Dot = len(beforeAbbr) + len(full)
+		}
+	}
+	return action
+}
+
+func insert(st *types.State, text string) {
+	st.Mutex.Lock()
+	defer st.Mutex.Unlock()
+	raw := &st.Raw
+	raw.Code = raw.Code[:raw.Dot] + text + raw.Code[raw.Dot:]
+	raw.Dot += len(text)
+}
---
+++ newedit/insert/insert_test.go
@@ -0,0 +1,140 @@
+package insert
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/elves/elvish/edit/tty"
+	"github.com/elves/elvish/edit/ui"
+	"github.com/elves/elvish/newedit/types"
+	"github.com/elves/elvish/parse"
+)
+
+func TestModeLine_Default(t *testing.T) {
+	m := &Mode{}
+	if m.ModeLine() != nil {
+		t.Errorf("got non-nil buf in default state")
+	}
+}
+
+func TestModeLine_LiteralPaste(t *testing.T) {
+	m := &Mode{}
+	st := &types.State{}
+
+	m.HandleEvent(tty.PasteSetting(true), st)
+	if m.ModeLine() != literalPasteModeLine {
+		t.Errorf("modeline != literalPasteModeLine during pasting")
+	}
+
+	m.HandleEvent(tty.PasteSetting(false), st)
+	if m.ModeLine() != nil {
+		t.Errorf("modeline != nil after pasting")
+	}
+}
+
+func TestModeLine_QuotePaste(t *testing.T) {
+	m := &Mode{}
+	m.Config.Raw.QuotePaste = true
+	st := &types.State{}
+
+	m.HandleEvent(tty.PasteSetting(true), st)
+
+	if m.ModeLine() != quotePasteModeLine {
+		t.Errorf("modeline != quotePasteModeLine during pasting with quote")
+	}
+}
+
+func TestHandleEvent_LiteralPaste(t *testing.T) {
+	testPaste(t, &Mode{}, "$100", "$100")
+}
+
+func TestHandleEvent_QuotePaste(t *testing.T) {
+	m := &Mode{}
+	m.Config.Raw.QuotePaste = true
+	testPaste(t, m, "$100", parse.Quote("$100"))
+}
+
+func testPaste(t *testing.T, m *Mode, input, want string) {
+	st := &types.State{}
+	st.Raw.Code = "[]"
+	st.Raw.Dot = 1
+
+	m.HandleEvent(tty.PasteSetting(true), st)
+	for _, r := range input {
+		m.HandleEvent(tty.KeyEvent{Rune: r}, st)
+	}
+	m.HandleEvent(tty.PasteSetting(false), st)
+
+	wantCode := "[" + want + "]"
+	if st.Raw.Code != wantCode {
+		t.Errorf("got code = %q, want %q", st.Raw.Code, wantCode)
+	}
+	if st.Raw.Dot != 1+len(want) {
+		t.Errorf("got dot = %v, want %v", st.Raw.Dot, 1+len(want))
+	}
+}
+
+var (
+	events = []tty.Event{
+		tty.KeyEvent{Rune: 'a'}, tty.KeyEvent{Rune: 'b'}, tty.MouseEvent{},
+		tty.KeyEvent{Rune: 'c'}, tty.RawRune('d')}
+	keyEvents = []ui.Key{{Rune: 'a'}, {Rune: 'b'}, {Rune: 'c'}}
+)
+
+func TestHandleEvent_CallsKeyHandler(t *testing.T) {
+	var keys []ui.Key
+	keyHandler := func(k ui.Key) types.HandlerAction {
+		keys = append(keys, k)
+		return types.NoAction
+	}
+	m := &Mode{KeyHandler: keyHandler}
+	st := &types.State{}
+
+	for _, event := range events {
+		m.HandleEvent(event, st)
+	}
+	if !reflect.DeepEqual(keys, keyEvents) {
+		t.Errorf("got keys %v, want %v", keys, keyEvents)
+	}
+}
+
+var abbrTests = []struct {
+	name     string
+	keys     []tty.KeyEvent
+	wantCode string
+}{
+	{"simple",
+		[]tty.KeyEvent{{Rune: 'x'}, {Rune: 'x'}},
+		"> /dev/null"},
+	{"suffix",
+		[]tty.KeyEvent{
+			{Rune: 'l'}, {Rune: ' '}, {Rune: 'x'}, {Rune: 'x'}},
+		"l > /dev/null"},
+	{"longest suffix",
+		[]tty.KeyEvent{
+			{Rune: 'l'}, {Rune: ' '}, {Rune: '2'}, {Rune: 'x'}, {Rune: 'x'}},
+		"l > &2"},
+	{"function key interrupts abbreviation",
+		[]tty.KeyEvent{
+			{Rune: 'x'}, {Rune: ui.Left}, {Rune: ui.Right}, {Rune: 'x'}},
+		"xx"},
+}
+
+func TestHandleEvent_ExpandsAbbr(t *testing.T) {
+	m := &Mode{}
+	m.AbbrIterate = func(f func(abbr, full string)) {
+		f("xx", "> /dev/null")
+		f("2xx", "> &2")
+	}
+	for _, test := range abbrTests {
+		t.Run(test.name, func(t *testing.T) {
+			st := &types.State{}
+			for _, k := range test.keys {
+				m.HandleEvent(k, st)
+			}
+			if st.Raw.Code != test.wantCode {
+				t.Errorf("got code = %q, want %q", st.Raw.Code, test.wantCode)
+			}
+		})
+	}
+}
