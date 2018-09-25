6b29eaef1d16914e5de3fb9ed0ecccacd8eb69d3
Qi Xiao <xiaqqaix@gmail.com>
2018-09-25 23:25:59 +0100

    eval/vals: Fix typo in comment

--- eval/vals/kind.go
+++ eval/vals/kind.go
@@ -15,7 +15,7 @@ type Kinder interface {
 // Kind returns the "kind" of the value, a concept similar to type but not yet
 // very well defined. It is implemented for the builtin types bool and string,
 // the Vector and Map types, and types implementing the Kinder interface. For
-// other types, it returns the Go type name of the argument preceeded by "!!".
+// other types, it returns the Go type name of the argument preceded by "!!".
 func Kind(v interface{}) string {
 	switch v := v.(type) {
 	case Kinder:
