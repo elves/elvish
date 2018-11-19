c42b1214288dd32d6bcfb89ef5a9926f6228889b
Qi Xiao <xiaqqaix@gmail.com>
2018-11-19 20:03:09 +0100

    newedit: Add common buffer builtins to the default binding.

--- newedit/default_bindings.go
+++ newedit/default_bindings.go
@@ -3,6 +3,25 @@ package newedit
 // Elvish code for default bindings, assuming the editor ns as the global ns.
 const defaultBindingsElv = `
 insert:binding = (binding-map [
+  &Left=  $move-left~
+  &Right= $move-right~
+
+  &Ctrl-Left=  $move-left-word~
+  &Ctrl-Right= $move-right-word~
+  &Alt-Left=   $move-left-word~
+  &Alt-Right=  $move-right-word~
+  &Alt-b=      $move-left-word~
+  &Alt-f=      $move-right-word~
+
+  &Home= $move-sol~
+  &End=  $move-eol~
+
+  &Backspace= $kill-left~
+  &Delete=    $kill-right~
+  &Ctrl-W=    $kill-left-word~
+  &Ctrl-U=    $kill-sol~
+  &Ctrl-K=    $kill-eol~
+
   &Ctrl-D=  $commit-eof~
   &Default= $insert:default-handler~
 ])
