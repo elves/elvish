25bdb6ecd7f96c2b533a8ab60411fd89e54f9798
Qi Xiao <xiaqqaix@gmail.com>
2018-12-07 20:19:58 +0000

    newedit/highlight: Make Highlighter opaque.

--- newedit/highlight/highlighter.go
+++ newedit/highlight/highlighter.go
@@ -1,22 +1,24 @@
 package highlight
 
 import (
-	"github.com/elves/elvish/parse"
 	"github.com/elves/elvish/styled"
 )
 
 // Highlighter is a code highlighter that can deliver results asynchronously.
 type Highlighter struct {
-	Check      func(n *parse.Chunk) error
-	HasCommand func(name string) bool
+	dep Dep
+}
+
+func NewHighlighter(dep Dep) *Highlighter {
+	return &Highlighter{dep}
 }
 
 // Get returns the highlighted code and static errors found in the code.
-func (hl Highlighter) Get(code string) (styled.Text, []error) {
-	return highlight(code, Dep{hl.Check, hl.HasCommand})
+func (hl *Highlighter) Get(code string) (styled.Text, []error) {
+	return highlight(code, hl.dep)
 }
 
 // LateUpdates returns a channel for notifying late updates.
-func (hl Highlighter) LateUpdates() <-chan struct{} {
+func (hl *Highlighter) LateUpdates() <-chan struct{} {
 	return nil
 }
