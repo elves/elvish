3927f7cf75e26c3bfeb6ca4bdfa2db057002356c
Qi Xiao <xiaqqaix@gmail.com>
2019-04-26 23:20:21 +0100

    Add _tests/cli.

--- _tests/cli/main.go
+++ _tests/cli/main.go
@@ -1,5 +1,27 @@
 // A test program for the cli package.
 package main
 
+import (
+	"fmt"
+	"io"
+
+	"github.com/elves/elvish/cli/clicore"
+)
+
 func main() {
+	app := clicore.NewAppFromStdIO()
+	for {
+		code, err := app.ReadCode()
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("error:", err)
+			}
+			break
+		}
+		fmt.Println("got:", code)
+		if code == "exit" {
+			fmt.Println("bye")
+			break
+		}
+	}
 }
