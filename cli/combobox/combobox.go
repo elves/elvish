4b05264d6c2d1690d54c8a0bbdce9fd92b7751af
Qi Xiao <xiaqqaix@gmail.com>
2019-08-18 23:11:09 +0100

    cli/combobox: Add package.

---
+++ cli/combobox/combobox.go
@@ -0,0 +1,63 @@
+// Package combobox implements the combobox widget, a combination of a listbox
+// and a codearea.
+package combobox
+
+import (
+	"github.com/elves/elvish/cli/clitypes"
+	"github.com/elves/elvish/cli/codearea"
+	"github.com/elves/elvish/cli/listbox"
+	"github.com/elves/elvish/cli/term"
+	"github.com/elves/elvish/edit/ui"
+)
+
+// Widget represents a combobox widget.
+type Widget struct {
+	CodeArea codearea.Widget
+	ListBox  listbox.Widget
+	OnFilter func(string)
+
+	// Whether filtering has ever been done.
+	hasFiltered bool
+	// Last filter value.
+	lastFilter string
+}
+
+var _ = clitypes.Widget(&Widget{})
+
+func (w *Widget) init() {
+	if w.OnFilter == nil {
+		w.OnFilter = func(string) {}
+	}
+	if !w.hasFiltered {
+		w.OnFilter("")
+		w.hasFiltered = true
+	}
+}
+
+// Render renders the codearea and the listbox below it.
+func (w *Widget) Render(width, height int) *ui.Buffer {
+	w.init()
+	buf := w.CodeArea.Render(width, height)
+	bufListBox := w.ListBox.Render(width, height-len(buf.Lines))
+	buf.Extend(bufListBox, false)
+	return buf
+}
+
+// Handle first lets the listbox handle the event, and if it is unhandled, lets
+// the codearea handle it. If the codearea has handled the event and the code
+// content has changed, it calls OnFilter with the new content.
+func (w *Widget) Handle(event term.Event) bool {
+	w.init()
+	if w.ListBox.Handle(event) {
+		return true
+	}
+	if w.CodeArea.Handle(event) {
+		filter := w.CodeArea.CopyState().CodeBuffer.Content
+		if filter != w.lastFilter {
+			w.OnFilter(filter)
+			w.lastFilter = filter
+		}
+		return true
+	}
+	return false
+}
---
+++ cli/combobox/combobox_test.go
@@ -0,0 +1,105 @@
+package combobox
+
+import (
+	"testing"
+
+	"github.com/elves/elvish/cli/clitypes"
+	"github.com/elves/elvish/cli/codearea"
+	"github.com/elves/elvish/cli/listbox"
+	"github.com/elves/elvish/cli/term"
+	"github.com/elves/elvish/edit/ui"
+	"github.com/elves/elvish/styled"
+)
+
+var renderTests = []clitypes.RenderTest{
+	{
+		Name: "rendering codearea and listbox",
+		Given: &Widget{
+			CodeArea: codearea.Widget{State: codearea.State{
+				CodeBuffer: codearea.CodeBuffer{Content: "filter", Dot: 6},
+			}},
+			ListBox: listbox.Widget{State: listbox.State{
+				Itemer: listbox.TestItemer{}, NItems: 2,
+			}},
+		},
+		Width: 10, Height: 24,
+		Want: ui.NewBufferBuilder(10).
+			WritePlain("filter").SetDotToCursor().
+			Newline().WriteStyled(styled.MakeText("item 0    ", "inverse")).
+			Newline().WritePlain("item 1"),
+	},
+	{
+		Name: "calling filter when rendering",
+		Given: installOnFilter(&Widget{
+			CodeArea: codearea.Widget{State: codearea.State{
+				CodeBuffer: codearea.CodeBuffer{Content: "filter", Dot: 6},
+			}},
+		}),
+		Width: 10, Height: 24,
+		Want: ui.NewBufferBuilder(10).
+			WritePlain("filter").SetDotToCursor().
+			Newline().WriteStyled(styled.MakeText("item 0    ", "inverse")).
+			Newline().WritePlain("item 1"),
+	},
+}
+
+func installOnFilter(w *Widget) *Widget {
+	w.OnFilter = func(string) {
+		w.ListBox.MutateListboxState(func(s *listbox.State) {
+			*s = listbox.State{Itemer: listbox.TestItemer{}, NItems: 2}
+		})
+	}
+	return w
+}
+
+func TestRender(t *testing.T) {
+	clitypes.TestRender(t, renderTests)
+}
+
+func TestHandle(t *testing.T) {
+	var onFilterCalled bool
+	var lastFilter string
+	w := &Widget{
+		ListBox: listbox.Widget{State: listbox.State{
+			Itemer: listbox.TestItemer{}, NItems: 2,
+		}},
+		OnFilter: func(filter string) {
+			onFilterCalled = true
+			lastFilter = filter
+		},
+	}
+
+	handled := w.Handle(term.K(ui.Down))
+	if !handled {
+		t.Errorf("listbox did not handle")
+	}
+	if w.ListBox.State.Selected != 1 {
+		t.Errorf("listbox state not changed")
+	}
+
+	handled = w.Handle(term.K('a'))
+	if !handled {
+		t.Errorf("codearea did not handle letter key")
+	}
+	if w.CodeArea.State.CodeBuffer.Content != "a" {
+		t.Errorf("codearea state not changed")
+	}
+	if lastFilter != "a" {
+		t.Errorf("OnFilter not called when codearea content changed")
+	}
+
+	onFilterCalled = false
+	handled = w.Handle(term.PasteSetting(true))
+	if !handled {
+		t.Errorf("codearea did not handle PasteSetting")
+	}
+	if onFilterCalled {
+		t.Errorf("OnFilter called when codearea content did not change")
+	}
+	w.Handle(term.PasteSetting(false))
+
+	handled = w.Handle(term.K('D', ui.Ctrl))
+	if handled {
+		t.Errorf("key unhandled by codearea and listbox got handled")
+	}
+}
