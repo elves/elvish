61963d486cf99461b907b8e2970a7f0b74556b10
Qi Xiao <xiaqqaix@gmail.com>
2019-08-25 15:17:41 +0100

    cli/layout: Make doc comment of ModeLine more accurate.

--- cli/layout/modeline.go
+++ cli/layout/modeline.go
@@ -2,8 +2,8 @@ package layout
 
 import "github.com/elves/elvish/styled"
 
-// ModePrompt returns a styled text that is suitable as the prompt in the codearea
-// of a combobox.
+// ModePrompt returns a callback suitable as the prompt in the codearea of a
+// combobox.
 func ModePrompt(content string, space bool) func() styled.Text {
 	p := styled.MakeText(content, "bold", "lightgray", "bg-magenta")
 	if space {
