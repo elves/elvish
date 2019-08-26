d4b9e5f993ab3cc50ddc16f7b054034fc7c8190e
Qi Xiao <xiaqqaix@gmail.com>
2019-08-26 21:47:35 +0100

    cliedit: Consolidate builtin files.

--- cliedit/builtins.go
+++ cliedit/builtins.go
@@ -7,9 +7,34 @@ import (
 
 	"github.com/elves/elvish/cli"
 	"github.com/elves/elvish/cli/codearea"
+	"github.com/elves/elvish/edit/eddefs"
 	"github.com/elves/elvish/eval"
 	"github.com/elves/elvish/util"
 )
+
+//elvdoc:fn binding-map
+//
+// Converts a normal map into a binding map.
+
+var makeBindingMap = eddefs.MakeBindingMap
+
+//elvdoc:fn commit-code
+//
+// Causes the Elvish REPL to end the current read iteration and evaluate the
+// code it just read. Internally, this works by raising a special exception.
+
+//elvdoc:fn commit-eof
+//
+// Causes the Elvish REPL to terminate. Internally, this works by raising a
+// special exception.
+
+func initMiscBuiltins(app *cli.App, ns eval.Ns) {
+	ns.AddGoFns("<edit>", map[string]interface{}{
+		"binding-map": makeBindingMap,
+		"commit-code": app.CommitCode,
+		"commit-eof":  app.CommitEOF,
+	})
+}
 
 var bufferBuiltinsData = map[string]func(*codearea.CodeBuffer){
 	"move-left":             makeMove(moveDotLeft),
