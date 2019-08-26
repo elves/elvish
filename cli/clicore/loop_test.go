4e5237a6a62e68343be7f884004f37145323f829
Qi Xiao <xiaqqaix@gmail.com>
2019-08-26 15:46:29 +0100

    cli/clicore: Merge loop_example_test.go into loop_test.go.

--- cli/clicore/loop_test.go
+++ cli/clicore/loop_test.go
@@ -1,6 +1,7 @@
 package clicore
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -120,4 +121,44 @@ func quitOn(retTrigger event, ret string) handleCb {
 	return func(e event) handleResult {
 		return handleResult{quit: e == retTrigger, buffer: ret}
 	}
+}
+
+func ExampleLoop() {
+	buffer := ""
+	firstDrawerCall := true
+	drawer := func(flag redrawFlag) {
+		// Because the consumption of events is batched, calls to the drawer is
+		// nondeterministic except for the first and final calls.
+		switch {
+		case firstDrawerCall:
+			fmt.Printf("initial buffer is %q\n", buffer)
+			firstDrawerCall = false
+		case flag&finalRedraw != 0:
+			fmt.Printf("final buffer is %q\n", buffer)
+		}
+	}
+	handler := func(ev event) handleResult {
+		if ev == '\n' {
+			return handleResult{quit: true, buffer: buffer}
+		}
+		buffer += string(ev.(rune))
+		return handleResult{}
+	}
+
+	ed := newLoop()
+	ed.HandleCb(handler)
+	go func() {
+		for _, event := range "echo\n" {
+			ed.Input(event)
+		}
+	}()
+	ed.RedrawCb(drawer)
+	buf, err := ed.Run()
+	fmt.Printf("returned buffer is %q\n", buf)
+	fmt.Printf("returned error is %v\n", err)
+	// Output:
+	// initial buffer is ""
+	// final buffer is "echo"
+	// returned buffer is "echo"
+	// returned error is <nil>
 }
