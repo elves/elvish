cc2df947acc6cd94b6815e34e2b636e0da21f2ef
Qi Xiao <xiaqqaix@gmail.com>
2019-09-01 12:41:28 +0100

    cli/colview: New package providing a column view widget.

---
+++ cli/colview/colview.go
@@ -0,0 +1,145 @@
+// Package colview provides a widget for arranging several widgets in a column.
+package colview
+
+import (
+	"sync"
+
+	"github.com/elves/elvish/cli/clitypes"
+	"github.com/elves/elvish/cli/term"
+	"github.com/elves/elvish/edit/ui"
+)
+
+// Widget is a colview widget.
+type Widget struct {
+	// Mutex for synchronizing access to State.
+	StateMutex sync.RWMutex
+	// Public state. Access concurrent to either of Widget's methods must be
+	// synchronized using StateMutex.
+	State State
+
+	// An overlay handler.
+	OverlayHandler clitypes.Handler
+	// A function that takes the number of columns and return weights for the
+	// widths of the columns. The returned slice must have a size of n. If this
+	// function is nil, all the columns will have the same weight.
+	Weights func(n int) []int
+}
+
+var _ = clitypes.Widget(&Widget{})
+
+// State keeps the state of the colview widget.
+type State struct {
+	Columns     []clitypes.Widget
+	FocusColumn int
+}
+
+func equalWeights(n int) []int {
+	weights := make([]int, n)
+	for i := 0; i < n; i++ {
+		weights[i] = 1
+	}
+	return weights
+}
+
+// Initializes nil members to sensible default values. This method is called at
+// the beginning of most public methods.
+func (w *Widget) init() {
+	if w.OverlayHandler == nil {
+		w.OverlayHandler = clitypes.DummyHandler{}
+	}
+	if w.Weights == nil {
+		w.Weights = equalWeights
+	}
+}
+
+// MutateColViewState calls the given function with a pointer to State, while
+// locking StateMutex.
+func (w *Widget) MutateColViewState(f func(*State)) {
+	w.StateMutex.Lock()
+	defer w.StateMutex.Unlock()
+	f(&w.State)
+}
+
+// CopyColViewState returns a copy of the state.
+func (w *Widget) CopyColViewState() State {
+	w.StateMutex.RLock()
+	defer w.StateMutex.RUnlock()
+	return w.State
+}
+
+const colGap = 1
+
+// Render renders all the columns side by side, putting the dot in the focused
+// column.
+func (w *Widget) Render(width, height int) *ui.Buffer {
+	w.init()
+
+	state := w.CopyColViewState()
+	ncols := len(state.Columns)
+	if ncols == 0 {
+		// No column.
+		return &ui.Buffer{Width: width}
+	}
+	if width < ncols {
+		// To narrow; give up by rendering nothing.
+		return &ui.Buffer{Width: width}
+	}
+	colWidths := distribute(width-(ncols-1)*colGap, w.Weights(ncols))
+	var buf ui.Buffer
+	for i, col := range state.Columns {
+		if i > 0 {
+			buf.Width += colGap
+		}
+		bufCol := col.Render(colWidths[i], height)
+		buf.ExtendRight(bufCol)
+	}
+	return &buf
+}
+
+// Handle handles the event first by consulting the overlay handler, and then
+// delegating the event to the currently focused column.
+func (w *Widget) Handle(event term.Event) bool {
+	w.init()
+
+	if w.OverlayHandler.Handle(event) {
+		return true
+	}
+	state := w.CopyColViewState()
+	if 0 <= state.FocusColumn && state.FocusColumn < len(state.Columns) {
+		return state.Columns[state.FocusColumn].Handle(event)
+	}
+	return false
+}
+
+// Distributes fullWidth according to the weights, rounding to integers.
+//
+// This works iteratively each step by taking the sum of all remaining weights,
+// and using floor(remainedWidth * currentWeight / remainedAllWeights) for the
+// current column.
+//
+// A simpler alternative is to simply use floor(fullWidth * currentWeight /
+// allWeights) at each step, and also giving the remainder to the last column.
+// However, this means that the last column gets all the rounding errors from
+// flooring, which can be big. The more sophisticated algorithm distributes the
+// rounding errors among all the remaining elements and can result in a much
+// better distribution, and as a special upside, does not need to handle the
+// last column as a special case.
+//
+// As an extreme example, consider the case of fullWidth = 9, weights = {1, 1,
+// 1, 1, 1} (five 1's). Using the simplistic algorithm, the widths are {1, 1, 1,
+// 1, 5}. Using the more complex algorithm, the widths are {1, 2, 2, 2, 2}.
+func distribute(fullWidth int, weights []int) []int {
+	remainedWidth := fullWidth
+	remainedWeight := 0
+	for _, weight := range weights {
+		remainedWeight += weight
+	}
+
+	widths := make([]int, len(weights))
+	for i, weight := range weights {
+		widths[i] = remainedWidth * weight / remainedWeight
+		remainedWidth -= widths[i]
+		remainedWeight -= weight
+	}
+	return widths
+}
---
+++ cli/colview/colview_test.go
@@ -0,0 +1,131 @@
+package colview
+
+import (
+	"testing"
+
+	"github.com/elves/elvish/cli/clitypes"
+	"github.com/elves/elvish/cli/listbox"
+	"github.com/elves/elvish/cli/term"
+	"github.com/elves/elvish/edit/ui"
+	"github.com/elves/elvish/styled"
+	"github.com/elves/elvish/tt"
+)
+
+var renderTests = []clitypes.RenderTest{
+	{
+		Name:  "colview no column",
+		Given: &Widget{},
+		Width: 10, Height: 24,
+		Want: &ui.Buffer{Width: 10},
+	},
+	{
+		Name: "colview width < number of columns",
+		Given: &Widget{State: State{
+			Columns: []clitypes.Widget{
+				makeListbox("x", 2, 0), makeListbox("y", 1, 0),
+				makeListbox("z", 3, 0), makeListbox("w", 1, 0),
+			},
+		}},
+		Width: 3, Height: 24,
+		Want: &ui.Buffer{Width: 3},
+	},
+	{
+		Name: "colview normal",
+		Given: &Widget{State: State{
+			Columns: []clitypes.Widget{
+				makeListbox("x", 2, 1),
+				makeListbox("y", 1, 0),
+				makeListbox("z", 3, -1),
+			},
+		}},
+		Width: 11, Height: 24,
+		Want: ui.NewBufferBuilder(11).
+			// first line
+			WritePlain("x0  ").
+			WriteStyled(styled.MakeText("y0 ", "inverse")).
+			WritePlain(" z0").
+			// second line
+			Newline().WriteStyled(styled.MakeText("x1 ", "inverse")).
+			WritePlain("     z1").
+			// third line
+			Newline().WritePlain("        z2"),
+	},
+}
+
+func makeListbox(prefix string, n, selected int) clitypes.Widget {
+	return &listbox.Widget{State: listbox.State{
+		Items:    listbox.TestItems{Prefix: prefix, NItems: n},
+		Selected: selected,
+	}}
+}
+
+func TestRender(t *testing.T) {
+	clitypes.TestRender(t, renderTests)
+}
+
+func TestHandle(t *testing.T) {
+	// Channel for recording the place an event was handled. -1 for the widget
+	// itself, column index for column.
+	handledBy := make(chan int, 10)
+	w := &Widget{
+		OverlayHandler: clitypes.MapHandler{
+			term.K('a'): func() { handledBy <- -1 },
+		},
+		State: State{
+			Columns: []clitypes.Widget{
+				&listbox.Widget{OverlayHandler: clitypes.MapHandler{
+					term.K('a'): func() { handledBy <- 0 },
+					term.K('b'): func() { handledBy <- 0 },
+				}},
+				&listbox.Widget{OverlayHandler: clitypes.MapHandler{
+					term.K('a'): func() { handledBy <- 1 },
+					term.K('b'): func() { handledBy <- 1 },
+				}},
+			},
+			FocusColumn: 1,
+		},
+	}
+
+	// Event handled by widget's overlay handler.
+	handled := w.Handle(term.K('a'))
+	if !handled {
+		t.Errorf("Handle -> false, want true")
+	}
+	if by := <-handledBy; by != -1 {
+		t.Errorf("Handled by %d, want -1", by)
+	}
+
+	// Event handled by the focused column.
+	handled = w.Handle(term.K('b'))
+	if !handled {
+		t.Errorf("Handle -> false, want true")
+	}
+	if by := <-handledBy; by != 1 {
+		t.Errorf("Handled by %d, want 1", by)
+	}
+
+	// Event unhandled.
+	handled = w.Handle(term.K('c'))
+	if handled {
+		t.Errorf("Handle -> true, want false")
+	}
+
+	// No focused column: event unhandled
+	w.MutateColViewState(func(s *State) { s.FocusColumn = -1 })
+	handled = w.Handle(term.K('b'))
+	if handled {
+		t.Errorf("Handle -> true, want false")
+	}
+}
+
+func TestDistribute(t *testing.T) {
+	tt.Test(t, tt.Fn("distribute", distribute), tt.Table{
+		// Nice integer distributions.
+		tt.Args(10, []int{1, 1}).Rets([]int{5, 5}),
+		tt.Args(10, []int{2, 3}).Rets([]int{4, 6}),
+		tt.Args(10, []int{1, 2, 2}).Rets([]int{2, 4, 4}),
+		// Approximate integer distributions.
+		tt.Args(10, []int{1, 1, 1}).Rets([]int{3, 3, 4}),
+		tt.Args(5, []int{1, 1, 1}).Rets([]int{1, 2, 2}),
+	})
+}
