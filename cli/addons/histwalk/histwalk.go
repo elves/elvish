4c1598daec2590452e7d181c19f5d3c11438d4b7
Qi Xiao <xiaqqaix@gmail.com>
2019-10-27 00:17:22 +0100

    cli/addons/histwalk: New addon for walking the history.

---
+++ cli/addons/histwalk/histwalk.go
@@ -0,0 +1,121 @@
+// Package histwalk implements the history walking addon.
+package histwalk
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/elves/elvish/cli"
+	"github.com/elves/elvish/cli/el"
+	"github.com/elves/elvish/cli/el/codearea"
+	"github.com/elves/elvish/cli/el/layout"
+	"github.com/elves/elvish/cli/histutil"
+	"github.com/elves/elvish/cli/term"
+	"github.com/elves/elvish/edit/ui"
+)
+
+var ErrHistWalkInactive = errors.New("the histwalk addon is not active")
+
+// Config keeps the configuration for the histwalk addon.
+type Config struct {
+	// Keybinding.
+	Binding el.Handler
+	// The history walker.
+	Walker histutil.Walker
+}
+
+type widget struct {
+	binding el.Handler
+	walker  histutil.Walker
+	onWalk  func()
+}
+
+func (w *widget) init() {
+	if w.binding == nil {
+		w.binding = el.DummyHandler{}
+	}
+	if w.onWalk == nil {
+		w.onWalk = func() {}
+	}
+}
+
+func (w *widget) Render(width, height int) *ui.Buffer {
+	w.init()
+	content := layout.ModeLine(
+		fmt.Sprintf(" HISTORY #%d ", w.walker.CurrentSeq()), false)
+	buf := ui.NewBufferBuilder(width).WriteStyled(content).Buffer()
+	buf.TrimToLines(0, height)
+	return buf
+}
+
+func (w *widget) Handle(event term.Event) bool {
+	w.init()
+	return w.binding.Handle(event)
+}
+
+// Start starts the histwalk addon.
+func Start(app *cli.App, cfg Config) {
+	if cfg.Walker == nil {
+		app.Notify("no history walker")
+		return
+	}
+	walker := cfg.Walker
+	prefix := walker.Prefix()
+	walker.Prev()
+	w := widget{binding: cfg.Binding, walker: walker}
+	w.onWalk = func() {
+		app.CodeArea.MutateCodeAreaState(func(s *codearea.State) {
+			s.PendingCode = codearea.PendingCode{
+				From: len(prefix), To: len(s.CodeBuffer.Content),
+				Content: walker.CurrentCmd()[len(prefix):],
+			}
+		})
+	}
+	w.init()
+	w.onWalk()
+	app.MutateAppState(func(s *cli.State) { s.Listing = &w })
+	app.Redraw(false)
+}
+
+// Prev walks to the previous entry in history. It returns ErrHistWalkInactive
+// if the histwalk addon is not active.
+func Prev(app *cli.App) error {
+	return walk(app, func(w *widget) error { return w.walker.Prev() })
+}
+
+// Next walks to the next entry in history. It returns ErrHistWalkInactive if
+// the histwalk addon is not active.
+func Next(app *cli.App) error {
+	return walk(app, func(w *widget) error { return w.walker.Next() })
+}
+
+// Close closes the histwalk addon. It does nothing if the histwalk addon is not
+// active.
+func Close(app *cli.App) {
+	app.MutateAppState(func(s *cli.State) {
+		if _, ok := s.Listing.(*widget); !ok {
+			return
+		}
+		s.Listing = nil
+		app.CodeArea.MutateCodeAreaState(func(s *codearea.State) {
+			s.PendingCode = codearea.PendingCode{}
+		})
+	})
+}
+
+func walk(app *cli.App, f func(*widget) error) error {
+	w, ok := getWidget(app)
+	if !ok {
+		return ErrHistWalkInactive
+	}
+	err := f(w)
+	if err == nil {
+		w.onWalk()
+	}
+	return err
+}
+
+func getWidget(app *cli.App) (*widget, bool) {
+	w, ok := app.CopyAppState().Listing.(*widget)
+	return w, ok
+}
---
+++ cli/addons/histwalk/histwalk_test.go
@@ -0,0 +1,86 @@
+package histwalk
+
+import (
+	"testing"
+
+	"github.com/elves/elvish/cli"
+	"github.com/elves/elvish/cli/el"
+	"github.com/elves/elvish/cli/el/codearea"
+	"github.com/elves/elvish/cli/el/layout"
+	"github.com/elves/elvish/cli/histutil"
+	"github.com/elves/elvish/cli/term"
+	"github.com/elves/elvish/edit/ui"
+	"github.com/elves/elvish/styled"
+)
+
+var styles = map[rune]string{
+	'-': "underlined",
+}
+
+func TestHistWalk(t *testing.T) {
+	app, ttyCtrl, cleanup := setup()
+	defer cleanup()
+
+	db := &histutil.TestDB{
+		//                 0       1        2         3        4         5
+		AllCmds: []string{"echo", "ls -l", "echo a", "ls -a", "echo a", "ls -a"},
+	}
+	walker := histutil.NewWalker(db, -1, nil, "ls")
+
+	app.CodeArea.State.CodeBuffer = codearea.CodeBuffer{Content: "ls", Dot: 2}
+
+	app.Redraw(false)
+	buf0 := ui.NewBufferBuilder(40).WritePlain("ls").SetDotToCursor().Buffer()
+	ttyCtrl.TestBuffer(t, buf0)
+
+	Start(app, Config{
+		Walker: walker,
+		Binding: el.MapHandler{
+			term.K(ui.Up):        func() { Prev(app) },
+			term.K(ui.Down):      func() { Next(app) },
+			term.K('[', ui.Ctrl): func() { Close(app) },
+		},
+	})
+	buf5 := makeBuf(
+		styled.MarkLines(
+			"ls -a", styles,
+			"  ---",
+		),
+		" HISTORY #5 ")
+	ttyCtrl.TestBuffer(t, buf5)
+
+	ttyCtrl.Inject(term.K(ui.Up))
+	// Skips item #3 as it is a duplicate.
+	buf1 := makeBuf(
+		styled.MarkLines(
+			"ls -l", styles,
+			"  ---",
+		),
+		" HISTORY #1 ")
+	ttyCtrl.TestBuffer(t, buf1)
+
+	ttyCtrl.Inject(term.K(ui.Down))
+	ttyCtrl.TestBuffer(t, buf5)
+
+	ttyCtrl.Inject(term.K('[', ui.Ctrl))
+	ttyCtrl.TestBuffer(t, buf0)
+}
+
+func makeBuf(codeArea styled.Text, modeline string) *ui.Buffer {
+	return ui.NewBufferBuilder(40).
+		WriteStyled(codeArea).
+		Newline().SetDotToCursor().
+		WriteStyled(layout.ModeLine(modeline, false)).
+		Buffer()
+}
+
+func setup() (*cli.App, cli.TTYCtrl, func()) {
+	tty, ttyCtrl := cli.NewFakeTTY()
+	ttyCtrl.SetSize(24, 40)
+	app := cli.NewApp(tty)
+	codeCh, _ := app.ReadCodeAsync()
+	return app, ttyCtrl, func() {
+		app.CommitEOF()
+		<-codeCh
+	}
+}
