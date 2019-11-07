087687fc6cdd3fbe66e6066d5a960b1e6f8218ef
Qi Xiao <xiaqqaix@gmail.com>
2019-11-07 23:57:37 +0100

    cliedit: Add $edit:selected-file; add tests for navigation.

--- cliedit/navigation.go
+++ cliedit/navigation.go
@@ -4,7 +4,13 @@ import (
 	"github.com/elves/elvish/cli"
 	"github.com/elves/elvish/cli/addons/navigation"
 	"github.com/elves/elvish/eval"
+	"github.com/elves/elvish/eval/vars"
 )
+
+//elvdoc:var selected-file
+//
+// Name of the currently selected file in navigation mode. $nil if not in
+// navigation mode.
 
 //elvdoc:var navigation:binding
 //
@@ -17,6 +23,16 @@ import (
 func initNavigation(app cli.App, ev *eval.Evaler, ns eval.Ns) {
 	bindingVar := newBindingVar(emptyBindingMap)
 	binding := newMapBinding(app, ev, bindingVar)
+
+	selectedFileVar := vars.FromGet(func() interface{} {
+		name := navigation.SelectedName(app)
+		if name == "" {
+			return nil
+		}
+		return name
+	})
+
+	ns.Add("selected-file", selectedFileVar)
 	ns.AddNs("navigation",
 		eval.Ns{
 			"binding": bindingVar,
---
+++ cliedit/navigation_test.go
@@ -0,0 +1,41 @@
+package cliedit
+
+import (
+	"os"
+	"testing"
+
+	"github.com/elves/elvish/cli/term"
+	"github.com/elves/elvish/edit/ui"
+	"github.com/elves/elvish/styled"
+	"github.com/elves/elvish/util"
+)
+
+func TestNavigation(t *testing.T) {
+	_, ttyCtrl, ev, cleanup := setupStarted()
+	defer cleanup()
+	util.ApplyDir(util.Dir{"d": util.Dir{"a": ""}})
+	err := os.Chdir("d")
+	if err != nil {
+		panic(err)
+	}
+
+	ttyCtrl.Inject(term.K('N', ui.Ctrl))
+	styles := map[rune]string{
+		'#': "blue inverse",
+		'-': "inverse",
+	}
+	wantBuf := bb().
+		WritePlain("~/d> ").Newline().SetDotToCursor().
+		WriteStyled(styled.MarkLines(
+			" d       a                    ", styles,
+			"####### --------------------- ",
+		)).
+		Buffer()
+	ttyCtrl.TestBuffer(t, wantBuf)
+
+	evalf(ev, `file = $edit:selected-file`)
+	wantFile := "a"
+	if file := ev.Global["file"].Get().(string); file != wantFile {
+		t.Errorf("Got $edit:selected-file %q, want %q", file, wantFile)
+	}
+}
