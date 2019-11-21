3c2c98efd897e18bd6c94899cf21232670478404
Qi Xiao <xiaqqaix@gmail.com>
2019-11-21 22:36:21 +0000

    cliedit/complete: Add a godoc comment.

--- cliedit/complete/filterers.go
+++ cliedit/complete/filterers.go
@@ -2,6 +2,8 @@ package complete
 
 import "strings"
 
+// FilterPrefix filters raw items by prefix. It can be used as a Filterer in
+// Config.
 func FilterPrefix(ctxName, seed string, items []RawItem) []RawItem {
 	var filtered []RawItem
 	for _, cand := range items {
