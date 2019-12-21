ab4bd872d70ff3589e6abb044abb9086b5f8c0ee
Qi Xiao <xiaqqaix@gmail.com>
2019-12-21 00:46:54 +0000

    diag: Add Error type.
    The Error type will supersede parse.Error and eval.CompilationError.

---
+++ diag/error.go
@@ -0,0 +1,27 @@
+package diag
+
+import "fmt"
+
+// Error represents an error with context that can be pretty-printed.
+type Error struct {
+	Type    string
+	Message string
+	Context SourceRange
+}
+
+// Error returns a plain text representation of the error.
+func (e *Error) Error() string {
+	return fmt.Sprintf("%s: %d-%d in %s: %s",
+		e.Type, e.Context.Begin, e.Context.End, e.Context.Name, e.Message)
+}
+
+// Range returns the range of the error.
+func (e *Error) Range() Ranging {
+	return e.Context.Range()
+}
+
+// PPrint pretty-prints the error.
+func (e *Error) PPrint(indent string) string {
+	header := fmt.Sprintf("%s: \033[31;1m%s\033[m\n", e.Type, e.Message)
+	return header + e.Context.PPrintCompact(indent+"  ")
+}
---
+++ diag/error_test.go
@@ -0,0 +1,38 @@
+package diag
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestError(t *testing.T) {
+	err := &Error{
+		Type:    "some error",
+		Message: "bad list",
+		Context: *parseSourceRange("echo [x]", "[", "]", true),
+	}
+
+	wantErrorString := "some error: 5-8 in [test]: bad list"
+	if gotErrorString := err.Error(); gotErrorString != wantErrorString {
+		t.Errorf("Error() -> %q, want %q", gotErrorString, wantErrorString)
+	}
+
+	wantRanging := Ranging{From: 5, To: 8}
+	if gotRanging := err.Range(); gotRanging != wantRanging {
+		t.Errorf("Range() -> %v, want %v", gotRanging, wantRanging)
+	}
+
+	culpritLineBegin = "<"
+	culpritLineEnd = ">"
+	wantPPrint := lines(
+		"some error: \033[31;1mbad list\033[m",
+		"[test], line 1: echo <[x]>",
+	)
+	if gotPPrint := err.PPrint(""); gotPPrint != wantPPrint {
+		t.Errorf("PPrint() -> %q, want %q", gotPPrint, wantPPrint)
+	}
+}
+
+func lines(lines ...string) string {
+	return strings.Join(lines, "\n")
+}
