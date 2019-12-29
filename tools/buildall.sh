2e93755ade1ec325677a702c5ab669c9224278ab
Qi Xiao <xiaqqaix@gmail.com>
2019-12-29 23:03:19 +0000

    Fix path of the buildinfo package in build commands.

--- tools/buildall.sh
+++ tools/buildall.sh
@@ -39,8 +39,8 @@ buildone() {
 
     echo -n "Building for $GOOS-$GOARCH... "
     go build -o $DST_DIR/$BIN -trimpath -ldflags \
-        "-X github.com/elves/elvish/buildinfo.Version=$VERSION \
-         -X github.com/elves/elvish/buildinfo.Reproducible=true" || {
+        "-X github.com/elves/elvish/pkg/buildinfo.Version=$VERSION \
+         -X github.com/elves/elvish/pkg/buildinfo.Reproducible=true" || {
         echo "Failed"
         return
     }
