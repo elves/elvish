e9a8d1be913e75cf2bf57eca68525d515f14930e
Qi Xiao <xiaqqaix@gmail.com>
2019-12-30 23:45:30 +0000

    pkg/edit: Support a &full flag in edit:redraw.
    Also use it in readline-binding. This fixes #883.

--- pkg/eval/bundled/readline-binding.elv.go
+++ pkg/eval/bundled/readline-binding.elv.go
@@ -14,7 +14,7 @@ b=[k f]{ edit:insert:binding[$k] = $f } {
     $b Ctrl-E $edit:move-dot-eol~
     $b Ctrl-F $edit:move-dot-right~
     $b Ctrl-H $edit:kill-rune-left~
-    $b Ctrl-L { clear > /dev/tty }
+    $b Ctrl-L { clear > /dev/tty; edit:redraw &full=$true }
     $b Ctrl-N $edit:end-of-history~
     # TODO: ^O
     $b Ctrl-P $edit:history:start~
