40cba03887a9b710c296acab1be7096649f2e852
Qi Xiao <xiaqqaix@gmail.com>
2019-12-31 16:50:44 +0000

    pkg/cli/term: Introduce a new fileReader helper.
    This helper can read from a file one byte at a time and can be stopped. It will
    be the basis of a new runeReader.

---
+++ pkg/cli/term/file_reader_unix_test.go
@@ -0,0 +1,74 @@
+// +build !windows,!plan9
+
+package term
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"testing"
+)
+
+func TestFileReader_ReadByte(t *testing.T) {
+	r, w, cleanup := setupFileReader()
+	defer cleanup()
+
+	content := []byte("0123456789")
+	w.Write(content)
+
+	// Test successful ReadByte calls.
+	for i := 0; i < len(content); i++ {
+		t.Run(fmt.Sprintf("byte %d", i), func(t *testing.T) {
+			b, err := r.ReadByte()
+			if err != nil {
+				t.Errorf("got err %v, want nil", err)
+			}
+			if b != content[i] {
+				t.Errorf("got byte %q, want %q", b, content[i])
+			}
+		})
+	}
+}
+
+func TestFileReader_ReadByte_EOF(t *testing.T) {
+	r, w, cleanup := setupFileReader()
+	defer cleanup()
+
+	w.Close()
+	_, err := r.ReadByte()
+	if err != io.EOF {
+		t.Errorf("got byte %v, want %v", err, io.EOF)
+	}
+}
+
+func TestFileReader_Stop(t *testing.T) {
+	r, _, cleanup := setupFileReader()
+	defer cleanup()
+
+	errCh := make(chan error, 1)
+	go func() {
+		_, err := r.ReadByte()
+		errCh <- err
+	}()
+	r.Stop()
+
+	if err := <-errCh; err != errStopped {
+		t.Errorf("got err %v, want %v", err, errStopped)
+	}
+}
+
+func setupFileReader() (reader fileReader, writer io.WriteCloser, cleanup func()) {
+	pr, pw, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+	r, err := newFileReader(pr)
+	if err != nil {
+		panic(err)
+	}
+	return r, pw, func() {
+		r.Close()
+		pr.Close()
+		pw.Close()
+	}
+}
