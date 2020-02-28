2108428157e9ab23740dd5e23c8dd04823a17b66
Kurtis Rader <krader@skepticism.us>
2020-02-27 21:37:11 -0800

    Tiny step to documenting the str: module

--- pkg/eval/str/str.go
+++ pkg/eval/str/str.go
@@ -8,6 +8,72 @@ import (
 	"github.com/elves/elvish/pkg/eval"
 )
 
+//elvdoc:fn compare
+//
+// ```elvish
+// str:compare $a $b
+// ```
+//
+// Compare returns an integer comparing two strings lexicographically. The
+// result will be 0 if a==b, -1 if a < b, and +1 if a > b.
+//
+// ```elvish-transcript
+// ~> str:compare a a
+// > 0
+// ~> str:compare a b
+// > -1
+// ~> str:compare b a
+// > 1
+// ```
+
+//elvdoc:fn contains
+//
+// ```elvish
+// str:contains $str $substr
+// ```
+//
+// Contains returns `$true` if `$substr` is within `$str` else `$false`.
+//
+// ```elvish-transcript
+// ~> str:contains abcd x
+// > $false
+// ~> str:contains abcd bc
+// > $true
+// ```
+
+//elvdoc:fn contains-any
+//
+// ```elvish
+// str:contains-any $str $chars
+// ```
+//
+// Contains-any returns `$true` if any Unicode code points in `$chars` is
+// within `$str` else `$false`.
+//
+// ```elvish-transcript
+// ~> str:contains-any abcd x
+// > $false
+// ~> str:contains-any abcd xby
+// > $true
+// ```
+
+//elvdoc:fn count
+//
+// ```elvish
+// str:count $str $substr
+// ```
+//
+// Count counts the number of non-overlapping instances of substr in s.
+// If substr is an empty string, Count returns 1 + the number of Unicode code
+// points in s.
+//
+// ```elvish-transcript
+// ~> str:count abcdefabcdef bc
+// > 2
+// ~> str:count abcdef ''
+// > 7
+// ```
+
 var Ns = eval.NewNs().AddGoFns("str:", fns)
 
 var fns = map[string]interface{}{
---
+++ website/ref/str.md
@@ -0,0 +1,10 @@
+<!-- toc -->
+
+# Introduction
+
+The `str:` module provides string manipulation functions.
+
+Function usages are given in the same format as in the
+reference doc for the [builtin module](builtin.html).
+
+@elvdoc -ns str: -dir ../pkg/eval/str
