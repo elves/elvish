2dc7d413079ab1e66beb5d103ce97384a1b46d72
Kurtis Rader <krader@skepticism.us>
2020-02-28 18:58:47 -0800

    Document another block of str: functions

--- pkg/eval/str/str.go
+++ pkg/eval/str/str.go
@@ -74,6 +74,102 @@ import (
 // > 7
 // ```
 
+//elvdoc:fn equal-fold
+//
+// ```elvish
+// str:equal-fold $str1 $str2
+// ```
+//
+// Equal-fold reports whether `$str1` and `$str2`, interpreted as UTF-8
+// strings, are equal under Unicode case-folding.
+//
+// ```elvish-transcript
+// ~> str:equal-fold ABC abc
+// > $true
+// ~> str:equal-fold abc ab
+// > $false
+// ```
+
+//elvdoc:fn has-prefix
+//
+// ```elvish
+// str:has-prefix $str $prefix
+// ```
+//
+// Has-prefix tests whether the string `$str` begins with `$prefix`
+//
+// ```elvish-transcript
+// ~> str:has-prefix abc ab
+// > $true
+// ~> str:has-prefix abc bc
+// > $false
+// ```
+
+//elvdoc:fn has-suffix
+//
+// ```elvish
+// str:has-suffix $str $suffix
+// ```
+//
+// Has-suffix tests whether the string `$str` begins with `$suffix`
+//
+// ```elvish-transcript
+// ~> str:has-suffix abc ab
+// > $false
+// ~> str:has-suffix abc bc
+// > $true
+// ```
+
+//elvdoc:fn index
+//
+// ```elvish
+// str:index $str $substr
+// ```
+//
+// Index returns the index of the first instance of `$substr` in `$str`, or -1
+// if `$substr` is not present in `$str`.
+
+//
+// ```elvish-transcript
+// ~> str:index abcd cd
+// > 2
+// ~> str:index abcd xyz
+// > -1
+// ```
+
+//elvdoc:fn index-any
+//
+// ```elvish
+// str:index-any $str $chars
+// ```
+//
+// Index-any returns the index of the first instance of any Unicode code point
+// from `$chars` in `$str`, or -1 if no Unicode code point from `$chars` is
+// present in `$str`.
+//
+// ```elvish-transcript
+// ~> str:index-any "chicken" "aeiouy"
+// > 2
+// ~> str:index-any l33t aeiouy
+// > -1
+// ```
+
+//elvdoc:fn last-index
+//
+// ```elvish
+// str:last-index $str $substr
+// ```
+//
+// Last-index returns the index of the last instance of `$substr` in `$str`,
+// or -1 if `$substr` is not present in `$str`.
+//
+// ```elvish-transcript
+// ~> str:last-index "elven speak elvish" elv
+// > 12
+// ~> str:last-index "elven speak elvish" romulan
+// > -1
+// ```
+
 var Ns = eval.NewNs().AddGoFns("str:", fns)
 
 var fns = map[string]interface{}{
