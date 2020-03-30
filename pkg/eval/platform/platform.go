a14d859a092aa3c0959d63ed1aeada7930a51917
Kurtis Rader <krader@skepticism.us>
2020-03-29 19:02:45 -0700

    Implement $platform:is-unix and is-windows
    This will facilitate elvish scripts determining whether it is safe to
    `use unix` or `use windows`. The implementation of the `unix` module is
    out for review in a separate commit. The `windows` module is likely to
    be implemented in the near future.

--- pkg/eval/platform/platform.go
+++ pkg/eval/platform/platform.go
@@ -23,7 +23,21 @@ import (
 // [`GOOS`](https://pkg.go.dev/runtime?tab=doc#pkg-constants) constant.
 // This is read-only.
 
+//elvdoc:var is-unix
+//
+// Whether or not the platform is UNIX-like. This includes Linux, macOS
+// (Darwin), FreeBSD, NetBSD, and OpenBSD. This can be used to decide, for
+// example, if the `unix` module is usable.
+// This is read-only.
+
+//elvdoc:var is-windows
+//
+// Whether or not the platform is Microsoft Windows.
+// This is read-only.
+
 var Ns = eval.Ns{
-	"arch": vars.NewReadOnly(runtime.GOARCH),
-	"os":   vars.NewReadOnly(runtime.GOOS),
+	"arch":       vars.NewReadOnly(runtime.GOARCH),
+	"os":         vars.NewReadOnly(runtime.GOOS),
+	"is-unix":    vars.NewReadOnly(isUnix),
+	"is-windows": vars.NewReadOnly(isWindows),
 }
