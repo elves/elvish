ccbad60ab9d9fa6e9300be2166d2cf50d53491a8
Qi Xiao <xiaqqaix@gmail.com>
2020-04-01 00:46:29 +0100

    pkg/ui: Test Style.SGR.
    This addresses #944.

--- pkg/ui/style_test.go
+++ pkg/ui/style_test.go
@@ -6,6 +6,22 @@ import (
 	"github.com/elves/elvish/pkg/tt"
 )
 
+func TestStyleSGR(t *testing.T) {
+	// Test the SGR sequences of style attributes indirectly via VTString of
+	// Text, since that is how they are used.
+	testTextVTString(t, []textVTStringTest{
+		{T("foo", Bold), "\033[1mfoo\033[m"},
+		{T("foo", Dim), "\033[2mfoo\033[m"},
+		{T("foo", Italic), "\033[3mfoo\033[m"},
+		{T("foo", Underlined), "\033[4mfoo\033[m"},
+		{T("foo", Blink), "\033[5mfoo\033[m"},
+		{T("foo", Inverse), "\033[7mfoo\033[m"},
+		{T("foo", FgRed), "\033[31mfoo\033[m"},
+		{T("foo", BgRed), "\033[41mfoo\033[m"},
+		{T("foo", Bold, FgRed, BgBlue), "\033[1;31;44mfoo\033[m"},
+	})
+}
+
 func TestStyleFromSGR(t *testing.T) {
 	tt.Test(t, tt.Fn("StyleFromSGR", StyleFromSGR), tt.Table{
 		tt.Args("1").Rets(Style{Bold: true}),
