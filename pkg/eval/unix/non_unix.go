ca864a440db75a8f6028c8ee4ddd08d971f889ce
Qi Xiao <xiaqqaix@gmail.com>
2020-04-03 02:23:14 +0100

    pkg/eval/unix: Fix godoc comments.

--- pkg/eval/unix/non_unix.go
+++ pkg/eval/unix/non_unix.go
@@ -1,15 +1,18 @@
 // +build  windows plan9 js
 
-// Package unix exposes variables and functions that deal with features
-// unique to UNIX-like operating systems. On non-UNIX operating systems it
-// will be an empty namespace.
+// Package unix exports an Elvish namespace that contains variables and
+// functions that deal with features unique to UNIX-like operating systems. On
+// non-UNIX operating systems it exports an empty namespace.
 package unix
 
 import (
 	"github.com/elves/elvish/pkg/eval"
 )
 
-// Indicate that this module should not be exposed as a usable elvish namespace.
+// ExposeUnixNs indicate whether this module should be exposed as a usable
+// elvish namespace.
 const ExposeUnixNs = false
 
+// Ns is an Elvish namespace that contains variables and functions that deal
+// with features unique to UNIX-like operating systems. On
 var Ns = eval.Ns{}
--- pkg/eval/unix/unix.go
+++ pkg/eval/unix/unix.go
@@ -1,17 +1,20 @@
 // +build !windows,!plan9,!js
 
-// Package unix exposes variables and functions that deal with features
-// unique to UNIX-like operating systems. On non-UNIX operating systems it
-// will be an empty namespace.
+// Package unix exports an Elvish namespace that contains variables and
+// functions that deal with features unique to UNIX-like operating systems. On
+// non-UNIX operating systems it exports an empty namespace.
 package unix
 
 import (
 	"github.com/elves/elvish/pkg/eval"
 )
 
-// Indicate that this module should be exposed as a usable elvish namespace.
+// ExposeUnixNs indicate whether this module should be exposed as a usable
+// elvish namespace.
 const ExposeUnixNs = true
 
+// Ns is an Elvish namespace that contains variables and functions that deal
+// with features unique to UNIX-like operating systems. On
 var Ns = eval.Ns{
 	"umask": UmaskVariable{},
 }
