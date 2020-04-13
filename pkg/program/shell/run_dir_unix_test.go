ee13ab6e5beb7c8eb6132460ada3505217dd24c7
Valentin Hăloiu <valentin.haloiu@gmail.com>
2020-04-13 14:26:34 +0200

    Check for existing runtime dirs first

--- pkg/program/shell/run_dir_unix_test.go
+++ pkg/program/shell/run_dir_unix_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"reflect"
 	"syscall"
 	"testing"
 
@@ -80,6 +81,83 @@ func TestGetSecureRunDir(t *testing.T) {
 			}
 			if stat.Mode&077 != 0 {
 				t.Errorf("Invalid run dir permissions")
+			}
+		})
+	}
+}
+
+func TestGetSecureRunDir_PreferExistingTmpDir(t *testing.T) {
+	xdgRuntimeDir, xdgCleanup := util.TestDir()
+	defer xdgCleanup()
+
+	tmpDir, tmpCleanup := util.TestDir()
+	defer tmpCleanup()
+
+	uid := os.Getuid()
+
+	tmpDirPath := filepath.Join(tmpDir, fmt.Sprintf("elvish-%d", uid))
+	err := os.MkdirAll(tmpDirPath, 0700)
+	if err != nil {
+		t.Errorf("Could not create test run dir: %v", err)
+	}
+
+	envOverrides := map[string]string{
+		"XDG_RUNTIME_DIR": xdgRuntimeDir,
+		"TMPDIR":          tmpDir,
+	}
+
+	restore := withTempEnvs(envOverrides)
+	defer restore()
+
+	runDir, err := getSecureRunDir()
+	if err != nil {
+		t.Errorf("Could not get secure run dir: %v", err)
+	}
+
+	if runDir != tmpDirPath {
+		t.Errorf("Got run dir %v, want %v", runDir, tmpDirPath)
+	}
+}
+
+func TestGetRunDirPaths(t *testing.T) {
+	uid := os.Getuid()
+
+	tests := []struct {
+		name          string
+		xdgRuntimeDir string
+		tmpdir        string
+		want          []string
+	}{
+		{
+			name:          "use XDG_RUNTIME_DIR when set",
+			xdgRuntimeDir: "/foo/runtime",
+			tmpdir:        "/foo/tmp",
+			want:          []string{"/foo/runtime/elvish", fmt.Sprintf("/foo/tmp/elvish-%d", uid)},
+		},
+		{
+			name:          "do not use XDG_RUNTIME_DIR when not set",
+			xdgRuntimeDir: "",
+			tmpdir:        "/foo/tmp",
+			want:          []string{fmt.Sprintf("/foo/tmp/elvish-%d", uid)},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Helper()
+
+			envOverrides := map[string]string{
+				"XDG_RUNTIME_DIR": test.xdgRuntimeDir,
+				"TMPDIR":          test.tmpdir,
+			}
+
+			restore := withTempEnvs(envOverrides)
+			defer restore()
+
+			runDirPaths := getRunDirPaths()
+			wantDirPaths := test.want
+			if !reflect.DeepEqual(runDirPaths, wantDirPaths) {
+				t.Errorf("Got run dir paths %v, want %v", runDirPaths, wantDirPaths)
 			}
 		})
 	}
