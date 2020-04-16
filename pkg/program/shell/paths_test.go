382bd087f0206acc090c358169f432388b8be3bd
Qi Xiao <xiaqqaix@gmail.com>
2020-04-16 21:25:12 +0100

    pkg/program/shell: More tests for paths.go.
    This addresses #944.

--- pkg/program/shell/paths_test.go
+++ pkg/program/shell/paths_test.go
@@ -4,11 +4,13 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/elves/elvish/pkg/util"
 )
 
 var j = filepath.Join
 
-func TestMakePaths_PopulatesSubPaths(t *testing.T) {
+func TestMakePaths_PopulatesUnsetSubPaths(t *testing.T) {
 	paths := MakePaths(os.Stderr, Paths{RunDir: "run", DataDir: "data", Bin: "bin"})
 	wantPaths := Paths{
 		RunDir:          "run",
@@ -24,5 +26,40 @@ func TestMakePaths_PopulatesSubPaths(t *testing.T) {
 	}
 	if paths != wantPaths {
 		t.Errorf("got paths %v, want %v", paths, wantPaths)
+	}
+}
+
+func TestMakePaths_RespectsSetSubPaths(t *testing.T) {
+	sock := "sock-override"
+	paths := MakePaths(os.Stderr, Paths{
+		RunDir: "run", DataDir: "data", Bin: "bin",
+		Sock: sock,
+	})
+	if paths.Sock != sock {
+		t.Errorf("got paths.Sock = %q, want %q", paths.Sock, sock)
+	}
+}
+
+func TestMakePaths_SetsAndCreatesDataDir(t *testing.T) {
+	home, cleanupDir := util.TestDir()
+	defer cleanupDir()
+	cleanupEnv := util.WithTempEnv("HOME", home)
+	defer cleanupEnv()
+
+	paths := MakePaths(os.Stderr, Paths{
+		RunDir: "run", Bin: "bin",
+	})
+
+	wantDataDir := home + "/.elvish"
+	if paths.DataDir != wantDataDir {
+		t.Errorf("paths.DataDir = %q, want %q", paths.DataDir, wantDataDir)
+	}
+
+	stat, err := os.Stat(paths.DataDir)
+	if err != nil {
+		t.Errorf("could not stat %q: %v", paths.DataDir, err)
+	}
+	if !stat.IsDir() {
+		t.Errorf("data dir %q is not dir", paths.DataDir)
 	}
 }
