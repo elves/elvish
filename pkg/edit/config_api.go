830103ddeb1b8d1dc6e885ed765a34d83e8402ac
xofyarg <xofyarg@gmail.com>
2020-05-16 14:49:04 -0700

    edit: introduce filter and add-cmd-filters
    Similar to `callHooks`, this change add a `callFilters` to run
    filters. A filter is function which takes input and output a boolean
    to indicate if the input passed or not. List of filter is chained by
    logical AND.
    
    `add-cmd-filters` will be called on current command prior to saving it
    into history, which gives the user flexibility to decide whether or
    not a command should be saved.

--- pkg/edit/config_api.go
+++ pkg/edit/config_api.go
@@ -49,6 +49,16 @@ func initAfterReadline(appSpec *cli.AppSpec, ev *eval.Evaler, ns eval.Ns) {
 	})
 }
 
+//elvdoc:var add-cmd-filters
+//
+// List of filters to run before adding a command to history.
+//
+// A filter is a function that takes a command as argument and outputs
+// a boolean value. If any of the filters outputs `$false`, the
+// command is not saved to history, and the rest of the filters are
+// not run. The default value of this list contains a filter which
+// ignores command starts with space.
+
 func callHooks(ev *eval.Evaler, name string, hook vals.List, args ...interface{}) {
 	i := -1
 	for it := hook.Iterator(); it.HasElem(); it.Next() {
@@ -68,6 +78,44 @@ func callHooks(ev *eval.Evaler, name string, hook vals.List, args ...interface{}
 		fm := eval.NewTopFrame(ev, parse.Source{Name: name}, ports)
 		fn.Call(fm, args, eval.NoOpts)
 	}
+}
+
+func callFilters(ev *eval.Evaler, name string, filters vals.List, args ...interface{}) bool {
+	i := -1
+	for it := filters.Iterator(); it.HasElem(); it.Next() {
+		i++
+		name := fmt.Sprintf("%s[%d]", name, i)
+		fn, ok := it.Elem().(eval.Callable)
+		if !ok {
+			// TODO(xiaq): This is not testable as it depends on stderr.
+			// Make it testable.
+			diag.Complainf(os.Stderr, "%s not function", name)
+			continue
+		}
+		// TODO(xiaq): This should use stdPorts, but stdPorts is currently
+		// unexported from eval.
+		ports := []*eval.Port{
+			eval.DevNullClosedChan, {File: os.Stdout}, {File: os.Stderr}}
+		fm := eval.NewTopFrame(ev, parse.Source{Name: name}, ports)
+		out, err := fm.CaptureOutput(func(fm *eval.Frame) error { return fn.Call(fm, args, eval.NoOpts) })
+		if err != nil {
+			diag.Complainf(os.Stderr, "%s return error", name)
+			continue
+		}
+		if len(out) != 1 {
+			diag.Complainf(os.Stderr, "filter %s should only return $true or $false", name)
+			continue
+		}
+		p, ok := out[0].(bool)
+		if !ok {
+			diag.Complainf(os.Stderr, "filter %s should return bool", name)
+			continue
+		}
+		if !p {
+			return false
+		}
+	}
+	return true
 }
 
 func newIntVar(i int) vars.PtrVar            { return vars.FromPtr(&i) }
