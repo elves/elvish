90860c1ba2aaea433331e77f9fa234c36c2abe7d
Kurtis Rader <krader@skepticism.us>
2020-05-19 20:28:33 -0700

    Implement platform hostname command
    Resolves #1028

--- pkg/eval/platform/platform_test.go
+++ pkg/eval/platform/platform_test.go
@@ -1,15 +1,34 @@
 package platform
 
 import (
+	"errors"
 	"runtime"
 	"testing"
 
 	"github.com/elves/elvish/pkg/eval"
 )
 
+const (
+	testHostname = "mach1.domain.tld"
+	testMachname = "mach1"
+)
+
+var hostnameFail = true
+
+func hostnameMock() (string, error) {
+	if hostnameFail {
+		hostnameFail = false
+		return "", errors.New("hostname cannot be determined")
+	}
+	return testHostname, nil
+}
+
 var That = eval.That
 
 func TestPlatform(t *testing.T) {
+	savedOsHostname := osHostname
+	osHostname = hostnameMock
+	defer func() { osHostname = savedOsHostname }()
 	setup := func(ev *eval.Evaler) { ev.Builtin.AddNs("platform", Ns) }
 	eval.TestWithSetup(t, setup,
 		That(`put $platform:arch`).Puts(runtime.GOARCH),
@@ -17,5 +36,10 @@ func TestPlatform(t *testing.T) {
 		That(`put $platform:is-windows`).Puts(runtime.GOOS == "windows"),
 		That(`put $platform:is-unix`).Puts(
 			runtime.GOOS != "windows" && runtime.GOOS != "plan9" && runtime.GOOS != "js"),
+		// The first time we invoke the mock it acts as if we can't determine
+		// the hostname. Make sure that is turned into the expected exception.
+		That(`platform:hostname`).ThrowsCause(errCannotDetermineHostname),
+		That(`platform:hostname`).Puts(testHostname),
+		That(`platform:hostname &strip-domain`).Puts(testMachname),
 	)
 }
