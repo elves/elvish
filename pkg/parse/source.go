28c076d64e1f136f65e514a89bee0cea734fa6dd
Qi Xiao <xiaqqaix@gmail.com>
2020-06-28 01:52:11 +0100

    pkg/parse: Convert Source into a structmap.

--- pkg/parse/source.go
+++ pkg/parse/source.go
@@ -2,9 +2,6 @@ package parse
 
 import (
 	"fmt"
-
-	"github.com/elves/elvish/pkg/util"
-	"github.com/xiaq/persistent/hash"
 )
 
 // TODO(xiaq): Move this into the diag package after implementing phantom types.
@@ -21,53 +18,22 @@ func SourceForTest(code string) Source {
 	return Source{Name: "[test]", Code: code}
 }
 
-func (src Source) Kind() string {
-	return "map"
-}
+// IsStructMap marks that Source is a structmap.
+func (src Source) IsStructMap() {}
 
-func (src Source) Hash() uint32 {
-	return hash.DJB(
-		hash.String(src.Name), hash.String(src.Code), hashBool(src.IsFile))
-}
-
-func hashBool(b bool) uint32 {
-	if b {
-		return 1
-	}
-	return 0
-}
-
-func (src Source) Equal(other interface{}) bool {
-	if src2, ok := other.(Source); ok {
-		return src == src2
-	}
-	return false
-}
-
+// Repr returns the representation of Source as if it were a map, except that
+// the code field is replaced by "...", since it is typically very large.
 func (src Source) Repr(int) string {
 	return fmt.Sprintf(
-		"<src name:%s code:... is-file:$%v>", Quote(src.Name), src.IsFile)
+		"[&name=%s &code=<...> &is-file=$%v]", Quote(src.Name), src.IsFile)
 }
 
-func (src Source) Index(k interface{}) (interface{}, bool) {
-	switch k {
-	case "name":
-		return src.Name, true
-	case "code":
-		return src.Code, true
-	case "path":
-		// For backward compatibility; remove after 0.14 release.
-		if src.IsFile {
-			return src.Name, true
-		}
-		return "", true
-	case "is-file":
-		return src.IsFile, true
-	default:
-		return nil, false
+// Path returns src.Name if src.IsFile is true, and an empty string otherwise.
+// It is kept for backward compatibility and will be removed after the 0.14
+// release.
+func (src Source) Path() string {
+	if src.IsFile {
+		return src.Name
 	}
-}
-
-func (src Source) IterateKeys(f func(interface{}) bool) {
-	util.Feed(f, "name", "code", "is-file")
+	return ""
 }
--- pkg/parse/source_test.go
+++ pkg/parse/source_test.go
@@ -5,24 +5,16 @@ import (
 
 	"github.com/elves/elvish/pkg/eval/vals"
 	. "github.com/elves/elvish/pkg/parse"
-	"github.com/xiaq/persistent/hash"
 )
 
-func TestSourceAsValue(t *testing.T) {
-	vals.TestValue(t, &Source{Name: "[tty]", Code: "echo"}).
-		Kind("map").
-		Hash(hash.DJB(hash.String("[tty]"), hash.String("echo"), 0)).
-		Equal(Source{Name: "[tty]", Code: "echo"}).
-		NotEqual(Source{Name: "[tty]", Code: "put"}).
-		Repr("<src name:'[tty]' code:... is-file:$false>").
-		AllKeys("name", "code", "is-file").
-		Index("name", "[tty]").
-		Index("code", "echo").
-		Index("is-file", false).
+func TestSourceAsStructMap(t *testing.T) {
+	vals.TestValue(t, Source{Name: "[tty]", Code: "echo"}).
+		Kind("structmap").
+		Repr("[&name='[tty]' &code=<...> &is-file=$false]").
+		AllKeys("name", "code", "is-file", "path").
 		Index("path", "")
 
-	vals.TestValue(t, &Source{Name: "/etc/rc.elv", Code: "echo", IsFile: true}).
-		Hash(hash.DJB(hash.String("/etc/rc.elv"), hash.String("echo"), 1)).
+	vals.TestValue(t, Source{Name: "/etc/rc.elv", Code: "echo", IsFile: true}).
 		Index("is-file", true).
 		Index("path", "/etc/rc.elv")
 }
