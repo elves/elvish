62298e4e3017a0fb89aded894db431545fd9a2a0
Shengjing Zhu <i@zhsj.me>
2020-07-11 01:41:14 +0800

    pkg/eval: Don't hardcode signal name in test
    On gccgo signal name is capitalized.

--- pkg/eval/exception_unix_test.go
+++ pkg/eval/exception_unix_test.go
@@ -15,13 +15,13 @@ func TestExternalCmdExit_Error(t *testing.T) {
 	tt.Test(t, tt.Fn("Error", error.Error), tt.Table{
 		tt.Args(ExternalCmdExit{0x0, "ls", 1}).Rets("ls exited with 0"),
 		tt.Args(ExternalCmdExit{0x100, "ls", 1}).Rets("ls exited with 1"),
-		// Note: all Unix'es have SIGINT = 2 and the syscall package has same
-		// string for it ("interrupt").
-		tt.Args(ExternalCmdExit{0x2, "ls", 1}).Rets("ls killed by signal interrupt"),
+		// Note: all Unix'es have SIGINT = 2, but syscall package has different
+		// string in gccgo("Interrupt") and gc("interrupt").
+		tt.Args(ExternalCmdExit{0x2, "ls", 1}).Rets("ls killed by signal " + syscall.SIGINT.String()),
 		// 0x80 + signal for core dumped
-		tt.Args(ExternalCmdExit{0x82, "ls", 1}).Rets("ls killed by signal interrupt (core dumped)"),
+		tt.Args(ExternalCmdExit{0x82, "ls", 1}).Rets("ls killed by signal " + syscall.SIGINT.String() + " (core dumped)"),
 		// 0x7f + signal<<8 for stopped
-		tt.Args(ExternalCmdExit{0x27f, "ls", 1}).Rets("ls stopped by signal interrupt (pid=1)"),
+		tt.Args(ExternalCmdExit{0x27f, "ls", 1}).Rets("ls stopped by signal " + syscall.SIGINT.String() + " (pid=1)"),
 	})
 	if runtime.GOOS == "linux" {
 		tt.Test(t, tt.Fn("Error", error.Error), tt.Table{
