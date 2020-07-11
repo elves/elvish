7ddcc5b1b58555ee19f730c4b72318cd38e16db1
Qi Xiao <xiaqqaix@gmail.com>
2020-07-11 14:20:01 +0100

    pkg/sys: Document why it's necessary to call signal.Ignore.

--- pkg/sys/signals_unix.go
+++ pkg/sys/signals_unix.go
@@ -12,6 +12,9 @@ func NotifySignals() chan os.Signal {
 	// This catches every signal regardless of whether it is ignored.
 	sigCh := make(chan os.Signal, sigsChanBufferSize)
 	signal.Notify(sigCh)
+	// Calling signal.Notify will reset the signal ignore status, so we need to
+	// call signal.Ignore every time we call signal.Notify.
+	//
 	// TODO: Remove this if, and when, job control is implemented. This
 	// handles the case of running an external command from an interactive
 	// prompt.
