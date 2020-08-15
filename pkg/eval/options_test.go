e25ee599db257b251d974748a1532eb7289ce060
Kurtis Rader <krader@skepticism.us>
2020-08-14 20:08:45 -0700

    Increase options.go coverage to 100%
    Related #1062

--- pkg/eval/options_test.go
+++ pkg/eval/options_test.go
@@ -1,28 +1,47 @@
 package eval
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 type opts struct {
 	FooBar string
 	POSIX  bool `name:"posix"`
 	Min    int
+	ignore bool // this should be ignored since it isn't exported
 }
 
 var scanOptionsTests = []struct {
 	rawOpts  RawOptions
 	preScan  opts
 	postScan opts
+	err      error
 }{
 	{RawOptions{"foo-bar": "lorem ipsum"},
-		opts{}, opts{FooBar: "lorem ipsum"}},
+		opts{}, opts{FooBar: "lorem ipsum"}, nil},
 	{RawOptions{"posix": true},
-		opts{}, opts{POSIX: true}},
+		opts{}, opts{POSIX: true}, nil},
+	// Since "ignore" is not exported it will result in an error when used.
+	{RawOptions{"ignore": true},
+		opts{}, opts{ignore: false}, errors.New("unknown option ignore")},
 }
 
 func TestScanOptions(t *testing.T) {
+	// scanOptions requires a pointer to struct.
+	err := scanOptions(RawOptions{}, opts{})
+	if err == nil {
+		t.Errorf("Scan should have reported invalid options arg error")
+	}
+
 	for _, test := range scanOptionsTests {
 		opts := test.preScan
-		scanOptions(test.rawOpts, &opts)
+		err := scanOptions(test.rawOpts, &opts)
+
+		if ((err == nil) != (test.err == nil)) ||
+			(err != nil && test.err != nil && err.Error() != test.err.Error()) {
+			t.Errorf("Scan error mismatch %v: want %q, got %q", test.rawOpts, test.err, err)
+		}
 		if opts != test.postScan {
 			t.Errorf("Scan %v => %v, want %v", test.rawOpts, opts, test.postScan)
 		}
