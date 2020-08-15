a3cb1627a3d83cef59565246e7e6bd59f07f0fe7
Kurtis Rader <krader@skepticism.us>
2020-08-14 22:20:35 -0700

    Replace "USERNAME" literals with a constant

--- pkg/shell/paths_windows.go
+++ pkg/shell/paths_windows.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/elves/elvish/pkg/util"
 )
 
 // getSecureRunDir stats elvish-$USERNAME under the default temp dir, creating
 // it if it doesn't yet exist, and return the directory name.
 func getSecureRunDir() (string, error) {
-	username := os.Getenv("USERNAME")
+	username := os.Getenv(util.EnvUSERNAME)
 
 	runDir := filepath.Join(os.TempDir(), "elvish-"+username)
 	err := os.MkdirAll(runDir, 0700)
