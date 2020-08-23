b2c85b7c2f1e3c5798da9472dcd412c3bd42cc3e
Qi Xiao <xiaqqaix@gmail.com>
2020-08-23 17:30:56 +0100

    pkg/eval: Add some basic benchmarks for code evaluation.

--- pkg/eval/benchmarks_test.go
+++ pkg/eval/benchmarks_test.go
@@ -6,24 +6,56 @@ import (
 	"github.com/elves/elvish/pkg/parse"
 )
 
-func BenchmarkOutputCaptureOverhead(b *testing.B) {
+func BenchmarkEval_Empty(b *testing.B) {
+	benchmarkEval(b.N, "")
+}
+
+func BenchmarkEval_NopCommand(b *testing.B) {
+	benchmarkEval(b.N, "nop")
+}
+
+func BenchmarkEval_PutCommand(b *testing.B) {
+	benchmarkEval(b.N, "put x")
+}
+
+func BenchmarkEval_ForLoop100WithEmptyBody(b *testing.B) {
+	benchmarkEval(b.N, "for x [(range 100)] { }")
+}
+
+func BenchmarkEval_EachLoop100WithEmptyBody(b *testing.B) {
+	benchmarkEval(b.N, "range 100 | each [x]{ }")
+}
+
+func benchmarkEval(n int, code string) {
+	ev := NewEvaler()
+	src := parse.Source{Name: "[benchmark]", Code: code}
+	op, err := ev.ParseAndCompile(src, nil)
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < n; i++ {
+		ev.Eval(op, EvalCfg{})
+	}
+}
+
+func BenchmarkOutputCapture_Overhead(b *testing.B) {
 	benchmarkOutputCapture(b.N, func(fm *Frame) {})
 }
 
-func BenchmarkOutputCaptureValues(b *testing.B) {
+func BenchmarkOutputCapture_Values(b *testing.B) {
 	benchmarkOutputCapture(b.N, func(fm *Frame) {
 		fm.ports[1].Chan <- "test"
 	})
 }
 
-func BenchmarkOutputCaptureBytes(b *testing.B) {
+func BenchmarkOutputCapture_Bytes(b *testing.B) {
 	bytesToWrite := []byte("test")
 	benchmarkOutputCapture(b.N, func(fm *Frame) {
 		fm.ports[1].File.Write(bytesToWrite)
 	})
 }
 
-func BenchmarkOutputCaptureMixed(b *testing.B) {
+func BenchmarkOutputCapture_Mixed(b *testing.B) {
 	bytesToWrite := []byte("test")
 	benchmarkOutputCapture(b.N, func(fm *Frame) {
 		fm.ports[1].Chan <- false
