0931789cdeaf74b039be4972bad7d1b5661f9697
Qi Xiao <xiaqqaix@gmail.com>
2021-02-13 01:26:13 +0000

    Fix style.

--- pkg/edit/config_api.go
+++ pkg/edit/config_api.go
@@ -161,10 +161,10 @@ func callFilters(ev *eval.Evaler, name string, filters vals.List, args ...interf
 	return true
 }
 
-func newIntVar(i int) vars.PtrVar            { return vars.FromPtr(&i) }
-func newFloatVar(f float64) vars.PtrVar      { return vars.FromPtr(&f) }
-func newBoolVar(b bool) vars.PtrVar          { return vars.FromPtr(&b) }
-func newListVar(l vals.List) vars.PtrVar     { return vars.FromPtr(&l) }
-func newMapVar(m vals.Map) vars.PtrVar       { return vars.FromPtr(&m) }
-func newFnVar(c eval.Callable) vars.PtrVar   { return vars.FromPtr(&c) }
+func newIntVar(i int) vars.PtrVar             { return vars.FromPtr(&i) }
+func newFloatVar(f float64) vars.PtrVar       { return vars.FromPtr(&f) }
+func newBoolVar(b bool) vars.PtrVar           { return vars.FromPtr(&b) }
+func newListVar(l vals.List) vars.PtrVar      { return vars.FromPtr(&l) }
+func newMapVar(m vals.Map) vars.PtrVar        { return vars.FromPtr(&m) }
+func newFnVar(c eval.Callable) vars.PtrVar    { return vars.FromPtr(&c) }
 func newBindingVar(b bindingsMap) vars.PtrVar { return vars.FromPtr(&b) }
