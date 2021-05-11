fc28dddb383243326bbe24d2bb7154ffb5594823
Supreet <singh.supreet14@gmail.com>
2021-05-11 10:56:37 +0530

    file:truncate added to file module, with requested changes

--- pkg/eval/mods/file/file.go
+++ pkg/eval/mods/file/file.go
@@ -1,20 +1,23 @@
 package file
 
 import (
+	"math/big"
 	"os"
 
 	"src.elv.sh/pkg/eval"
+	"src.elv.sh/pkg/eval/errs"
 	"src.elv.sh/pkg/eval/vals"
 )
 
 var Ns = eval.NsBuilder{}.AddGoFns("file:", fns).Ns()
 
 var fns = map[string]interface{}{
-	"close":   close,
-	"open":    open,
-	"pipe":    pipe,
-	"prclose": prclose,
-	"pwclose": pwclose,
+	"close":    close,
+	"open":     open,
+	"pipe":     pipe,
+	"prclose":  prclose,
+	"pwclose":  pwclose,
+	"truncate": truncate,
 }
 
 //elvdoc:fn open
@@ -123,4 +126,56 @@ func prclose(p vals.Pipe) error {
 
 func pwclose(p vals.Pipe) error {
 	return p.WriteEnd.Close()
+}
+
+//elvdoc:fn truncate
+//
+// ```elvish
+// file:truncate $filename $size
+// ```
+// Truncate changes the size of
+// the named file. If the file is
+// a symbolic link, it changes the
+// size of the link's target.
+//
+// If there is an error it will
+// be of type *PathError.
+
+func truncate(name string, size vals.Num) error {
+	var offset int64
+	switch size.(type) {
+	case int:
+		offset = int64(size.(int))
+		if offset < 0 {
+			return errs.BadValue{
+				What:   "Numeric input for the function is wrong",
+				Valid:  "positive 64 bit integer",
+				Actual: "Number less than 0",
+			}
+		}
+	case *big.Int:
+		if (size.(*big.Int)).IsInt64() {
+			offset = (size.(*big.Int)).Int64()
+			if offset < 0 {
+				return errs.BadValue{
+					What:   "Numeric input for the function is wrong",
+					Valid:  "positive 64 bit integer",
+					Actual: "Number less than 0",
+				}
+			}
+		} else {
+			return errs.BadValue{
+				What:   "Numeric input for function is wrong",
+				Valid:  "positive 64 bit integer",
+				Actual: "exceeding the MaxInt64",
+			}
+		}
+	default:
+		return errs.BadValue{
+			What:   "size argument for os.Truncate",
+			Valid:  "int or *big.Int",
+			Actual: "string",
+		}
+	}
+	return os.Truncate(name, offset)
 }
