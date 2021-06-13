a823eb8fbbe90e82c8c7abc34260f13660008b8a
Qi Xiao <xiaqqaix@gmail.com>
2021-06-13 16:27:36 +0100

    Fix a broken link.

--- pkg/eval/mods/file/file.go
+++ pkg/eval/mods/file/file.go
@@ -68,7 +68,7 @@ func close(f vals.File) error {
 //
 // Create a new pipe that can be used in redirections. A pipe contains a read-end and write-end.
 // Each pipe object is a [pseudo-map](#pseudo-map) with fields `r` (the read-end [file
-// object](./language.html#File)) and `w` (the write-end).
+// object](./language.html#file)) and `w` (the write-end).
 
 // When redirecting command input from a pipe with `<`, the read-end is used. When redirecting
 // command output to a pipe with `>`, the write-end is used. Redirecting both input and output with
