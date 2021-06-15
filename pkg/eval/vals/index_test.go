2093ac680ffa597a760099c8a5ed675bcec124d7
Qi Xiao <xiaqqaix@gmail.com>
2021-06-15 18:48:24 +0100

    Increase coverage of pkg/eval/vals/index_list.go to 100%.
    This addresses #1234.

--- pkg/eval/vals/index_test.go
+++ pkg/eval/vals/index_test.go
@@ -46,16 +46,20 @@ func TestIndex(t *testing.T) {
 		Args(li4, "0").Rets("foo", nil),
 		Args(li4, "3").Rets("ipsum", nil),
 		Args(li0, "0").Rets(Any, errs.OutOfRange{
-			What: "index here", ValidLow: "0", ValidHigh: "-1", Actual: "0"}),
+			What: "index", ValidLow: "0", ValidHigh: "-1", Actual: "0"}),
 		Args(li4, "4").Rets(Any, errs.OutOfRange{
-			What: "index here", ValidLow: "0", ValidHigh: "3", Actual: "4"}),
+			What: "index", ValidLow: "0", ValidHigh: "3", Actual: "4"}),
 		Args(li4, "5").Rets(Any, errs.OutOfRange{
-			What: "index here", ValidLow: "0", ValidHigh: "3", Actual: "5"}),
+			What: "index", ValidLow: "0", ValidHigh: "3", Actual: "5"}),
+		Args(li4, z).Rets(Any,
+			errs.OutOfRange{What: "index", ValidLow: "0", ValidHigh: "3", Actual: z}),
 		// Negative indices: -n <= i < 0.
 		Args(li4, "-1").Rets("ipsum", nil),
 		Args(li4, "-4").Rets("foo", nil),
 		Args(li4, "-5").Rets(Any, errs.OutOfRange{
-			What: "negative index here", ValidLow: "-4", ValidHigh: "-1", Actual: "-5"}),
+			What: "negative index", ValidLow: "-4", ValidHigh: "-1", Actual: "-5"}),
+		Args(li4, "-"+z).Rets(Any,
+			errs.OutOfRange{What: "negative index", ValidLow: "-4", ValidHigh: "-1", Actual: "-" + z}),
 		// Float indices are not allowed even if the value is an integer.
 		Args(li4, 0.0).Rets(Any, errIndexMustBeInteger),
 
@@ -63,10 +67,10 @@ func TestIndex(t *testing.T) {
 		Args(li4, 0).Rets("foo", nil),
 		Args(li4, 3).Rets("ipsum", nil),
 		Args(li4, 5).Rets(nil, errs.OutOfRange{
-			What: "index here", ValidLow: "0", ValidHigh: "3", Actual: "5"}),
+			What: "index", ValidLow: "0", ValidHigh: "3", Actual: "5"}),
 		Args(li4, -1).Rets("ipsum", nil),
 		Args(li4, -5).Rets(nil, errs.OutOfRange{
-			What: "negative index here", ValidLow: "-4", ValidHigh: "-1", Actual: "-5"}),
+			What: "negative index", ValidLow: "-4", ValidHigh: "-1", Actual: "-5"}),
 
 		// Half-open slices.
 		Args(li4, "1..3").Rets(Eq(MakeList("bar", "lorem")), nil),
@@ -102,13 +106,19 @@ func TestIndex(t *testing.T) {
 		Args(li4, "3..=").Rets(Eq(MakeList("ipsum")), nil),
 		Args(li4, "..=").Rets(Eq(li4), nil),
 
-		// Index out of range.
+		// Slice index out of range.
 		Args(li4, "-5:1").Rets(nil, errs.OutOfRange{
-			What: "negative index here", ValidLow: "-4", ValidHigh: "-1", Actual: "-5"}),
+			What: "negative index", ValidLow: "-4", ValidHigh: "-1", Actual: "-5"}),
 		Args(li4, "0:5").Rets(nil, errs.OutOfRange{
-			What: "index here", ValidLow: "0", ValidHigh: "4", Actual: "5"}),
+			What: "index", ValidLow: "0", ValidHigh: "4", Actual: "5"}),
+		Args(li4, z+":").Rets(nil,
+			errs.OutOfRange{What: "index", ValidLow: "0", ValidHigh: "4", Actual: z}),
+		// Slice index upper < lower
 		Args(li4, "3:2").Rets(nil, errs.OutOfRange{
-			What: "slice upper index here", ValidLow: "3", ValidHigh: "4", Actual: "2"}),
+			What: "slice upper index", ValidLow: "3", ValidHigh: "4", Actual: "2"}),
+		Args(li4, "-1:-2").Rets(nil,
+			errs.OutOfRange{What: "negative slice upper index",
+				ValidLow: "-1", ValidHigh: "-1", Actual: "-2"}),
 
 		// Malformed list indices.
 		Args(li4, "a").Rets(Any, errIndexMustBeInteger),
