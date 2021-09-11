8c8a80ff5261a361ba50f110f3aa283c7fa6d1dc
Qi Xiao <xiaqqaix@gmail.com>
2021-09-11 02:58:54 +0100

    Fix staticcheck error.

--- pkg/eval/vals/aliased_types_test.go
+++ pkg/eval/vals/aliased_types_test.go
@@ -9,6 +9,7 @@ import (
 
 func TestMakeMap_PanicsWithOddNumberOfArguments(t *testing.T) {
 	Test(t, Fn("Recover", testutil.Recover), Table{
+		//lint:ignore SA5012 testing panic
 		Args(func() { MakeMap("foo") }).Rets("odd number of arguments to MakeMap"),
 	})
 }
