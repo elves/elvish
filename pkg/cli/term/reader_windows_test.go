0219b99addf2fdf76ee6d7f4f17c9c6360b2236b
Qi Xiao <xiaqqaix@gmail.com>
2021-10-03 00:03:02 +0100

    pkg/cli/term: More tests for convertEvent in reader_windows.go.

--- pkg/cli/term/reader_windows_test.go
+++ pkg/cli/term/reader_windows_test.go
@@ -5,6 +5,7 @@ import (
 
 	"src.elv.sh/pkg/sys/ewindows"
 	"src.elv.sh/pkg/tt"
+	"src.elv.sh/pkg/ui"
 )
 
 var Args = tt.Args
@@ -16,6 +17,34 @@ func TestConvertEvent(t *testing.T) {
 		// Only convert KeyDown events
 		Args(&ewindows.KeyEvent{BKeyDown: 0}).Rets(nil),
 
-		Args(&ewindows.KeyEvent{BKeyDown: 1, UChar: [2]byte{'a', 0}}).Rets(K('a')),
+		Args(charKeyEvent('a', 0)).Rets(K('a')),
+		Args(charKeyEvent('A', shift)).Rets(K('A')),
+		Args(charKeyEvent('µ', leftCtrl|rightAlt)).Rets(K('µ')),
+		Args(charKeyEvent('ẞ', leftCtrl|rightAlt|shift)).Rets(K('ẞ')),
+
+		Args(funcKeyEvent(0x1b, 0)).Rets(K('[', ui.Ctrl)),
+
+		// Functional key with modifiers
+		Args(funcKeyEvent(0x08, 0)).Rets(K(ui.Backspace)),
+		Args(funcKeyEvent(0x08, leftCtrl)).Rets(K(ui.Backspace, ui.Ctrl)),
+		Args(funcKeyEvent(0x08, leftCtrl|leftAlt|shift)).Rets(K(ui.Backspace, ui.Ctrl, ui.Alt, ui.Shift)),
+
+		// Functional keys with an alphanumeric base
+		Args(funcKeyEvent('2', leftCtrl)).Rets(K('2', ui.Ctrl)),
+		Args(funcKeyEvent('A', leftCtrl)).Rets(K('A', ui.Ctrl)),
+		Args(funcKeyEvent('A', leftAlt)).Rets(K('a', ui.Alt)),
+
+		// Unrecognized functional key
+		Args(funcKeyEvent(0, 0)).Rets(nil),
 	})
+}
+
+func charKeyEvent(r uint16, mod uint32) *ewindows.KeyEvent {
+	return &ewindows.KeyEvent{
+		BKeyDown: 1, DwControlKeyState: mod, UChar: [2]byte{byte(r), byte(r >> 8)}}
+}
+
+func funcKeyEvent(code uint16, mod uint32) *ewindows.KeyEvent {
+	return &ewindows.KeyEvent{
+		BKeyDown: 1, DwControlKeyState: mod, WVirtualKeyCode: code}
 }
