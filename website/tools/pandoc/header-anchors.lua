9dfda4c7533b6261ed66e6a46f2d0358d3ad085b
Qi Xiao <xiaqqaix@gmail.com>
2021-11-30 01:45:53 +0000

    Use onclick="" to make :hover work on mobile.
    Anchor link will be shown after clicking the header. Not sure how this works,
    but it does work. Seen on https://stackoverflow.com/a/25673064/566659.

--- website/tools/pandoc/header-anchors.lua
+++ website/tools/pandoc/header-anchors.lua
@@ -1,6 +1,9 @@
 function Header(el)
-  local link = pandoc.Link('', '#'..el.identifier, '',
+  local id = el.identifier
+  if id == '' then return el end
+  local link = pandoc.Link('', '#'..id, '',
                            {['class'] = 'anchor icon-link', ['aria-hidden'] = 'true'})
   el.content:insert(link)
+  el.attributes['onclick'] = ''
   return el
 end
