6a9931e662a476f2a86c2354673d9846d11525e4
Qi Xiao <xiaqqaix@gmail.com>
2021-12-06 00:30:36 +0000

    Bump version and deprecation level
    Now that v0.17-release has been branched, the HEAD build will show deprecations
    intended for 0.17.0, and be considered a pre-release version of 0.18.0.

--- pkg/prog/prog.go
+++ pkg/prog/prog.go
@@ -20,7 +20,7 @@ import (
 // DeprecationLevel is a global flag that controls which deprecations to show.
 // If its value is X, Elvish shows deprecations that should be shown for version
 // 0.X.
-var DeprecationLevel = 16
+var DeprecationLevel = 17
 
 // Flags keeps command-line flags.
 type Flags struct {
