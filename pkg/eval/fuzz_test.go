11de899040dffcec3ded1dd94bdd6eb94e924775
Qi Xiao <xiaqqaix@gmail.com>
2022-01-01 23:52:08 +0000

    Add fuzz test for (*eval.Evaler).Check.

---
+++ pkg/eval/fuzz_test.go
@@ -0,0 +1,19 @@
+//go:build go1.18
+// +build go1.18
+
+package eval
+
+import (
+	"testing"
+
+	"src.elv.sh/pkg/parse"
+)
+
+func FuzzCheck(f *testing.F) {
+	f.Add("echo")
+	f.Add("put $x")
+	f.Add("put foo bar | each {|x| echo $x }")
+	f.Fuzz(func(t *testing.T, code string) {
+		NewEvaler().Check(parse.Source{Name: "fuzz", Code: code}, nil)
+	})
+}
