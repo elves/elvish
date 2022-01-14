f5b645cc2d647e0aa75cbe884a4cd17818fc6c13
Christian Walter <christian.walter@9elements.com>
2022-01-14 14:23:01 +0100

    pkg/edit: Don't call AddDir on storedefs if it is not ready
    Signed-off-by: Christian Walter <christian.walter@9elements.com>

--- pkg/edit/listing.go
+++ pkg/edit/listing.go
@@ -124,9 +124,12 @@ func initLocation(ed *Editor, ev *eval.Evaler, st storedefs.Store, commonBinding
 			// TODO(xiaq): Surface the error.
 			return
 		}
-		st.AddDir(wd, 1)
+		// If st is not ready - don't try to call it
+		if st != nil {
+			st.AddDir(wd, 1)
+		}
 		kind, root := workspaceIterator.Parse(wd)
-		if kind != "" {
+		if kind != "" && st != nil {
 			st.AddDir(kind+wd[len(root):], 1)
 		}
 	})
