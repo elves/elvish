707aaffcedf7e2ffbb02a91554352cf20916ce81
moko256 <koutaro.mo@gmail.com>
2022-01-27 06:17:29 +0900

    Use `src.elv.sh/pkg/rpc` instead of `net/rpc`.

--- pkg/daemon/server.go
+++ pkg/daemon/server.go
@@ -7,7 +7,6 @@ package daemon
 
 import (
 	"net"
-	"net/rpc"
 	"os"
 	"os/signal"
 	"syscall"
@@ -15,6 +14,7 @@ import (
 	"src.elv.sh/pkg/daemon/internal/api"
 	"src.elv.sh/pkg/logutil"
 	"src.elv.sh/pkg/prog"
+	"src.elv.sh/pkg/rpc"
 	"src.elv.sh/pkg/store"
 )
 
