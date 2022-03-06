effa2670079409760d4c40af711e3285cb85a7fc
Qi Xiao <xiaqqaix@gmail.com>
2022-03-06 23:48:46 +0000

    Add comment for the semantics of SIGTERM on Windows.

--- pkg/shell/signal_windows.go
+++ pkg/shell/signal_windows.go
@@ -8,6 +8,8 @@ import (
 
 func handleSignal(sig os.Signal, stderr io.Writer) {
 	switch sig {
+	// See https://pkg.go.dev/os/signal#hdr-Windows for the semantics of SIGTERM
+	// on Windows.
 	case syscall.SIGTERM:
 		os.Exit(0)
 	}
