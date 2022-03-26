ee309bf1c6f46e614201d91db18e296fd964faa3
Kurtis Rader <krader@skepticism.us>
2022-03-25 19:54:15 -0700

    Don't dot import pkg/parse
    Qualified imports of pkg/parse outnumber unqualified (66 to 1). Improve
    consistency, and clarity, by changing the dot (unqualified) imports of
    that package symbols to qualified.

--- pkg/parse/source_test.go
+++ pkg/parse/source_test.go
@@ -4,15 +4,15 @@ import (
 	"testing"
 
 	"src.elv.sh/pkg/eval/vals"
-	. "src.elv.sh/pkg/parse"
+	"src.elv.sh/pkg/parse"
 )
 
 func TestSourceAsStructMap(t *testing.T) {
-	vals.TestValue(t, Source{Name: "[tty]", Code: "echo"}).
+	vals.TestValue(t, parse.Source{Name: "[tty]", Code: "echo"}).
 		Kind("structmap").
 		Repr("[&name='[tty]' &code=<...> &is-file=$false]").
 		AllKeys("name", "code", "is-file")
 
-	vals.TestValue(t, Source{Name: "/etc/rc.elv", Code: "echo", IsFile: true}).
+	vals.TestValue(t, parse.Source{Name: "/etc/rc.elv", Code: "echo", IsFile: true}).
 		Index("is-file", true)
 }
