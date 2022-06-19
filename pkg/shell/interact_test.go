78d914b24b815a7e9157da26a4e238f87fd18d6b
Qi Xiao <xiaqqaix@gmail.com>
2022-06-19 19:08:11 +0100

    pkg/shell: Run TestInteract_ConnectsToDaemon on Windows too.
    This test now succeeds on Windows too, now that the daemon also uses Unix
    sockets on Windows.

--- pkg/shell/interact_test.go
+++ pkg/shell/interact_test.go
@@ -1,15 +1,19 @@
 package shell
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
+	"src.elv.sh/pkg/daemon"
 	"src.elv.sh/pkg/env"
 	. "src.elv.sh/pkg/prog/progtest"
 	"src.elv.sh/pkg/testutil"
 )
 
-func TestInteract(t *testing.T) {
+func TestInteract_Eval(t *testing.T) {
 	setupCleanHomePaths(t)
 	testutil.InTempDir(t)
 	testutil.MustWriteFile("rc.elv", "echo hello from rc.elv")
@@ -54,6 +58,37 @@ func TestInteract_RCPath_XDG_CONFIG_HOME(t *testing.T) {
 
 	Test(t, &Program{},
 		thatElvishInteract().WritesStdout("hello XDG_CONFIG_HOME rc.elv\n"),
+	)
+}
+
+func TestInteract_ConnectsToDaemon(t *testing.T) {
+	testutil.InTempDir(t)
+
+	// Run the daemon in the same process for simplicity.
+	daemonDone := make(chan struct{})
+	defer func() {
+		select {
+		case <-daemonDone:
+		case <-time.After(testutil.Scaled(2 * time.Second)):
+			t.Errorf("timed out waiting for daemon to quit")
+		}
+	}()
+	readyCh := make(chan struct{})
+	go func() {
+		daemon.Serve("sock", "db", daemon.ServeOpts{Ready: readyCh})
+		close(daemonDone)
+	}()
+	select {
+	case <-readyCh:
+		// Do nothing
+	case <-time.After(testutil.Scaled(2 * time.Second)):
+		t.Fatalf("timed out waiting for daemon to start")
+	}
+
+	Test(t, &Program{ActivateDaemon: daemon.Activate},
+		thatElvishInteract("-sock", "sock", "-db", "db").
+			WithStdin("use daemon; echo $daemon:pid\n").
+			WritesStdout(fmt.Sprintln(os.Getpid())),
 	)
 }
 
--- pkg/shell/interact_unix_test.go
+++ pkg/shell/interact_unix_test.go
@@ -3,13 +3,9 @@
 package shell
 
 import (
-	"fmt"
-	"os"
 	"path/filepath"
 	"testing"
-	"time"
 
-	"src.elv.sh/pkg/daemon"
 	"src.elv.sh/pkg/env"
 
 	. "src.elv.sh/pkg/prog/progtest"
@@ -24,36 +20,5 @@ func TestInteract_RCPath_Default(t *testing.T) {
 
 	Test(t, &Program{},
 		thatElvishInteract().WritesStdout("hello new rc.elv\n"),
-	)
-}
-
-func TestInteract_ConnectsToDaemon(t *testing.T) {
-	testutil.InTempDir(t)
-
-	// Run the daemon in the same process for simplicity.
-	daemonDone := make(chan struct{})
-	defer func() {
-		select {
-		case <-daemonDone:
-		case <-time.After(testutil.Scaled(2 * time.Second)):
-			t.Errorf("timed out waiting for daemon to quit")
-		}
-	}()
-	readyCh := make(chan struct{})
-	go func() {
-		daemon.Serve("sock", "db", daemon.ServeOpts{Ready: readyCh})
-		close(daemonDone)
-	}()
-	select {
-	case <-readyCh:
-		// Do nothing
-	case <-time.After(testutil.Scaled(2 * time.Second)):
-		t.Fatalf("timed out waiting for daemon to start")
-	}
-
-	Test(t, &Program{ActivateDaemon: daemon.Activate},
-		thatElvishInteract("-sock", "sock", "-db", "db").
-			WithStdin("use daemon; echo $daemon:pid\n").
-			WritesStdout(fmt.Sprintln(os.Getpid())),
 	)
 }
