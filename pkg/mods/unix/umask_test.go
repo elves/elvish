ff204a19eb1764f851c4e7477e493f2ab0539fa1
Qi Xiao <xiaqqaix@gmail.com>
2022-11-11 16:22:59 +0000

    pkg/mods/unix: Fix race condition in UmaskVariable.Get.

--- pkg/mods/unix/umask_test.go
+++ pkg/mods/unix/umask_test.go
@@ -3,10 +3,15 @@
 package unix
 
 import (
+	"os"
+	"strconv"
+	"sync"
 	"testing"
 
 	"src.elv.sh/pkg/eval/errs"
 	"src.elv.sh/pkg/eval/evaltest"
+	"src.elv.sh/pkg/must"
+	"src.elv.sh/pkg/testutil"
 )
 
 // Note that this unit test assumes a UNIX environment with a POSIX compatible
@@ -54,4 +59,53 @@ func TestUmask(t *testing.T) {
 		That(`set unix:umask = -1`).Throws(errs.OutOfRange{
 			What: "umask", ValidLow: "0", ValidHigh: "0o777", Actual: "-0o1"}),
 	)
+}
+
+func TestUmaskGetRace(t *testing.T) {
+	testutil.Umask(t, 0o22)
+	testutil.InTempDir(t)
+
+	// An old implementation of UmaskVariable.Get had a bug where it will
+	// briefly set the umask to 0, which can impact files created concurrently.
+	for i := 0; i < 100; i++ {
+		filename := strconv.Itoa(i)
+		runParallel(
+			func() {
+				// Calling UmaskVariable.Get is much quicker than creating a
+				// file, so do it in a loop to increase the chance of triggering
+				// a race condition.
+				for j := 0; j < 100; j++ {
+					UmaskVariable{}.Get()
+				}
+			},
+			func() {
+				must.OK(create(filename, 0o666))
+			})
+
+		perm := must.OK1(os.Stat(filename)).Mode().Perm()
+		if perm != 0o644 {
+			t.Errorf("got perm %o, want 0o644 (run %d)", perm, i)
+		}
+	}
+}
+
+func runParallel(funcs ...func()) {
+	var wg sync.WaitGroup
+	wg.Add(len(funcs))
+	for _, f := range funcs {
+		f := f
+		go func() {
+			f()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func create(name string, perm os.FileMode) error {
+	file, err := os.OpenFile(name, os.O_CREATE, perm)
+	if err == nil {
+		file.Close()
+	}
+	return err
 }
