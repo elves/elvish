1f0f2a3e9d5f5a7771416481566e985a198cafff
Qi Xiao <xiaqqaix@gmail.com>
2022-11-21 11:39:33 +0000

    pkg/mods/file: Only test is-tty with /dev/tty if it can be opened.
    The file cannot be opened when not running in a terminal.

--- pkg/mods/file/file_unix_test.go
+++ pkg/mods/file/file_unix_test.go
@@ -3,6 +3,7 @@
 package file
 
 import (
+	"os"
 	"testing"
 
 	"src.elv.sh/pkg/eval"
@@ -14,10 +15,22 @@ func TestIsTTY(t *testing.T) {
 		ev.ExtendGlobal(eval.BuildNs().AddNs("file", Ns))
 	}
 
-	evaltest.TestWithSetup(t, setup,
-		That("file:is-tty 0 < /dev/null").Puts(false),
-		That("file:is-tty (num 0) < /dev/null").Puts(false),
-		That("file:is-tty 0 < /dev/tty").Puts(true),
-		That("file:is-tty (num 0) < /dev/tty").Puts(true),
-	)
+	if canOpen("/dev/null") {
+		evaltest.TestWithSetup(t, setup,
+			That("file:is-tty 0 < /dev/null").Puts(false),
+			That("file:is-tty (num 0) < /dev/null").Puts(false),
+		)
+	}
+	if canOpen("/dev/tty") {
+		evaltest.TestWithSetup(t, setup,
+			That("file:is-tty 0 < /dev/tty").Puts(true),
+			That("file:is-tty (num 0) < /dev/tty").Puts(true),
+		)
+	}
+}
+
+func canOpen(name string) bool {
+	f, err := os.Open(name)
+	f.Close()
+	return err == nil
 }
