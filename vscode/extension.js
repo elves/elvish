bcb9a6f149ce90d14cce701e4782bc460029f017
jiujieti <melody2014ymq@gmail.com>
2023-01-15 16:57:09 +0000

    Create basic command for evaluating elvish

--- vscode/extension.js
+++ vscode/extension.js
@@ -1,8 +1,12 @@
 const { LanguageClient } = require("vscode-languageclient/node");
+import * as vscode from 'vscode';
 
 let client;
 
-function activate() {
+function activate(context) {
+    // Creates an Elvish output channel.
+    let elvishOutputChannel = vscode.window.createOutputChannel("Elvish");
+   
     client = new LanguageClient(
         "elvish",
         "Elvish Language Server",
@@ -10,6 +14,22 @@ function activate() {
         { documentSelector: [{ scheme: "file", language: "elvish" }] }
     );
     client.start();
+
+    const command = "elvish.eval";
+    const commandHandler = () => {
+
+        const window = vscode.window;
+        const activeEditor = window.activeTextEditor;
+        if (activeEditor && client) {
+            const { text } = activeEditor.document.lineAt(activeEditor.selection.active.line);
+            client.sendRequest("elvish/eval", {code: text})
+                .then(result => {
+                    const resultString = JSON.stringify(result);
+                    elvishOutputChannel.appendLine(resultString);
+                });
+        }
+    };
+    context.subscriptions.push(vscode.commands.registerCommand(command, commandHandler));
 }
 
 function deactivate() {
