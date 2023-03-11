2a4740a523e7cc2f025913f44f0f70b54c934826
Qi Xiao <xiaqqaix@gmail.com>
2023-03-11 20:04:55 +0000

    pkg/sys/eunix: Simplify build constraints for the termiosFlag definition.

--- pkg/sys/eunix/termiosflag_darwin.go
+++ pkg/sys/eunix/termiosflag_darwin.go
@@ -1,8 +1,9 @@
-//go:build darwin && (amd64 || arm64)
-
 package eunix
 
 // Only Darwin uses 64-bit flags in Termios on 64-bit architectures. See:
 // https://cs.opensource.google/search?q=%5BIOCL%5Dflag.*uint64&sq=&ss=go%2Fx%2Fsys
+//
+// Darwin uses 32-bit flags on 32-bit architectures, but Go no longer supports
+// them since Go 1.15.
 
 type termiosFlag = uint64
