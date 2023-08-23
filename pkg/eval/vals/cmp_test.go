a15f0f7d0a1c9d675fbc26eeffd9a2da4e4e4ef1
Qi Xiao <xiaqqaix@gmail.com>
2023-08-22 22:07:26 -0400

    pkg/eval/vals: Test that structmaps are treated like maps by CmpTotal.

--- pkg/eval/vals/cmp_test.go
+++ pkg/eval/vals/cmp_test.go
@@ -1,7 +1,27 @@
 package vals
 
-import "testing"
+import (
+	"testing"
+
+	"src.elv.sh/pkg/tt"
+)
 
 func TestCmp(t *testing.T) {
 	// Cmp is tested by tests of the Elvish compare command.
+}
+
+func TestCmpTotal_StructMap(t *testing.T) {
+	// CmpTotal should pretend that structmaps are maps too. Since maps don't
+	// have an internal ordering, comparing a structmap to another structmap or
+	// to a map should always return CmpEqual, like comparing two maps.
+	//
+	// This is not covered by tests of the Elvish compare command because Elvish
+	// code are not supposed to know which values are actually structmaps.
+	x := testStructMap{}
+	y := testStructMap2{}
+	z := EmptyMap
+	tt.Test(t, tt.Fn("CmpTotal", CmpTotal), tt.Table{
+		tt.Args(x, y).Rets(CmpEqual),
+		tt.Args(x, z).Rets(CmpEqual),
+	})
 }
