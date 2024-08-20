0b01bf95861712b3a8d8e5bf64493ccf446d274a
Qi Xiao <xiaqqaix@gmail.com>
2024-08-20 09:44:33 +0100

    pkg/vals: Add benchmarks for structmap operations.

--- pkg/eval/vals/struct_map_test.go
+++ pkg/eval/vals/struct_map_test.go
@@ -74,3 +74,118 @@ func TestPseudoMap(t *testing.T) {
 		Index("score", 100.0).
 		Index("score-plus-ten", 110.0)
 }
+
+// Benchmarks for reflection-based operations on a structmap compared to manual
+// implementations.
+
+type structmap struct {
+	Foo   string
+	Bar   string
+	Lorem string
+}
+
+func (structmap) IsStructMap() {}
+
+func BenchmarkStructMap_Index(b *testing.B) {
+	benchmarkStructMap_Index(b, structmap{})
+}
+
+type structmapIndexer structmap
+
+var _ Indexer = structmapIndexer{}
+
+func (s structmapIndexer) Index(k any) (any, bool) {
+	switch k {
+	case "foo":
+		return s.Foo, true
+	case "bar":
+		return s.Bar, true
+	case "lorem":
+		return s.Lorem, true
+	}
+	return nil, false
+}
+
+func BenchmarkStructMap_Index_Manual(b *testing.B) {
+	benchmarkStructMap_Index(b, structmapIndexer{})
+}
+
+func benchmarkStructMap_Index(b *testing.B, value any) {
+	for range b.N {
+		Index(value, "foo")
+		Index(value, "bar")
+		Index(value, "lorem")
+		Index(value, "ipsum")
+	}
+}
+
+func BenchmarkStructMap_HasKey(b *testing.B) {
+	benchmarkStructMap_HasKey(b, structmap{})
+}
+
+type structmapHasKeyer structmap
+
+var _ HasKeyer = structmapHasKeyer{}
+
+func (s structmapHasKeyer) HasKey(k any) bool {
+	return k == "foo" || k == "bar" || k == "lorem"
+}
+
+func BenchmarkStructMap_HasKey_Manual(b *testing.B) {
+	benchmarkStructMap_HasKey(b, structmapHasKeyer{})
+}
+
+func benchmarkStructMap_HasKey(b *testing.B, value any) {
+	for range b.N {
+		HasKey(value, "foo")
+		HasKey(value, "bar")
+		HasKey(value, "lorem")
+		HasKey(value, "ipsum")
+	}
+}
+
+func BenchmarkStructMap_IterateKeys(b *testing.B) {
+	benchmarkStructMap_IterateKeys(b, structmap{})
+}
+
+type structmapKeysIterator structmap
+
+var _ KeysIterator = structmapKeysIterator{}
+
+func (s structmapKeysIterator) IterateKeys(f func(any) bool) {
+	if f("foo") {
+		if f("bar") {
+			f("lorem")
+		}
+	}
+}
+
+func BenchmarkStructMap_IterateKeys_Manual(b *testing.B) {
+	benchmarkStructMap_IterateKeys(b, structmapKeysIterator{})
+}
+
+func benchmarkStructMap_IterateKeys(b *testing.B, value any) {
+	for range b.N {
+		IterateKeys(value, func(any) bool { return true })
+	}
+}
+
+func BenchmarkStructMap_Len(b *testing.B) {
+	benchmarkStructMap_Len(b, structmap{})
+}
+
+type structmapLener structmap
+
+var _ Lener = structmapLener{}
+
+func (s structmapLener) Len() int { return 3 }
+
+func BenchmarkStructMap_Len_Manual(b *testing.B) {
+	benchmarkStructMap_Len(b, structmapLener{})
+}
+
+func benchmarkStructMap_Len(b *testing.B, value any) {
+	for range b.N {
+		Len(value)
+	}
+}
