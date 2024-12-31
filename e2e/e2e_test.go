8b43b8e795db07e6944e34f90a8c2e093feade50
Qi Xiao <xiaqqaix@gmail.com>
2024-12-31 14:41:07 +0000

    e2e: Test all entrypoints.
    This fixes #1658.

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -15,18 +15,34 @@ import (
 var transcripts embed.FS
 
 const buildScript = `
-# Using "-o $workdir/" instead of "-o $workdir/elvish" gets us the correct file
-# extension on Windows
-go build (if (not-eq $E:GOCOVERDIR '') { put -cover }) -o $workdir/ src.elv.sh/cmd/elvish
+# This script relies on the fact that go tests are always run in the package's
+# directory; it should be run before changing directory.
+#
+# Note: Using "-o $workdir/" instead of "-o $workdir/elvish" here gets us the
+# correct file extension on Windows
+go build (if (not-eq $E:GOCOVERDIR '') { put -cover }) -o $workdir/ src.elv.sh/$entrypoint
 `
 
-func TestTranscripts(t *testing.T) {
+func TestTranscripts_Default(t *testing.T) {
+	testTranscripts(t, "cmd/elvish")
+}
+
+func TestTranscripts_NoDaemon(t *testing.T) {
+	testTranscripts(t, "cmd/nodaemon/elvish")
+}
+
+func TestTranscripts_WithPprof(t *testing.T) {
+	testTranscripts(t, "cmd/withpprof/elvish")
+}
+
+func testTranscripts(t *testing.T, entrypoint string) {
 	workdir := t.TempDir()
 	err := eval.NewEvaler().Eval(
 		parse.Source{Name: "[build]", Code: buildScript},
 		eval.EvalCfg{
 			Global: eval.BuildNs().AddVars(map[string]vars.Var{
-				"workdir": vars.NewReadOnly(workdir),
+				"entrypoint": vars.NewReadOnly(entrypoint),
+				"workdir":    vars.NewReadOnly(workdir),
 			}).Ns()})
 	if err != nil {
 		t.Fatal(err)
