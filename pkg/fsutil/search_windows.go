cddb06467764d1890c798bbe5c67260e38dbf0f8
Balthild <ibalthild@gmail.com>
2025-02-19 00:04:20 -0600

    Always consider .ps1 executable regardless of PATHEXT, aligning the behavior of cmd

--- pkg/fsutil/search_windows.go
+++ pkg/fsutil/search_windows.go
@@ -13,10 +13,15 @@ func isExecutable(stat os.FileInfo) bool {
 	return !stat.IsDir() && isExecutableExt(filepath.Ext(stat.Name()))
 }
 
-// Determines determines a file name extension is considered executable.
-// It honors PATHEXT but defaults to extensions ".com", ".exe", ".ps1", ".bat", ".cmd"
+// Determines a file name extension is considered executable.
+// It honors PATHEXT but defaults to extensions ".com", ".exe", ".bat", ".cmd"
 // if that env var isn't set.
+// Additionally, it treats ".ps1" as executable.
 func isExecutableExt(ext string) bool {
+	if ext == ".ps1" {
+		return true
+	}
+
 	validExts := make(map[string]bool)
 	if pathext := os.Getenv(env.PATHEXT); pathext != "" {
 		for _, e := range filepath.SplitList(strings.ToLower(pathext)) {
@@ -32,7 +37,6 @@ func isExecutableExt(ext string) bool {
 		validExts = map[string]bool{
 			".com": true,
 			".exe": true,
-			".ps1": true,
 			".bat": true,
 			".cmd": true,
 		}
