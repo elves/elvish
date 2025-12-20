40677f79111f10c826dd214c2368edc5b8b6face
Qi Xiao <xiaqqaix@gmail.com>
2025-12-20 17:40:15 +0000

    pkg/etk/comps: More tests for listbox.

--- pkg/mods/etk/etk.go
+++ pkg/mods/etk/etk.go
@@ -45,6 +45,7 @@ var Ns = eval.BuildNsNamed("etk").
 				return fmt.Sprintf("%#v", event)
 			}
 		},
+		"-string-items": comps.StringItems,
 		"with-init": func(fm *eval.Frame, compAny any, inits vals.Map) (etk.Comp, error) {
 			// TODO: Integrate the parsing into vals.ScanToGo
 			comp, err := scanComp(fm, compAny)
